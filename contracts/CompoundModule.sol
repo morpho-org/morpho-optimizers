1fa8cc844f03fb11e9f9faa12398d129b87ec295
MerlinEgalite <egalite.merlin@gmail.com>
2021-06-29 17:59:14 +0100

    fix: library and payable issues

--- contracts/CompoundModule.sol
+++ contracts/CompoundModule.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import {ICErc20, ICEth} from "./interfaces/ICompound.sol";
 import "./interfaces/IOracle.sol";
 
-contract CompoundModule is Math {
+contract CompoundModule {
 
     /* Structs */
 
@@ -27,13 +27,13 @@ contract CompoundModule is Math {
     uint256 constant public COLLATERAL_FACTOR = 12000; // Collateral factor in basis points 120% by default.
     uint256 constant public DENOMINATOR = 10000; // Collateral factor in basis points.
 
-    address constant public wethAddress = 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2;
+    address constant public wethAddress = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address payable constant public cEtherAddress =
         payable(0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5);
     address constant public daiAddress = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address payable constant public cDaiAddress =
         payable(0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643);
-    address constant public oracleAddress = 0x0000000000000000000000000000000000000000000000000000000000000000;
+    address constant public oracleAddress = 0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643;
 
     ICEth public cEthToken = ICEth(cEtherAddress);
     ICErc20 public cDaiToken = ICErc20(cDaiAddress);
@@ -43,7 +43,7 @@ contract CompoundModule is Math {
     /* External */
 
     function lend() external payable {
-        _supplyEthToCompound{value: msg.value}();
+        _supplyEthToCompound(msg.value);
         if (lendingBalanceOf[msg.sender].total == 0) {
             lenderToIndex[msg.sender] = currentLenders.length;
             currentLenders.push(msg.sender);
@@ -73,7 +73,7 @@ contract CompoundModule is Math {
         borrowingBalanceOf[msg.sender] += _amount;
 
         // Transfer ETH to borrower
-        msg.sender.transfer(_amount);
+        payable(msg.sender).transfer(_amount);
     }
 
     function payBackAll() external payable {
@@ -81,8 +81,8 @@ contract CompoundModule is Math {
             msg.value >= borrowingBalanceOf[msg.sender],
             "Must payback all the debt."
         );
-        address(this).transfer(msg.value);
-        _supplyEthToCompound{value: msg.value}();
+        payable(address(this)).transfer(msg.value);
+        _supplyEthToCompound(msg.value);
         _findUsedCTokensAndUnuse(msg.value);
         uint256 daiAmountOut = oracle.consult(wethAddress, msg.value, daiAddress); // This is the equivalent amount to repay in DAI
         uint256 amountToRedeem = (daiAmountOut / DENOMINATOR) *
@@ -118,22 +118,22 @@ contract CompoundModule is Math {
     function _cashOut(address _lender, uint256 _amount) internal {
         if (lendingBalanceOf[_lender].used == 0) {
             lendingBalanceOf[_lender].total -= _amount;
-            _redeemLending(_amount);
+            _redeemLending(msg.sender, _amount);
         } else if (cEthToken.balanceOf(address(this)) > 0) {
             _findUnusedCTokensAndUse(_amount);
-            _redeemLending(_amount);
+            _redeemLending(msg.sender, _amount);
         } else {
             cEthToken.borrow(_amount);
             borrowingBalanceOf[_lender] += _amount;
             lendingBalanceOf[_lender].total -= _amount;
-            _lender.transfer(_amount);
+            payable(_lender).transfer(_amount);
         }
         delete currentLenders[lenderToIndex[_lender]];
     }
 
     function _redeemCollateral(address _borrower, uint256 _amount) internal {
         require(
-            borrowingBalanceOf[_borrower].used == 0,
+            borrowingBalanceOf[_borrower] == 0,
             "Borrowing must be repaid before redeeming collateral."
         );
         require(_amount <= collateralBalanceOf[msg.sender].total, "");
@@ -153,12 +153,11 @@ contract CompoundModule is Math {
         // The sender does not have any collateral anymore
         lendingBalanceOf[msg.sender].total -= _amount;
         // Finally leech transfers it to the user
-        _lender.transfer(amountRedeemed);
+        payable(_lender).transfer(amountRedeemed);
     }
 
-    function _supplyEthToCompound() internal payable returns (bool) {
-        uint256 result = cEthToken.mint{value: msg.value}();
-        require(result == 0, "Call to Compound failed.");
+    function _supplyEthToCompound(uint256 _amount) internal {
+        cEthToken.mint{value: _amount}(); // Revert on error.
     }
 
     function _supplyDaiToCompound(uint256 _amount) internal {
@@ -198,14 +197,6 @@ contract CompoundModule is Math {
         require(result == 0, "Call to Compound failed.");
     }
 
-    // TODO: can be a public function
-    function _calculateAmountToRepay(address _borrower)
-        internal
-        returns (uint256)
-    {
-        return borrowingBalanceOf[_borrower].used;
-    }
-
     function _findUnusedCTokensAndUse(uint256 _amount) internal {
         uint256 remainingLiquidityToUse = _amount;
         uint256 i;
@@ -216,7 +207,7 @@ contract CompoundModule is Math {
                 lendingBalanceOf[lenderAddress].used;
 
             if (usable > 0) {
-                uint256 amountToUse = min(usable, remainingLiquidityToUse);
+                uint256 amountToUse = Math.min(usable, remainingLiquidityToUse);
                 lendingBalanceOf[lenderAddress].used += amountToUse;
                 remainingLiquidityToUse -= amountToUse;
             }
@@ -234,7 +225,7 @@ contract CompoundModule is Math {
             uint256 unusable = lendingBalanceOf[lenderAddress].used;
 
             if (unusable > 0) {
-                uint256 amountToUnuse = min(
+                uint256 amountToUnuse = Math.min(
                     unusable,
                     remainingLiquidityToUnuse
                 );
