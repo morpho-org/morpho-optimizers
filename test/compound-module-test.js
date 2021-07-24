32171bb6182766c7afd1458299eda79ab75130ab
MerlinEgalite <egalite.merlin@gmail.com>
2021-07-24 23:03:01 +0200

    test: impersonate dai minter

--- test/compound-module-test.js
+++ test/compound-module-test.js
@@ -1,13 +1,17 @@
 const { expect } = require("chai");
+const hre = require("hardhat");
 const { utils, BigNumber } = require('ethers');
+const daiAbi = require('./abis/dai-abi.json');
 
-describe("CompoundModule Contract", () => {
+describe("CompoundModuleETH Contract", () => {
 
   const WETH_ADDRESS = "0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B";
   const CETH_ADDRESS = "0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5";
   const DAI_ADDRESS = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
   const CDAI_ADDRESS = "0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643";
   const COMPOUND_ORACLE_ADDRESS = "0x841616a5CBA946CF415Efe8a326A621A794D0f97";
+
+  const cTokenDecimals = 8; // all cTokens have 8 decimal places
 
   let cEthToken;
   let cDaiToken;
@@ -20,17 +24,42 @@ describe("CompoundModule Contract", () => {
   let borrower;
   let addrs;
 
+  const underlyingToCToken = (amount, exchangeRateCurrent, underlyingDecimals) => {
+    const mantissa = 18 + parseInt(underlyingDecimals) - cTokenDecimals;
+    return amount * BigNumber.from(10).pow(mantissa) / exchangeRateCurrent;
+  }
+
+  const cTokenToUnderlying = (amount, exchangeRateCurrent, underlyingDecimals) => {
+    const mantissa = 18 + parseInt(underlyingDecimals) - cTokenDecimals;
+    return amount * exchangeRateCurrent / BigNumber.from(10).pow(mantissa);
+  }
+
   beforeEach(async () => {
     // CompoundModule
-    CompoundModule = await ethers.getContractFactory("CompoundModule");
-    [owner, lender, borrower, ...addrs] = await ethers.getSigners();
+    CompoundModule = await ethers.getContractFactory("CompoundModuleETH");
+    [lender, borrower, owner, ...addrs] = await ethers.getSigners();
     compoundModule = await CompoundModule.deploy();
     await compoundModule.deployed();
 
-    // CEth
     cEthToken = await ethers.getContractAt("ICEth", CETH_ADDRESS, owner);
     cDaiToken = await ethers.getContractAt("ICErc20", CDAI_ADDRESS, owner);
-    daiToken = await ethers.getContractAt("ICERC20", DAI_ADDRESS, owner);
+    const daiMcdJoin = '0x9759A6Ac90977b93B58547b4A71c78317f391A28';
+    await hre.network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [daiMcdJoin],
+    });
+    const daiSigner = await ethers.getSigner(daiMcdJoin)
+    daiToken = await ethers.getContractAt(daiAbi, DAI_ADDRESS, daiSigner);
+
+    // Address of Join (has auth) https://changelog.makerdao.com/ -> releases -> contract addresses -> MCD_JOIN_DAI
+    const daiAmount = utils.parseUnits("10000");
+    const ethAmount = utils.parseUnits("100");
+    await network.provider.send("hardhat_setBalance", [
+      daiMcdJoin,
+      utils.hexValue(ethAmount),
+    ]);
+    await daiToken.mint(lender.getAddress(), daiAmount, { from: daiMcdJoin });
+    console.log((await daiToken.balanceOf(lender.getAddress())).toString());
   });
 
   describe("Deployment", () => {
@@ -41,35 +70,31 @@ describe("CompoundModule Contract", () => {
     });
   });
 
-
-  // UNITARY TESTS
-
   // lend
   describe("Lend function when there is no borrowers", () => {
     it("Should have correct balances at the beginning", async () => {
-      const ethAmount = utils.parseUnits("1");
-      // expect(await lender.getBalance()).to.equal(ownerBalance);
-      expect((await compoundModule.lendingBalanceOf(owner.getAddress())).onComp).to.equal(0);
-      expect((await compoundModule.lendingBalanceOf(owner.getAddress())).onMorpho).to.equal(0);
+      expect((await compoundModule.lendingBalanceOf(lender.getAddress())).onComp).to.equal(0);
+      expect((await compoundModule.lendingBalanceOf(lender.getAddress())).onMorpho).to.equal(0);
     })
 
-    it("Should not work with amount 0", async () => {
-      const ethAmount = utils.parseUnits("0");
-      await expect(compoundModule.lend({ from: owner.getAddress(), value: ethAmount.toNumber() })).to.be.revertedWith("Amount cannot be 0");
+    it("Should revert with amount 0", async () => {
+      await expect(compoundModule.connect(lender).lend(0)).to.be.revertedWith("Amount cannot be 0");
     })
 
     it("Should have the right amount of cETH in onComp lending balance after", async () => {
-      const ethAmount = utils.parseUnits("1");
-      compoundModule.lend({ from: owner.getAddress(), value: ethAmount });
-      expectedOnCompLendingBalance = await cEthToken.exchangeRateCurrent();
-      console.log("exchange current rate:", expectedOnCompLendingBalance)
-      expect(Number((await compoundModule.lendingBalanceOf(owner.getAddress())).onComp)).to.equal(expectedOnCompLendingBalance);
+      const amount = utils.parseUnits("10");
+      await daiToken.connect(lender).approve(compoundModule.address, 10);
+      await compoundModule.connect(lender).lend(10);
+      console.log(await daiToken.balanceOf(compoundModule.address));
+      exchangeRateCurrent = await cDaiToken.exchangeRateCurrent();
+      // const expectedLendingBalanceOnMorpho = underlyingToCToken(amount, exchangeRateCurrent, 18);
+      // expect(Number((await compoundModule.lendingBalanceOf(lender.getAddress())).onMorpho)).to.equal(expectedLendingBalanceOnMorpho);
     })
 
     it("Should have the right amount of ETH in onMorpho lending balance after", async () => {
       const ethAmount = utils.parseUnits("1");
       compoundModule.lend({ from: owner.getAddress(), value: ethAmount });
-      expectedOnMorphoLendingBalance = 0;
+      const expectedLendingBalanceOnMorpho = 0;
       expect((await compoundModule.lendingBalanceOf(owner.getAddress())).onMorpho).to.equal(expectedOnMorphoLendingBalance);
     })
 
