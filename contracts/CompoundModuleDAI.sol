8a7393c759e4750fa6393640782c1590817119b3
MerlinEgalite <egalite.merlin@gmail.com>
2021-07-29 12:36:28 +0200

    feat: set solc to 0.8.6

--- contracts/CompoundModuleDAI.sol
+++ contracts/CompoundModuleDAI.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.0;
+pragma solidity 0.8.6;
 
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
