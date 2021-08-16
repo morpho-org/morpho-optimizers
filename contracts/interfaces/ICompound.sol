6afbb801f97e671afe35d34dd2fc00cab60cd9ae
MathisGD <delaunay.mathis@hotmail.fr>
2021-08-16 11:42:02 +0200

    Upgrade to solidity 0.8.7

--- contracts/interfaces/ICompound.sol
+++ contracts/interfaces/ICompound.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.8.6;
+pragma solidity 0.8.7;
 
 interface ICErc20 {
     function mint(uint256) external returns (uint256);
--- contracts/interfaces/IOracle.sol
+++ contracts/interfaces/IOracle.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.8.6;
+pragma solidity 0.8.7;
 
 interface IOracle {
     function consult() external view returns (uint256);
