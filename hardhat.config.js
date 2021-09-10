949d8e9fa1df6f6a7d2b592dcc4967248ac043ee
MerlinEgalite <egalite.merlin@gmail.com>
2021-09-10 21:35:44 +0200

    feat: add package and deployer

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,30 +1,35 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 require("dotenv").config({ path: "./.env.local" });
+require("@giry/hardhat-test-solidity");
 require("@nomiclabs/hardhat-waffle");
+require("hardhat-contract-sizer");
 require("hardhat-gas-reporter");
 require("solidity-coverage");
+require("hardhat-deploy");
 
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
       forking: {
-        url: `https://eth-trace.gateway.pokt.network/v1/lb/${process.env.POCKET_NETWORK_ID}`
-        // url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+        // url: `https://eth-trace.gateway.pokt.network/v1/lb/${process.env.POCKET_NETWORK_ID}`
+        url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
       },
-      // accounts: [
-      //   {
-      //     privateKey: "0xae756dcb08a84a119e4910d1ba4dfeb180b0ec5ec4a25223fae2669f36559dd1", // Lender
-      //     balance: "10000000000000000000000"
-      //   },
-      //   {
-      //     privateKey: "0x574028dad40752ed4448624f35ecb32821b0b0791652a34c10aa78053a08a730", // Borrower
-      //     balance: "10000000000000000000000"
-      //   },
-      // ],
+      allowUnlimitedContractSize: true,
       blockNumber: 7710600, // Beginning from a specific block number allows caching data and a faster setup.
+    },
+    kovan: {
+      accounts: ["0xae756dcb08a84a119e4910d1ba4dfeb180b0ec5ec4a25223fae2669f36559dd1"],
+        // },
+        // {
+        //   privateKey: "0x574028dad40752ed4448624f35ecb32821b0b0791652a34c10aa78053a08a730",
+        // },
+      // ],
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+    }
+  },
+  namedAccounts: {
+    deployer: {
+        "kovan": "0x2F25DB0982Fd8E8be238281e4b6c413Eda688637",
     },
   },
   solidity: {
@@ -32,7 +37,7 @@ module.exports = {
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200,
+        runs: 1,
       },
     },
   },
