e0159300d378d57d5b88cf355a2c52a6f60c500a
MerlinEgalite <egalite.merlin@gmail.com>
2021-09-11 16:31:34 +0200

    deploy: add etherscan verifyer

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("dotenv").config({ path: "./.env.local" });
+require("@nomiclabs/hardhat-etherscan");
 require("@giry/hardhat-test-solidity");
 require("@nomiclabs/hardhat-waffle");
 require("hardhat-contract-sizer");
@@ -19,30 +20,20 @@ module.exports = {
     },
     kovan: {
       accounts: ["0xae756dcb08a84a119e4910d1ba4dfeb180b0ec5ec4a25223fae2669f36559dd1"],
-        // },
-        // {
-        //   privateKey: "0x574028dad40752ed4448624f35ecb32821b0b0791652a34c10aa78053a08a730",
-        // },
-      // ],
+      // privateKey: "0x574028dad40752ed4448624f35ecb32821b0b0791652a34c10aa78053a08a730",
       url: `https://kovan.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
     },
     rinkeby: {
       accounts: ["0xae756dcb08a84a119e4910d1ba4dfeb180b0ec5ec4a25223fae2669f36559dd1"],
-        // },
-        // {
-        //   privateKey: "0x574028dad40752ed4448624f35ecb32821b0b0791652a34c10aa78053a08a730",
-        // },
-      // ],
-      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      gas: 2100000,
+      gasPrice: 8000000000
     },
     ropsten: {
       accounts: ["0xae756dcb08a84a119e4910d1ba4dfeb180b0ec5ec4a25223fae2669f36559dd1"],
-        // },
-        // {
-        //   privateKey: "0x574028dad40752ed4448624f35ecb32821b0b0791652a34c10aa78053a08a730",
-        // },
-      // ],
-      url: `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      gas: 2100000,
+      gasPrice: 8000000000
     }
   },
   namedAccounts: {
@@ -58,6 +49,9 @@ module.exports = {
         runs: 200,
       },
     },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
   },
   gasReporter: {
     currency: 'USD',
