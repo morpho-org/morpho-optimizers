33d87c73453d8d9eec2e0671c7d6ff7e3f8e5da6
MathisGD <delaunay.mathis@hotmail.fr>
2021-09-16 12:57:16 +0200

    Solidity tests: fix linter

--- contracts/tests/CompoundModule_Test.sol
+++ contracts/tests/CompoundModule_Test.sol
@@ -3,4 +3,7 @@ pragma solidity 0.8.7;
 
 import "@giry/hardhat-test-solidity/test.sol" as Test;
 
-contract CompoundModule_Test {}
+// solhint-disable-next-line contract-name-camelcase
+contract CompoundModule_Test {
+
+}
