808fd78214ae36e3c5ef03d48d0982bb2ed791b2
MerlinEgalite <egalite.merlin@gmail.com>
2021-09-22 19:37:54 +0200

    refactor: change names

--- contracts/interfaces/ICompPositionsManager.sol
+++ contracts/interfaces/ICompPositionsManager.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.7;
 
-interface ICompoundModule {
+interface ICompPositionsManager {
     function enterMarkets(address[] memory markets) external returns (uint256[] memory);
 
     function setComptroller(address _proxyComptrollerAddress) external;
--- scripts/arguments.js
+++ scripts/arguments.js
@@ -1,4 +1,4 @@
 module.exports = [
-  '0xb7dA3f6BCC140D08cBF89EA271F3B41d69AbAE46', // Morpho
+  '0x441E566CF99b08B9C4973Aa8C2B33593C1808df9', // Morpho
   '0xcfa7b0e37f5ac60f3ae25226f5e39ec59ad26152', // Proxy
 ];
