5bf4cca9cf2fcdc35e63acb862a1de7283a8aac5
MerlinEgalite <egalite.merlin@gmail.com>
2021-09-28 22:24:37 +0200

    simple-volume-sorted::refactor: rename variables and remove unused one

--- contracts/libraries/DoubleLinkedList.sol
+++ contracts/libraries/DoubleLinkedList.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.7;
 
 library DoubleLinkedList {
     struct Account {
-        address add;
+        address id;
         address next;
         address prev;
         uint256 value;
@@ -12,30 +12,29 @@ library DoubleLinkedList {
 
     struct List {
         mapping(address => Account) accounts;
-        mapping(uint256 => address) slotToAddress;
         address head;
         address tail;
         uint256 counter;
     }
 
-    function get(List storage _list, address _add) internal view returns (Account memory account) {
-        return _list.accounts[_add];
+    function get(List storage _list, address _id) internal view returns (Account memory account) {
+        return _list.accounts[_id];
     }
 
-    function getNext(List storage _list, address _add) internal view returns (address) {
-        return _list.accounts[_add].next;
+    function getNext(List storage _list, address _id) internal view returns (address) {
+        return _list.accounts[_id].next;
     }
 
     function addHead(
         List storage _list,
-        address _add,
+        address _id,
         uint256 _value
     ) internal returns (bool) {
-        if (!_contains(_list, _add)) {
-            _createAccount(_list, _add, _value);
-            _link(_list, _add, _list.head);
-            _setHead(_list, _add);
-            if (_list.tail == address(0)) _setTail(_list, _add);
+        if (!_contains(_list, _id)) {
+            _createAccount(_list, _id, _value);
+            _link(_list, _id, _list.head);
+            _setHead(_list, _id);
+            if (_list.tail == address(0)) _setTail(_list, _id);
             return true;
         } else {
             return false;
@@ -44,16 +43,16 @@ library DoubleLinkedList {
 
     function addTail(
         List storage _list,
-        address _add,
+        address _id,
         uint256 _value
     ) internal returns (bool) {
-        if (!_contains(_list, _add)) {
+        if (!_contains(_list, _id)) {
             if (_list.head == address(0)) {
-                addHead(_list, _add, _value);
+                addHead(_list, _id, _value);
             } else {
-                _createAccount(_list, _add, _value);
-                _link(_list, _list.tail, _add);
-                _setTail(_list, _add);
+                _createAccount(_list, _id, _value);
+                _link(_list, _list.tail, _id);
+                _setTail(_list, _id);
             }
             return true;
         } else {
@@ -61,23 +60,23 @@ library DoubleLinkedList {
         }
     }
 
-    function remove(List storage _list, address _add) internal returns (bool) {
-        if (_contains(_list, _add)) {
-            Account memory account = _list.accounts[_add];
-            if (_list.head == _add && _list.tail == _add) {
+    function remove(List storage _list, address _id) internal returns (bool) {
+        if (_contains(_list, _id)) {
+            Account memory account = _list.accounts[_id];
+            if (_list.head == _id && _list.tail == _id) {
                 _setHead(_list, address(0));
                 _setTail(_list, address(0));
-            } else if (_list.head == _add) {
+            } else if (_list.head == _id) {
                 _setHead(_list, account.next);
                 _list.accounts[account.next].prev = address(0);
-            } else if (_list.tail == _add) {
+            } else if (_list.tail == _id) {
                 _setTail(_list, account.prev);
                 _list.accounts[account.prev].next = address(0);
             } else {
                 _link(_list, account.prev, account.next);
             }
             _list.counter -= 1;
-            delete _list.accounts[account.add];
+            delete _list.accounts[account.id];
             return true;
         } else {
             return false;
@@ -86,49 +85,49 @@ library DoubleLinkedList {
 
     function insertSorted(
         List storage _list,
-        address _add,
+        address _id,
         uint256 _value
     ) internal {
-        require(!_contains(_list, _add));
+        require(!_contains(_list, _id));
         address previous = _list.head;
         while (previous != _list.tail && _list.accounts[previous].value >= _value) {
             previous = _list.accounts[previous].next;
         }
-        insertBefore(_list, previous, _add, _value);
+        insertBefore(_list, previous, _id, _value);
     }
 
     function insertBefore(
         List storage _list,
         address _nextId,
-        address _add,
+        address _id,
         uint256 _value
     ) internal {
-        require(!_contains(_list, _add));
-        if (_nextId == _list.head) addHead(_list, _add, _value);
-        else insertAfter(_list, _list.accounts[_nextId].prev, _add, _value);
+        require(!_contains(_list, _id));
+        if (_nextId == _list.head) addHead(_list, _id, _value);
+        else insertAfter(_list, _list.accounts[_nextId].prev, _id, _value);
     }
 
     function insertAfter(
         List storage _list,
         address _prevId,
-        address _add,
+        address _id,
         uint256 _value
     ) internal returns (bool) {
-        require(!_contains(_list, _add));
+        require(!_contains(_list, _id));
         if (_prevId == _list.tail) {
-            return addTail(_list, _add, _value);
+            return addTail(_list, _id, _value);
         } else {
             Account memory prevAccount = _list.accounts[_prevId];
             Account memory nextAccount = _list.accounts[prevAccount.next];
-            _createAccount(_list, _add, _value);
-            _link(_list, _add, nextAccount.add);
-            _link(_list, prevAccount.add, _add);
+            _createAccount(_list, _id, _value);
+            _link(_list, _id, nextAccount.id);
+            _link(_list, prevAccount.id, _id);
             return true;
         }
     }
 
-    function contains(List storage _list, address _add) internal view returns (bool) {
-        return _contains(_list, _add);
+    function contains(List storage _list, address _id) internal view returns (bool) {
+        return _contains(_list, _id);
     }
 
     function length(List storage _list) internal view returns (uint256) {
@@ -143,22 +142,22 @@ library DoubleLinkedList {
         return _list.tail;
     }
 
-    function _setHead(List storage _list, address _add) private {
-        _list.head = _add;
+    function _setHead(List storage _list, address _id) private {
+        _list.head = _id;
     }
 
-    function _setTail(List storage _list, address _add) private {
-        _list.tail = _add;
+    function _setTail(List storage _list, address _id) private {
+        _list.tail = _id;
     }
 
     function _createAccount(
         List storage _list,
-        address _add,
+        address _id,
         uint256 _value
     ) private {
         _list.counter += 1;
-        Account memory account = Account(_add, address(0), address(0), _value, true);
-        _list.accounts[_add] = account;
+        Account memory account = Account(_id, address(0), address(0), _value, true);
+        _list.accounts[_id] = account;
     }
 
     function _link(
@@ -170,8 +169,8 @@ library DoubleLinkedList {
         _list.accounts[_nextId].prev = _prevId;
     }
 
-    function _contains(List storage _list, address _add) private view returns (bool) {
-        return _list.accounts[_add].isIn;
+    function _contains(List storage _list, address _id) private view returns (bool) {
+        return _list.accounts[_id].isIn;
     }
 
     function _length(List storage _list) private view returns (uint256) {
