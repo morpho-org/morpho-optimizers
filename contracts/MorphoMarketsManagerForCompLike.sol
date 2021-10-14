79e6dcfb180825dde454c3c340389531389e1b50
MerlinEgalite <egalite.merlin@gmail.com>
2021-10-14 22:38:58 +0200

    feat: add events

--- contracts/MorphoMarketsManagerForCompLike.sol
+++ contracts/MorphoMarketsManagerForCompLike.sol
@@ -36,6 +36,26 @@ contract MorphoMarketsManagerForCompLike is Ownable {
      */
     event MarketCreated(address _marketAddress);
 
+    /** @dev Emitted when a new market is listed.
+     *  @param _marketAddress The address of the market that has been listed.
+     */
+    event MarketListed(address _marketAddress);
+
+    /** @dev Emitted when a new market is listed.
+     *  @param _marketAddress The address of the market that has been listed.
+     */
+    event MarketDelisted(address _marketAddress);
+
+    /** @dev Emitted when the comptroller is set on the `compLikePositionsManager`.
+     *  @param _comptrollerAddress The address of the comptroller proxy.
+     */
+    event ComptrollerSet(address _comptrollerAddress);
+
+    /** @dev Emitted when the `positionsManagerForCompLike` is set.
+     *  @param _positionsManagerForCompLike The address of the `positionsManagerForCompLike`.
+     */
+    event PositionsManagerForCompLikeSet(address _positionsManagerForCompLike);
+
     /** @dev Emitted when the p2pBPY of a market is updated.
      *  @param _marketAddress The address of the market to update.
      *  @param _newValue The new value of the p2pBPY.
@@ -66,12 +86,16 @@ contract MorphoMarketsManagerForCompLike is Ownable {
     /* External */
 
     /** @dev Sets the `positionsManagerForCompLike` to interact with Compound.
-     *  @param _compLikePositionsManager The address of compound module.
+     *  @param _positionsManagerForCompLike The address of compound module.
      */
-    function setPositionsManagerForCompLike(address _compLikePositionsManager) external onlyOwner {
+    function setPositionsManagerForCompLike(address _positionsManagerForCompLike)
+        external
+        onlyOwner
+    {
         require(!isPositionsManagerSet, "positions-manager-already-set");
         isPositionsManagerSet = true;
-        positionsManagerForCompLike = IPositionsManagerForCompLike(_compLikePositionsManager);
+        positionsManagerForCompLike = IPositionsManagerForCompLike(_positionsManagerForCompLike);
+        emit PositionsManagerForCompLikeSet(_positionsManagerForCompLike);
     }
 
     /** @dev Sets the comptroller address.
@@ -79,6 +103,7 @@ contract MorphoMarketsManagerForCompLike is Ownable {
      */
     function setComptroller(address _proxyComptrollerAddress) external onlyOwner {
         positionsManagerForCompLike.setComptroller(_proxyComptrollerAddress);
+        emit ComptrollerSet(_proxyComptrollerAddress);
     }
 
     /** @dev Creates new market to borrow/supply.
@@ -104,6 +129,7 @@ contract MorphoMarketsManagerForCompLike is Ownable {
      */
     function listMarket(address _marketAddress) external onlyOwner isMarketCreated(_marketAddress) {
         isListed[_marketAddress] = true;
+        emit MarketListed(_marketAddress);
     }
 
     /** @dev Sets a market as unlisted.
@@ -115,6 +141,7 @@ contract MorphoMarketsManagerForCompLike is Ownable {
         isMarketCreated(_marketAddress)
     {
         isListed[_marketAddress] = false;
+        emit MarketDelisted(_marketAddress);
     }
 
     /** @dev Updates thresholds below the ones suppliers and borrowers cannot enter markets.
