3c93b88dc2459318ed641cbda0773d501d581a91
MerlinEgalite <egalite.merlin@gmail.com>
2021-10-17 21:35:21 +0200

    coverage: update script and config dile

--- .solcover.js
+++ .solcover.js
@@ -1,3 +1,9 @@
 module.exports = {
-  skipFiles: ['interfaces/ICompound.sol', 'interfaces/IOracle.sol', 'oracle/ChainlinkOracle.sol'],
+  skipFiles: [
+    'interfaces/compound/ICompound.sol',
+    'interfaces/IMarketsManagerForCompLike.sol',
+    'interfaces/IPositionsManagerForCompLike.sol',
+    'libraries/RedBlackBinaryTree.sol',
+    'test/SimplePriceOracle.sol',
+  ],
 };
