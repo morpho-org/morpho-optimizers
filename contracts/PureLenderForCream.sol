9525a57957d607882ac86a10a211a1bd5fdb68f8
Gabriel <gabriel.astieres@gmail.com>
2021-10-19 14:11:35 +0200

    feat(wip): reward tracking mechanism

---
+++ contracts/PureLenderForCream.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol"; // TODO Not sure to be needed
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol"; // TODO Not sure to be needed
+
+import {ICErc20, IComptroller} from "./interfaces/compound/ICompound.sol";
+import "./interfaces/IPositionsManagerForCompLike.sol";
+import "./interfaces/IMarketsManagerForCompLike.sol";
+
+contract PureLenderForCream is Ownable, ReentrancyGuard {
+    using SafeERC20 for IERC20;
+
+    mapping(address => mapping(address => uint256)) public supplyBalanceInOf; // For a given market, the supply balance of user.
+
+    IMarketsManagerForCompLike marketsManager;
+    IPositionsManagerForCompLike public positionsManager;
+
+    /** @dev Emitted when a supply happens.
+     *  @param _account The address of the supplier.
+     *  @param _crERC20Address The address of the market where assets are supplied into.
+     *  @param _amount The amount of assets.
+     */
+    event Supplied(address indexed _account, address indexed _crERC20Address, uint256 _amount);
+
+    /** @dev Emitted when a withdraw happens.
+     *  @param _account The address of the withdrawer.
+     *  @param _crERC20Address The address of the market from where assets are withdrawn.
+     *  @param _amount The amount of assets.
+     */
+    event Withdrawn(address indexed _account, address indexed _crERC20Address, uint256 _amount);
+    
+    
+    // TODO staking mechanism of Ctokens
+
+    // /** @dev Emitted when a staking happens.
+    //  *  @param _account The address of the staker.
+    //  *  @param _cTokenAddress The address of the cToken.
+    //  *  @param _amount The amount of assets.
+    //  */
+    // event Staked(address indexed _account, address indexed _cTokenAddress, uint256 _amount);
+
+    // /** @dev Emitted when a unstaking happens.
+    //  *  @param _account The address of the unstaker.
+    //  *  @param _cTokenAddress The address of the cToken.
+    //  *  @param _amount The amount of assets.
+    //  */
+    // event Unstaked(address indexed _account, address indexed _cTokenAddress, uint256 _amount);
+    
+
+
+    /** @dev Prevents a user to access a market not created yet.
+     *  @param _crERC20Address The address of the market.
+     */
+    modifier isMarketCreated(address _crERC20Address) {
+        require(marketsManager.isCreated(_crERC20Address), "mkt-not-created");
+        _;
+    }
+
+    constructor(address _morphoPositionsManagerForCream) {
+        positionsManager = IPositionsManagerForCompLike(_morphoPositionsManagerForCream);
+        marketsManager = IMarketsManagerForCompLike(positionsManager.marketsManagerForCompLike());
+    }
+
+    // User must approve _amount for contract
+    // No need to check isMarketCreated, done by supply
+    function supply(address _ERC20Address, uint256 _amount)
+        external
+        nonReentrant
+    {
+        require(_amount > 0, "supply:amount=0");
+        IERC20 erc20Token = IERC20(_ERC20Address);
+        erc20Token.safeApprove(msg.sender, address(this), _amount);
+
+        positionsManager.supply(_ERC20Address, _amount); // TODO supply must be called with crToken address !!!!!
+
+        supplyBalanceInOf[_ERC20Address][msg.sender] += _amount;
+
+        emit Supplied(msg.sender, _ERC20Address, _amount);
+    }
+
+    function withdraw(address _ERC20Address, uint256 _amount) external nonReentrant {
+        require(_amount > 0, "withdraw:amount=0");
+        require(_amount <= supplyBalanceInOf[_ERC20Address][msg.sender], "withdraw:amount>balance");
+
+        IERC20 erc20Token = ICErc20(_ERC20Address);
+        positionsManager.withdraw(_ERC20Address, _amount); // TODO withdraw must be called with crToken address !!!!!
+
+        supplyBalanceInOf[_ERC20Address][msg.sender] -= _amount;
+        erc20Token.safeTransfer(msg.sender, _amount);
+
+        emit Withdrawn(msg.sender, _ERC20Address, _amount);
+    }
+
+
+    // TODO staking mechanism of Ctokens
+
+    // // User must approve _amount for contract
+    // function stake(address _crERC20Address, uint256 _amount) external nonReentrant {
+    //     require(_amount > 0, "stake:amount=0");
+
+    //     ICErc20 crERC20Token = ICErc20(_crERC20Address);
+    //     crERC20Token.safeTransferFrom(msg.sender, address(this), _amount);
+
+    //     IERC20 erc20Token = IERC20(crERC20Token.underlying());
+
+    //     // TODO remove from cream and supply ?
+    // }
+
+    // function unstake(address _crERC20Address, uint256 _amount) external nonReentrant {}
+
+
+}
---
+++ test/pureLenderForCream-test.js
@@ -0,0 +1,83 @@
+require('dotenv').config({ path: '.env.local' });
+const { utils, BigNumber } = require('ethers');
+const { ethers } = require('hardhat');
+const { expect } = require('chai');
+const hre = require('hardhat');
+const config = require('@config/polygon-config.json').polygon;
+
+
+
+describe('MorphoPositionsManagerForCream Contract', () => {
+    let cUsdcToken;
+    let cDaiToken;
+    let cUsdtToken;
+    let cMkrToken;
+    let daiToken;
+    let usdtToken;
+    let uniToken;
+    let MorphoPositionsManagerForCream;
+    let morphoPositionsManagerForCream;
+    let fakeCreamPositionsManager;
+  
+    let signers;
+    let owner;
+    let supplier1;
+    let supplier2;
+    let supplier3;
+    let borrower1;
+    let borrower2;
+    let borrower3;
+    let liquidator;
+    let addrs;
+    let suppliers;
+    let borrowers;
+  
+    let underlyingThreshold;
+  
+    beforeEach(async () => {
+      // Users
+      signers = await ethers.getSigners();
+      [owner, supplier1, supplier2, supplier3, borrower1, borrower2, borrower3, liquidator, ...addrs] = signers;
+      suppliers = [supplier1, supplier2, supplier3];
+      borrowers = [borrower1, borrower2, borrower3];
+  
+      // Deploy contracts
+      PureLenderForCream = await ethers.getContractFactory('PureLenderForCream');
+      PureLenderForCream = await PureLenderForCream.deploy();
+      await PureLenderForCream.deployed();
+  
+      // Get contract dependencies
+      const cTokenAbi = require(config.tokens.cToken.abi);
+      cUsdcToken = await ethers.getContractAt(cTokenAbi, config.tokens.cUsdc.address, owner);
+      cDaiToken = await ethers.getContractAt(cTokenAbi, config.tokens.cDai.address, owner);
+      cUsdtToken = await ethers.getContractAt(cTokenAbi, config.tokens.cUsdt.address, owner);
+      cUniToken = await ethers.getContractAt(cTokenAbi, config.tokens.cUni.address, owner);
+      cMkrToken = await ethers.getContractAt(cTokenAbi, config.tokens.cMkr.address, owner); // This is in fact crLINK tokens (no crMKR on Polygon)
+  
+      comptroller = await ethers.getContractAt(require(config.cream.comptroller.abi), config.cream.comptroller.address, owner);
+      compoundOracle = await ethers.getContractAt(require(config.cream.oracle.abi), comptroller.oracle(), owner);
+  
+      // Mint some ERC20
+      daiToken = await getTokens('0x27f8d03b3a2196956ed754badc28d73be8830a6e', 'whale', signers, config.tokens.dai, utils.parseUnits('10000'));
+      usdcToken = await getTokens('0x1a13f4ca1d028320a707d99520abfefca3998b7f', 'whale', signers, config.tokens.usdc, BigNumber.from(10).pow(10));
+      usdtToken = await getTokens('0x44aaa9ebafb4557605de574d5e968589dc3a84d1', 'whale', signers, config.tokens.usdt, BigNumber.from(10).pow(10));
+      uniToken = await getTokens('0xf7135272a5584eb116f5a77425118a8b4a2ddfdb', 'whale', signers, config.tokens.uni, utils.parseUnits('100'));
+  
+      underlyingThreshold = utils.parseUnits('1');
+  
+      // Create and list markets
+      await morphoMarketsManagerForCompLike.connect(owner).setPositionsManagerForCompLike(morphoPositionsManagerForCream.address);
+      await morphoMarketsManagerForCompLike.connect(owner).createMarket(config.tokens.cDai.address);
+      await morphoMarketsManagerForCompLike.connect(owner).createMarket(config.tokens.cUsdc.address);
+      await morphoMarketsManagerForCompLike.connect(owner).createMarket(config.tokens.cUni.address);
+      await morphoMarketsManagerForCompLike.connect(owner).createMarket(config.tokens.cUsdt.address);
+      await morphoMarketsManagerForCompLike.connect(owner).updateThreshold(config.tokens.cUsdc.address, BigNumber.from(1).pow(6));
+      await morphoMarketsManagerForCompLike.connect(owner).updateThreshold(config.tokens.cUsdt.address, BigNumber.from(1).pow(6));
+    });
+});
+
+describe('Deployment', () => {
+    it('Should deploy the contract with the right values', async () => {
+
+    });
+  });
