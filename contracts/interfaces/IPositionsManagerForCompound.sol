5522332c89d458f2f48bf19c4216be366b3dac50
npasquie <nicpas@live.fr>
2021-11-19 13:06:03 +0100

    refactor(threshold): remove ability to change comptroller

--- contracts/interfaces/IPositionsManagerForCompound.sol
+++ contracts/interfaces/IPositionsManagerForCompound.sol
@@ -4,8 +4,6 @@ pragma solidity 0.8.7;
 interface IPositionsManagerForCompound {
     function createMarket(address _marketAddress) external returns (uint256[] memory);
 
-    function setComptroller(address _proxyComptrollerAddress) external;
-
     function setMaxNumberOfUsersInTree(uint16) external;
 
     function setThreshold(address, uint256) external;
