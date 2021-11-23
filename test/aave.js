3ac3db971b15acf23fa6244cbdeeebf9e0965193
MerlinEgalite <egalite.merlin@gmail.com>
2021-11-23 22:13:39 +0100

    feat(aave-module): implement claim rewards

--- test/aave.js
+++ test/aave.js
@@ -29,6 +29,7 @@ describe('PositionsManagerForAave Contract', () => {
   let daiToken;
   let usdtToken;
   let wbtcToken;
+  let meticToken;
   let PositionsManagerForAave;
   let positionsManagerForAave;
   let marketsManagerForAave;
