9ea95a8f7e00563e3c69c8e293a136326f77cef3
MathisGD <delaunay.mathis@hotmail.fr>
2021-12-01 15:55:13 +0100

    aave-fifo: merge position storage

--- contracts/aave/PositionsManagerForAave.sol
+++ contracts/aave/PositionsManagerForAave.sol
@@ -9,17 +9,21 @@ import "@openzeppelin/contracts/utils/math/Math.sol";
 import "./libraries/DoubleLinkedList.sol";
 import "./libraries/aave/WadRayMath.sol";
 import "./libraries/ErrorsForAave.sol";
+import "./libraries/DoubleLinkedList.sol";
 import "./interfaces/aave/IPriceOracleGetter.sol";
 import {IVariableDebtToken} from "./interfaces/aave/IVariableDebtToken.sol";
 import {IAToken} from "./interfaces/aave/IAToken.sol";
 import "./interfaces/aave/IAaveIncentivesController.sol";
-import "./PositionsManagerStorageForAave.sol";
+import "./interfaces/aave/ILendingPoolAddressesProvider.sol";
+import "./interfaces/aave/IProtocolDataProvider.sol";
+import "./interfaces/aave/ILendingPool.sol";
+import "./interfaces/IMarketsManagerForAave.sol";
 
 /**
  *  @title PositionsManagerForAave
  *  @dev Smart contract interacting with Aave to enable P2P supply/borrow positions that can fallback on Aave's pool using poolToken tokens.
  */
-contract PositionsManagerForAave is ReentrancyGuard, PositionsManagerStorageForAave {
+contract PositionsManagerForAave is ReentrancyGuard {
     using DoubleLinkedList for DoubleLinkedList.List;
     using WadRayMath for uint256;
     using SafeMath for uint256;
@@ -67,6 +71,40 @@ contract PositionsManagerForAave is ReentrancyGuard, PositionsManagerStorageForA
         address tokenCollateralAddress; // The address of the collateral asset.
         IPriceOracleGetter oracle; // Aave oracle.
     }
+
+    /* Structs */
+
+    struct SupplyBalance {
+        uint256 inP2P; // In p2pUnit, a unit that grows in value, to keep track of the interests/debt increase when users are in p2p.
+        uint256 onPool; // In aToken.
+    }
+
+    struct BorrowBalance {
+        uint256 inP2P; // In p2pUnit.
+        uint256 onPool; // In adUnit, a unit that grows in value, to keep track of the debt increase when users are in Aave. Multiply by current borrowIndex to get the underlying amount.
+    }
+
+    /* Storage */
+
+    uint16 public NMAX = 1000;
+    uint256 public constant LIQUIDATION_CLOSE_FACTOR_PERCENT = 5000; // In basis points.
+    bytes32 public constant DATA_PROVIDER_ID =
+        0x1000000000000000000000000000000000000000000000000000000000000000; // Id of the data provider.
+    mapping(address => DoubleLinkedList.List) internal suppliersInP2P; // Suppliers in peer-to-peer.
+    mapping(address => DoubleLinkedList.List) internal suppliersOnPool; // Suppliers on Aave.
+    mapping(address => DoubleLinkedList.List) internal borrowersInP2P; // Borrowers in peer-to-peer.
+    mapping(address => DoubleLinkedList.List) internal borrowersOnPool; // Borrowers on Aave.
+    mapping(address => mapping(address => SupplyBalance)) public supplyBalanceInOf; // For a given market, the supply balance of user.
+    mapping(address => mapping(address => BorrowBalance)) public borrowBalanceInOf; // For a given market, the borrow balance of user.
+    mapping(address => mapping(address => bool)) public accountMembership; // Whether the account is in the market or not.
+    mapping(address => address[]) public enteredMarkets; // Markets entered by a user.
+    mapping(address => uint256) public threshold; // Thresholds below the ones suppliers and borrowers cannot enter markets.
+    mapping(address => uint256) public capValue; // Caps above the ones suppliers cannot add more liquidity.
+
+    IMarketsManagerForAave public marketsManagerForAave;
+    ILendingPoolAddressesProvider public addressesProvider;
+    IProtocolDataProvider public dataProvider;
+    ILendingPool public lendingPool;
 
     /* Events */
 
