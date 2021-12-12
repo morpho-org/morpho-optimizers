e74862112abbfe637055cb90d8743d8a0e37153a
MerlinEgalite <egalite.merlin@gmail.com>
2021-12-12 19:38:50 +0100

    fix(dll): fix wrong insertion + add getters

--- contracts/common/libraries/DoubleLinkedList.sol
+++ contracts/common/libraries/DoubleLinkedList.sol
@@ -17,16 +17,50 @@ library DoubleLinkedList {
     /** @dev Returns the `account` linked to `_id`.
      *  @param _list The list to search in.
      *  @param _id The address of the account.
-     *  @return value The value of the account.
+     *  @return The value of the account.
      */
     function getValueOf(List storage _list, address _id) internal view returns (uint256) {
         return _list.accounts[_id].value;
     }
 
+    /** @dev Returns the address at the head of the `_list`.
+     *  @param _list The list to get the head.
+     *  @return The address of the head.
+     */
+    function getHead(List storage _list) internal view returns (address) {
+        return _list.head;
+    }
+
+    /** @dev Returns the address at the tail of the `_list`.
+     *  @param _list The list to get the tail.
+     *  @return The address of the tail.
+     */
+    function getTail(List storage _list) internal view returns (address) {
+        return _list.tail;
+    }
+
+    /** @dev Returns the next id address from the current `_id`.
+     *  @param _list The list to search in.
+     *  @param _id The address of the account.
+     *  @return The address of the next account.
+     */
+    function getNext(List storage _list, address _id) internal view returns (address) {
+        return _list.accounts[_id].next;
+    }
+
+    /** @dev Returns the previous id address from the current `_id`.
+     *  @param _list The list to search in.
+     *  @param _id The address of the account.
+     *  @return The address of the previous account.
+     */
+    function getPrev(List storage _list, address _id) internal view returns (address) {
+        return _list.accounts[_id].prev;
+    }
+
     /** @dev Removes an account of the `_list`.
      *  @param _list The list to search in.
      *  @param _id The address of the account.
-     *  @return bool Whether the account has been removed or not.
+     *  @return Whether the account has been removed or not.
      */
     function remove(List storage _list, address _id) internal returns (bool) {
         if (_list.accounts[_id].value != 0) {
@@ -71,12 +105,10 @@ library DoubleLinkedList {
 
         address nextId;
         address prevId;
-        if (numberOfIterations <= _maxIterations && current != _list.tail) {
+        if (numberOfIterations < _maxIterations && current != _list.tail) {
             prevId = _list.accounts[current].prev;
             nextId = current;
-        } else {
-            prevId = _list.tail;
-        }
+        } else prevId = _list.tail;
 
         _list.accounts[_id] = Account(prevId, nextId, _value);
 
@@ -84,13 +116,5 @@ library DoubleLinkedList {
         else _list.head = _id;
         if (nextId != address(0)) _list.accounts[nextId].prev = _id;
         else _list.tail = _id;
-    }
-
-    /** @dev Returns the address at the head of the `_list`.
-     *  @param _list The list to get the head.
-     *  @return The address.
-     */
-    function getHead(List storage _list) internal view returns (address) {
-        return _list.head;
     }
 }
