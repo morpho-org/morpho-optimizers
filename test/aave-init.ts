9880de734f513a5cbb2eb15e6c488006b03a10a1
Gabriel <gabriel.astieres@gmail.com>
2021-12-16 12:56:34 +0400

    feat: give DAI to owner

--- test/aave-init.ts
+++ test/aave-init.ts
@@ -1,9 +1,9 @@
 import * as dotenv from 'dotenv';
 dotenv.config({ path: './.env.local' });
-import { BigNumber, Contract, Signer } from 'ethers';
+import { utils, BigNumber, Contract, Signer } from 'ethers';
 import hre, { ethers } from 'hardhat';
 const config = require(`@config/${process.env.NETWORK}-config.json`);
-import { MAX_INT, to6Decimals } from './utils/common-helpers';
+import { MAX_INT, to6Decimals, getTokens } from './utils/common-helpers';
 
 import { WAD } from './utils/aave-helpers';
 
@@ -13,9 +13,10 @@ import { WAD } from './utils/aave-helpers';
 
 // Tokens
 let aDaiToken: Contract;
-let aUsdcToken: Contract;
 let daiToken: Contract;
 let usdcToken: Contract;
+let wbtcToken: Contract;
+let wmaticToken: Contract;
 let variableDebtDaiToken: Contract;
 
 // Contracts
@@ -28,6 +29,20 @@ let protocolDataProvider: Contract;
 let oracle: Contract;
 
 let owner: Signer;
+
+const giveTokensTo = async (token: string, receiver: string, amount: BigNumber, slotPosition: number): Promise<void> => {
+  // Get storage slot index
+  let index = ethers.utils.solidityKeccak256(
+    ['uint256', 'uint256'],
+    [receiver, slotPosition] // key, slot
+  );
+  await setStorageAt(token, index, ethers.utils.hexlify(ethers.utils.zeroPad(amount.toHexString(), 32)));
+};
+
+const setStorageAt = async (address: string, index: string, value: string): Promise<void> => {
+  await hre.network.provider.send('hardhat_setStorageAt', [address, index, value]);
+  await hre.network.provider.send('evm_mine', []); // Just mines to the next block
+};
 
 describe('Create a local fork', () => {
   it('Start init', async () => {
@@ -56,7 +71,6 @@ describe('Create a local fork', () => {
     const aTokenAbi = require(config.tokens.aToken.abi);
     const variableDebtTokenAbi = require(config.tokens.variableDebtToken.abi);
     aDaiToken = await ethers.getContractAt(aTokenAbi, config.tokens.aDai.address, owner);
-    aUsdcToken = await ethers.getContractAt(aTokenAbi, config.tokens.aUsdc.address, owner);
     variableDebtDaiToken = await ethers.getContractAt(variableDebtTokenAbi, config.tokens.variableDebtDai.address, owner);
     lendingPool = await ethers.getContractAt(require(config.aave.lendingPool.abi), config.aave.lendingPool.address, owner);
     lendingPoolAddressesProvider = await ethers.getContractAt(
@@ -74,11 +88,20 @@ describe('Create a local fork', () => {
     // Mint some tokens
     daiToken = await ethers.getContractAt(require(config.tokens.dai.abi), config.tokens.dai.address, owner);
     usdcToken = await ethers.getContractAt(require(config.tokens.usdc.abi), config.tokens.usdc.address, owner);
+    wbtcToken = await ethers.getContractAt(require(config.tokens.wbtc.abi), config.tokens.dai.address, owner);
+    wmaticToken = await ethers.getContractAt(require(config.tokens.wmatic.abi), config.tokens.usdc.address, owner);
 
     // Create and list markets
     await marketsManagerForAave.connect(owner).setPositionsManager(positionsManagerForAave.address);
     await marketsManagerForAave.connect(owner).setLendingPool();
     await marketsManagerForAave.connect(owner).createMarket(config.tokens.aDai.address, WAD, MAX_INT);
     await marketsManagerForAave.connect(owner).createMarket(config.tokens.aUsdc.address, to6Decimals(WAD), MAX_INT);
+    await marketsManagerForAave.connect(owner).createMarket(config.tokens.aWbtc.address, BigNumber.from(10).pow(4), MAX_INT);
+    await marketsManagerForAave.connect(owner).createMarket(config.tokens.aUsdt.address, to6Decimals(WAD), MAX_INT);
+    await marketsManagerForAave.connect(owner).createMarket(config.tokens.aWmatic.address, WAD, MAX_INT);
+  });
+
+  it('Owner uses Morpho so that AAVE is saved on disk', async () => {
+    await giveTokensTo(daiToken.address, await owner.getAddress(), utils.parseUnits('10000'), 0);
   });
 });
