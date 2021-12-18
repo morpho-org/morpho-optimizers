526d1b9ea530f17a8e97e75b887780885f9714c0
MerlinEgalite <egalite.merlin@gmail.com>
2021-12-18 16:51:27 +0100

    ci: fix test

--- test/aave.ts
+++ test/aave.ts
@@ -1226,10 +1226,10 @@ describe('PositionsManagerForAave Contract', () => {
       expect((await positionsManagerForAave.borrowBalanceInOf(config.tokens.aDai.address, borrower1.getAddress())).onPool).to.equal(0);
       expect(
         removeDigitsBigNumber(
-          2,
+          3,
           (await positionsManagerForAave.borrowBalanceInOf(config.tokens.aDai.address, borrower1.getAddress())).inP2P
         )
-      ).to.equal(removeDigitsBigNumber(2, expectedBorrowBalanceInP2PAfter));
+      ).to.equal(removeDigitsBigNumber(3, expectedBorrowBalanceInP2PAfter));
 
       // Check liquidator balances
       let diff;
