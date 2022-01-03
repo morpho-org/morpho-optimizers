4b84fbc075203f9eebc642a9a47559a9a05e0fa8
Philippe GONDAY <philippe@gonday.com>
2022-01-03 19:58:42 +0100

    feat: add more borrow test

--- contracts/aave/test/BorrowTest.t.sol
+++ contracts/aave/test/BorrowTest.t.sol
@@ -16,8 +16,15 @@ import "./User.sol";
 import "./Attacker.sol";
 
 contract BorrowTest is TestSetup {
-    // 2.1 - The borrower tries to borrow more than his collateral allows, the transaction reverts.
-    function testFail_borrow_2_1() public {
+    // 2.1 - The user borrows less than the threshold of the given market, the transaction reverts.
+    function testFail_2_1() public {
+        uint256 amount = to6Decimals(positionsManager.threshold(aDai) - 1);
+        borrower1.approve(dai, amount);
+        borrower1.borrow(aDai, amount);
+    }
+
+    // 2.2 - The borrower tries to borrow more than his collateral allows, the transaction reverts.
+    function testFail_borrow_2_2() public {
         uint256 amount = 10 ether;
 
         borrower1.approve(usdc, amount);
@@ -32,8 +39,8 @@ contract BorrowTest is TestSetup {
         borrower1.borrow(aDai, maxToBorrow + 1);
     }
 
-    // 2.2 - There are no available suppliers: all of the borrowed amount is onPool.
-    function test_borrow_2_2() public {
+    // 2.3 - There are no available suppliers: all of the borrowed amount is onPool.
+    function test_borrow_2_3() public {
         uint256 amount = 10 ether;
 
         borrower1.approve(usdc, amount);
@@ -49,8 +56,8 @@ contract BorrowTest is TestSetup {
         assertGt(onPool, 0);
     }
 
-    // 2.3 - There is 1 available supplier, he matches 100% of the borrower liquidity, everything is inP2P.
-    function test_borrow_2_3() public {
+    // 2.4 - There is 1 available supplier, he matches 100% of the borrower liquidity, everything is inP2P.
+    function test_borrow_2_4() public {
         uint256 amount = 10 ether;
 
         supplier1.approve(dai, amount);
@@ -60,17 +67,20 @@ contract BorrowTest is TestSetup {
         borrower1.supply(aUsdc, amount);
         borrower1.borrow(aDai, amount / 2);
 
+        (uint256 supplyInP2P, ) = positionsManager.supplyBalanceInOf(aDai, address(supplier1));
+        assertGt(supplyInP2P, 0);
+
         (uint256 inP2P, uint256 onPool) = positionsManager.borrowBalanceInOf(
             aDai,
             address(borrower1)
         );
-        assertGt(inP2P, 0);
+        assertEq(inP2P, supplyInP2P);
         assertEq(onPool, 0);
     }
 
-    // 2.4 - There is 1 available supplier, he doesn't match 100% of the borrower liquidity.
+    // 2.5 - There is 1 available supplier, he doesn't match 100% of the borrower liquidity.
     // Borrower inP2P is equal to the supplier previous amount onPool, the rest is set onPool.
-    function test_borrow_2_4() public {
+    function test_borrow_2_5() public {
         uint256 amount = 2 ether;
         supplier1.approve(dai, amount);
         supplier1.supply(aDai, amount);
@@ -91,8 +101,8 @@ contract BorrowTest is TestSetup {
         assertGt(onPool, 0);
     }
 
-    // 2.5 - There are NMAX (or less) supplier that match the borrowed amount, everything is inP2P after NMAX (or less) match.
-    function test_borrow_2_5() public {
+    // 2.6 - There are NMAX (or less) supplier that match the borrowed amount, everything is inP2P after NMAX (or less) match.
+    function test_borrow_2_6() public {
         uint256 amount = 5 ether;
         uint256 totalAmount = 0;
 
@@ -123,9 +133,9 @@ contract BorrowTest is TestSetup {
         assertEq(onPool, 0);
     }
 
-    // 2.6 - The NMAX biggest supplier don't match all of the borrowed amount, after NMAX match, the rest is borrowed and set onPool.
+    // 2.7 - The NMAX biggest supplier don't match all of the borrowed amount, after NMAX match, the rest is borrowed and set onPool.
     // ⚠️ most gas expensive borrow scenario.
-    function test_borrow_2_6() public {
+    function test_borrow_2_7() public {
         uint256 amount = 5 ether;
         uint256 totalAmount = 0;
 
