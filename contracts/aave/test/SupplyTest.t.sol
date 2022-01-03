6415166fa7cc1240a4f0a0cdadf58a1ba1f6bc08
Gabriel <gabriel.astieres@gmail.com>
2022-01-03 13:12:37 +0100

    feat: organize test files per function category

---
+++ contracts/aave/test/SupplyTest.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GNU AGPLv3
+pragma solidity 0.8.7;
+
+import "ds-test/test.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import "../PositionsManagerForAave.sol";
+import "../MarketsManagerForAave.sol";
+import "./TestSetup.sol";
+
+import "@config/Config.sol";
+import "./HEVM.sol";
+import "./Utils.sol";
+import "./SimplePriceOracle.sol";
+import "./User.sol";
+import "./Attacker.sol";
+
+contract SupplyTest is TestSetup {}
