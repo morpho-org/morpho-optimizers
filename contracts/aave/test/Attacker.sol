444c8ed7f0f70a7694321fbf4f1b31436a18a913
Philippe <philippe@gonday.com>
2022-01-03 13:44:54 +0100

    feat: dapptools config and tests
    feat: add lib as submodules
    
    feat: correct some tests issues
    
    feat: rename test dir with JS tests for Makefile issue
    
    feat: rename test dir with JS tests for Makefile issue
    
    feat: rename test dir with JS tests for Makefile issue

---
+++ contracts/aave/test/Attacker.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GNU AGPLv3
+pragma solidity 0.8.7;
+
+import "../PositionsManagerForAave.sol";
+import "../MarketsManagerForAave.sol";
+
+contract Attacker {
+    ILendingPool internal lendingPool;
+
+    constructor(ILendingPool _lendingPool) {
+        lendingPool = _lendingPool;
+    }
+
+    receive() external payable {}
+
+    function approve(
+        address _token,
+        address _spender,
+        uint256 _amount
+    ) external {
+        IERC20(_token).approve(_spender, _amount);
+    }
+
+    function transfer(
+        address _token,
+        address _recipient,
+        uint256 _amount
+    ) external {
+        IERC20(_token).transfer(_recipient, _amount);
+    }
+
+    function deposit(
+        address _asset,
+        uint256 _amount,
+        address _onBehalfOf,
+        uint16 _referralCode
+    ) external {
+        lendingPool.deposit(_asset, _amount, _onBehalfOf, _referralCode);
+    }
+}
