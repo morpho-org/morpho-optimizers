2568e27f9fd9074557aebf17714719490ad43863
julien <julien.thomas84@gmail.com>
2022-01-18 15:04:46 -0500

    refacto: separate test contracts to functionnal contracts

--- test-foundry/TestBorrow.t.sol
+++ test-foundry/TestBorrow.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GNU AGPLv3
 pragma solidity 0.8.7;
 
-import "contracts/aave/libraries/aave/WadRayMath.sol";
+import "@contracts/aave/libraries/aave/WadRayMath.sol";
 
 import "./utils/TestSetup.sol";
 
