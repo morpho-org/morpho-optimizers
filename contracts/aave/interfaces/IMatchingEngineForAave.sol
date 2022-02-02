0eebe44208267cddff7818899ae4910e7d25fa3a
MathisGD <mathis.gontier_delaunay@telecom-sudparis.eu>
2022-02-02 13:18:28 +0100

    refactor(readability): split positionManager

---
+++ contracts/aave/interfaces/IMatchingEngineForAave.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GNU AGPLv3
+pragma solidity 0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IAToken} from "./aave/IAToken.sol";
+
+interface IMatchingEngineForAave {
+    function matchSuppliers(
+        IAToken,
+        IERC20,
+        uint256
+    ) external returns (uint256);
+
+    function unmatchSuppliers(address, uint256) external;
+
+    function matchBorrowers(
+        IAToken,
+        IERC20,
+        uint256
+    ) external returns (uint256);
+
+    function unmatchBorrowers(address, uint256) external;
+
+    function updateBorrowers(address _poolTokenAddress, address _account) external;
+
+    function updateSuppliers(address _poolTokenAddress, address _account) external;
+}
--- contracts/aave/interfaces/IMatchingEngineManager.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GNU AGPLv3
-pragma solidity 0.8.7;
-
-interface IMatchingEngineManager {
-    function updateBorrowers(address _poolTokenAddress, address _account) external;
-
-    function updateSuppliers(address _poolTokenAddress, address _account) external;
-}
