803c1f2a1e6ade87a1f82f81151d173059064af7
gabriel <gabriel.astieres@gmail.com>
2022-02-03 15:38:04 +0100

    improve: remove cap-value

--- test-foundry/TestGovernance.t.sol
+++ test-foundry/TestGovernance.t.sol
@@ -30,20 +30,20 @@ contract TestGovernance is TestSetup {
     // Should revert when the function is called with an improper market
     function test_revert_on_not_real_market() public {
         hevm.expectRevert("");
-        marketsManager.createMarket(usdt, WAD, type(uint256).max);
+        marketsManager.createMarket(usdt, WAD);
     }
 
     // Only Owner should be able to create markets in peer-to-peer
     function test_only_owner_can_create_markets_1() public {
         for (uint256 i = 0; i < pools.length; i++) {
             hevm.expectRevert("Ownable: caller is not the owner");
-            supplier1.createMarket(pools[i], WAD, type(uint256).max);
+            supplier1.createMarket(pools[i], WAD);
 
             hevm.expectRevert("Ownable: caller is not the owner");
-            borrower1.createMarket(pools[i], WAD, type(uint256).max);
+            borrower1.createMarket(pools[i], WAD);
         }
 
-        marketsManager.createMarket(aWeth, WAD, type(uint256).max);
+        marketsManager.createMarket(aWeth, WAD);
     }
 
     // marketsManagerForAave should not be changed after already set by Owner
@@ -52,25 +52,13 @@ contract TestGovernance is TestSetup {
         marketsManager.setPositionsManager(address(fakePositionsManager));
     }
 
-    // Only Owner should be able to set cap value
-    function test_only_owner_can_set_cap_value() public {
-        uint256 newCapValue = 2 * 1e18;
-        marketsManager.setCapValue(aUsdc, newCapValue);
-
-        hevm.expectRevert("Ownable: caller is not the owner");
-        supplier1.setCapValue(aUsdc, newCapValue);
-
-        hevm.expectRevert("Ownable: caller is not the owner");
-        borrower1.setCapValue(aUsdc, newCapValue);
-    }
-
     // Should create a market the with right values
     function test_create_market_with_right_values() public {
         DataTypes.ReserveData memory data = lendingPool.getReserveData(aave);
         uint256 expectedSPY = (data.currentLiquidityRate + data.currentVariableBorrowRate) /
             2 /
             SECOND_PER_YEAR;
-        marketsManager.createMarket(aAave, WAD, type(uint256).max);
+        marketsManager.createMarket(aAave, WAD);
 
         assertTrue(marketsManager.isCreated(aAave));
         assertEq(marketsManager.supplyP2PSPY(aAave), expectedSPY);
