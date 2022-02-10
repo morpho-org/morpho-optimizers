3b7003f0677ce22692537fe366231fc367b35747
MerlinEgalite <egalite.merlin@gmail.com>
2022-02-10 22:34:42 +0100

    ✨ (modular-iterations) add possibility to adapt the number of iterations

--- contracts/aave/PositionsManagerForAaveStorage.sol
+++ contracts/aave/PositionsManagerForAaveStorage.sol
@@ -37,11 +37,13 @@ contract PositionsManagerForAaveStorage is ReentrancyGuard, Pausable {
 
     /// Storage ///
 
-    uint8 public NMAX = 20;
+    uint8 public NDS = 20; // Max number of iterations in data structure sorting process.
+    uint8 public NMAX = 20; // Max number of iterations in repay and withdraw functions.
     uint8 public constant NO_REFERRAL_CODE = 0;
     uint8 public constant VARIABLE_INTEREST_MODE = 2;
     uint16 public constant MAX_BASIS_POINTS = 10000; // 100% in basis points.
     uint16 public constant LIQUIDATION_CLOSE_FACTOR_PERCENT = 5000; // 50% in basis points.
+    uint32 public MIN_GAS_LEFT = 300e3;
     bytes32 public constant DATA_PROVIDER_ID =
         0x1000000000000000000000000000000000000000000000000000000000000000; // Id of the data provider.
     mapping(address => DoubleLinkedList.List) internal suppliersInP2P; // Suppliers in peer-to-peer.
