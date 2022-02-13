7348e696153f2b2e1eff8c3ee6b2bb982e34ef84
MerlinEgalite <egalite.merlin@gmail.com>
2022-02-13 15:51:57 +0100

    🔥 remove hardhat tests for clarity

--- test-foundry/utils/TestDoubleLinkedList.sol
+++ test-foundry/utils/TestDoubleLinkedList.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.7;
 
-import "../common/libraries/DoubleLinkedList.sol";
+import "@contracts/common/libraries/DoubleLinkedList.sol";
 
 contract TestDoubleLinkedList {
     using DoubleLinkedList for DoubleLinkedList.List;
