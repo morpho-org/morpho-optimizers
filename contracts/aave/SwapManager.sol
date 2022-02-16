c5e16754c5cbb9e88149e3c7a170da16b843fa2c
MerlinEgalite <egalite.merlin@gmail.com>
2022-02-16 14:25:27 +0100

    ⚡️ (claim-and-swap) various improvements

--- contracts/aave/SwapManager.sol
+++ contracts/aave/SwapManager.sol
@@ -21,20 +21,26 @@ contract SwapManager is ISwapManager {
     uint24 public constant POOL_FEE = 3000; // Fee on Uniswap.
     uint256 public constant ONE_PERCENT = 100; // 1% in basis points.
     uint256 public constant MAX_BASIS_POINTS = 10000; // 100% in basis points.
+    uint32 public constant TWAP_INTERVAL = 3600; // 1 hour interval.
 
     // Hard coded addresses as they are the same accross chains
     address public constant FACTORY = 0x1F98431c8aD98523631AE4a59f267346ea31F984; // The address of the Uniswap V3 factory.
     address public constant WETH9 = 0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619; // Intermediate token address.
     ISwapRouter public swapRouter = ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
-    address public immutable MORPHO; // Morpho token.
+
+    address public immutable REWARD_TOKEN; // The reward token address.
+    address public immutable MORPHO; // Morpho token address.
+
+    IUniswapV3Pool public pool0;
+    IUniswapV3Pool public pool1;
 
     /// Events ///
 
     /// @notice Emitted when a swap to Morpho tokens happens.
     /// @param _receiver The address of the receiver.
     /// @param _amountIn The amount of reward token swapped.
-    /// @param _amountOut The amount of tokens received.
-    event Swap(address _receiver, uint256 _amountIn, uint256 _amountOut);
+    /// @param _amountOut The amount of Morpho token received.
+    event Swapped(address _receiver, uint256 _amountIn, uint256 _amountOut);
 
     /// Structs ///
 
@@ -51,37 +57,37 @@ contract SwapManager is ISwapManager {
 
     /// @notice Constructs the SwapManager contract.
     /// @param _morphoToken The Morpho token address.
-    constructor(address _morphoToken) {
+    /// @param _rewardToken The reward token address.
+    constructor(address _morphoToken, address _rewardToken) {
         MORPHO = _morphoToken;
+        REWARD_TOKEN = _rewardToken;
+        pool0 = IUniswapV3Pool(
+            PoolAddress.computeAddress(
+                FACTORY,
+                PoolAddress.getPoolKey(_rewardToken, WETH9, POOL_FEE)
+            )
+        );
+        pool1 = IUniswapV3Pool(
+            PoolAddress.computeAddress(
+                FACTORY,
+                PoolAddress.getPoolKey(WETH9, _morphoToken, POOL_FEE)
+            )
+        );
     }
 
     /// External ///
 
     /// @dev Swaps reward tokens to Morpho token.
-    /// @param _tokenIn The address of the reward token.
     /// @param _amountIn The amount of reward token to swap.
     /// @param _receiver The address of the receiver of the Morpho tokens.
     /// @return amountOut The amount of Morpho tokens sent.
-    function swapToMorphoToken(
-        address _tokenIn,
-        uint256 _amountIn,
-        address _receiver
-    ) external override returns (uint256 amountOut) {
-        IERC20(_tokenIn).safeTransferFrom(msg.sender, address(this), _amountIn);
-        IERC20(_tokenIn).safeApprove(address(swapRouter), _amountIn);
-
-        OracleTwapVars memory vars;
-
-        IUniswapV3Pool pool0 = IUniswapV3Pool(
-            PoolAddress.computeAddress(FACTORY, PoolAddress.getPoolKey(_tokenIn, WETH9, POOL_FEE))
-        );
-        IUniswapV3Pool pool1 = IUniswapV3Pool(
-            PoolAddress.computeAddress(FACTORY, PoolAddress.getPoolKey(WETH9, MORPHO, POOL_FEE))
-        );
-
+    function swapToMorphoToken(uint256 _amountIn, address _receiver)
+        external
+        override
+        returns (uint256 amountOut)
+    {
         uint32[] memory secondsAgo = new uint32[](2);
-        uint32 twapInterval = 3600; // 1 hour interval
-        secondsAgo[0] = twapInterval;
+        secondsAgo[0] = TWAP_INTERVAL;
         secondsAgo[1] = 0;
 
         (int56[] memory tickCumulatives0, ) = pool0.observe(secondsAgo);
@@ -92,46 +98,48 @@ contract SwapManager is ISwapManager {
 
         // Ticks (imprecise as it's an integer) to price
         uint160 sqrtPriceX960 = TickMath.getSqrtRatioAtTick(
-            int24((tickCumulatives0[1] - tickCumulatives0[0]) / int24(uint24(twapInterval)))
+            int24((tickCumulatives0[1] - tickCumulatives0[0]) / int24(uint24(TWAP_INTERVAL)))
         );
         uint160 sqrtPriceX961 = TickMath.getSqrtRatioAtTick(
-            int24((tickCumulatives1[1] - tickCumulatives1[0]) / int24(uint24(twapInterval)))
+            int24((tickCumulatives1[1] - tickCumulatives1[0]) / int24(uint24(TWAP_INTERVAL)))
         );
-        vars.priceX960 = getPriceX96FromSqrtPriceX96(sqrtPriceX960);
-        vars.priceX961 = getPriceX96FromSqrtPriceX96(sqrtPriceX961);
+        uint256 priceX960 = getPriceX96FromSqrtPriceX96(sqrtPriceX960);
+        uint256 priceX961 = getPriceX96FromSqrtPriceX96(sqrtPriceX961);
+        uint256 numerator;
+        uint256 denominator;
 
         // Computation depends on the position of token in pools
-        if (_tokenIn == pool0.token0() && WETH9 == pool1.token0()) {
-            vars.numerator = vars.priceX960 * vars.priceX961 * _amountIn;
-            vars.denominator = 2**96 * 2**96;
-        } else if (_tokenIn == pool0.token1() && WETH9 == pool1.token0()) {
-            vars.numerator = 2**96 * vars.priceX961 * _amountIn;
-            vars.denominator = vars.priceX960 * 2**96;
-        } else if (_tokenIn == pool0.token0() && WETH9 == pool1.token1()) {
-            vars.numerator = 2**96 * vars.priceX960 * _amountIn;
-            vars.denominator = vars.priceX961 * 2**96;
+        if (REWARD_TOKEN == pool0.token0() && WETH9 == pool1.token0()) {
+            numerator = priceX960 * priceX961 * _amountIn;
+            denominator = 2**96 * 2**96;
+        } else if (REWARD_TOKEN == pool0.token1() && WETH9 == pool1.token0()) {
+            numerator = 2**96 * priceX961 * _amountIn;
+            denominator = priceX960 * 2**96;
+        } else if (REWARD_TOKEN == pool0.token0() && WETH9 == pool1.token1()) {
+            numerator = 2**96 * priceX960 * _amountIn;
+            denominator = priceX961 * 2**96;
         } else {
-            vars.numerator = 2**96 * 2**96 * _amountIn;
-            vars.denominator = vars.priceX960 * vars.priceX961;
+            numerator = 2**96 * 2**96 * _amountIn;
+            denominator = priceX960 * priceX961;
         }
 
         // Max slippage of 1% for the trade
-        vars.expectedAmountOutMinimum =
-            (vars.numerator * (MAX_BASIS_POINTS - ONE_PERCENT)) /
-            (vars.denominator * MAX_BASIS_POINTS);
+        uint256 expectedAmountOutMinimum = (numerator * (MAX_BASIS_POINTS - ONE_PERCENT)) /
+            (denominator * MAX_BASIS_POINTS);
 
         ISwapRouter.ExactInputParams memory params = ISwapRouter.ExactInputParams({
-            path: abi.encodePacked(_tokenIn, POOL_FEE, WETH9, POOL_FEE, MORPHO),
+            path: abi.encodePacked(REWARD_TOKEN, POOL_FEE, WETH9, POOL_FEE, MORPHO),
             recipient: _receiver,
             deadline: block.timestamp,
             amountIn: _amountIn,
-            amountOutMinimum: vars.expectedAmountOutMinimum
+            amountOutMinimum: expectedAmountOutMinimum
         });
 
-        // Executes the swap
+        // Execute the swap
+        IERC20(REWARD_TOKEN).safeApprove(address(swapRouter), _amountIn);
         amountOut = swapRouter.exactInput(params);
 
-        emit Swap(_receiver, _amountIn, amountOut);
+        emit Swapped(_receiver, _amountIn, amountOut);
     }
 
     /// public ///
--- contracts/aave/interfaces/ISwapManager.sol
+++ contracts/aave/interfaces/ISwapManager.sol
@@ -2,9 +2,7 @@
 pragma solidity 0.8.7;
 
 interface ISwapManager {
-    function swapToMorphoToken(
-        address _tokenIn,
-        uint256 _amountIn,
-        address _receiver
-    ) external returns (uint256 amountOut);
+    function swapToMorphoToken(uint256 _amountIn, address _receiver)
+        external
+        returns (uint256 amountOut);
 }
