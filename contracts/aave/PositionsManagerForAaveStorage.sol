c797059000a642226891ad9fdaeab765f951e450
MerlinEgalite <egalite.merlin@gmail.com>
2022-02-16 14:29:42 +0100

    🎨 (claim-and-swap) move SwapManager into common folder

--- contracts/aave/PositionsManagerForAaveStorage.sol
+++ contracts/aave/PositionsManagerForAaveStorage.sol
@@ -9,7 +9,7 @@ import "./interfaces/aave/ILendingPool.sol";
 import "./interfaces/IMarketsManagerForAave.sol";
 import "./interfaces/IMatchingEngineForAave.sol";
 import "./interfaces/IRewardsManager.sol";
-import "./interfaces/ISwapManager.sol";
+import "../common/interfaces/ISwapManager.sol";
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
