1bcf2adc64646f3ed47cefad9e43e8a5fb257603
MathisGD <mathis.gontier_delaunay@telecom-sudparis.eu>
2022-02-20 21:55:38 +0100

    fix shareOfTheDelta calculation

--- contracts/aave/MarketsManagerForAave.sol
+++ contracts/aave/MarketsManagerForAave.sol
@@ -243,9 +243,9 @@ contract MarketsManagerForAave is Ownable {
             );
             uint256 shareOfTheDelta = positionsManager
             .supplyP2PDelta(_marketAddress)
-            .rayMul(supplyP2PExchangeRate[_marketAddress])
-            .rayDiv(normalizedIncome)
-            .rayDiv(positionsManager.supplyP2PAmount(_marketAddress)); // in RAY
+            .rayMul(WadRayMath.wadToRay(supplyP2PExchangeRate[_marketAddress]))
+            .rayDiv(WadRayMath.wadToRay(normalizedIncome))
+            .rayDiv(WadRayMath.wadToRay(positionsManager.supplyP2PAmount(_marketAddress))); // in RAY
 
             supplyP2PExchangeRate[_marketAddress] = supplyP2PExchangeRate[_marketAddress].rayMul(
                 (
@@ -274,9 +274,9 @@ contract MarketsManagerForAave is Ownable {
             );
             uint256 shareOfTheDelta = positionsManager
             .borrowP2PDelta(_marketAddress)
-            .rayMul(borrowP2PExchangeRate[_marketAddress])
-            .rayDiv(normalizedVariableDebt)
-            .rayDiv(positionsManager.borrowP2PAmount(_marketAddress)); // in RAY
+            .rayMul(WadRayMath.wadToRay(borrowP2PExchangeRate[_marketAddress]))
+            .rayDiv(WadRayMath.wadToRay(normalizedVariableDebt))
+            .rayDiv(WadRayMath.wadToRay(positionsManager.borrowP2PAmount(_marketAddress))); // in RAY
 
             borrowP2PExchangeRate[_marketAddress] = borrowP2PExchangeRate[_marketAddress].rayMul(
                 (
