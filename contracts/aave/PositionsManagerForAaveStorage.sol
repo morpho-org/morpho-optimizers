b72c0472b030250161c1b804fe5be88205fa6982
MerlinEgalite <egalite.merlin@gmail.com>
2022-02-21 15:55:24 +0100

    🎨 (improvements) extract P2P exchange rate computation

--- contracts/aave/PositionsManagerForAaveStorage.sol
+++ contracts/aave/PositionsManagerForAaveStorage.sol
@@ -45,7 +45,7 @@ contract PositionsManagerForAaveStorage is ReentrancyGuard, Pausable {
     }
 
     struct Delta {
-        uint256 supplyDelta; // Difference between the stored P2P supply amount and the real P2P supply amount (in scaled balance).
+        uint256 supplyP2PDelta; // Difference between the stored P2P supply amount and the real P2P supply amount (in scaled balance).
         uint256 borrowP2PDelta; // Difference between the stored P2P borrow amount and the real P2P borrow amount (in adUnit).
         uint256 supplyP2PAmount; // Sum of all stored P2P supply (in P2P unit).
         uint256 borrowP2PAmount; // Sum of all stored P2P borrow (in P2P unit).
