4466c58a468aab5301f3732590f6c9b61cb20ffd
Philippe Gonday <philippe@gonday.com>
2022-03-17 15:47:24 +0100

    ✅ (compound-test) adapt setup + add solmate submodule

--- test-foundry/compound/TestGovernance.t.sol
+++ test-foundry/compound/TestGovernance.t.sol
@@ -1,13 +1,9 @@
 // SPDX-License-Identifier: GNU AGPLv3
 pragma solidity 0.8.7;
 
-import "@contracts/aave/libraries/aave/WadRayMath.sol";
-
 import "./setup/TestSetup.sol";
 
 contract TestGovernance is TestSetup {
-    using WadRayMath for uint256;
-
     // ==============
     // = Deployment =
     // ==============
@@ -15,15 +11,13 @@ contract TestGovernance is TestSetup {
     // Deployment
     // Should deploy the contract with the right values
     function test_deploy_contract() public {
-        DataTypes.ReserveData memory data = lendingPool.getReserveData(dai);
-        uint256 expectedSPY = (data.currentLiquidityRate + data.currentVariableBorrowRate) /
-            2 /
-            SECOND_PER_YEAR;
+        ICToken cToken = ICToken(cDai);
+        uint256 expectedBPY = (cToken.supplyRatePerBlock() + cToken.borrowRatePerBlock()) / 2;
 
-        assertEq(marketsManager.supplyP2PSPY(aDai), expectedSPY);
-        assertEq(marketsManager.borrowP2PSPY(aDai), expectedSPY);
-        assertEq(marketsManager.supplyP2PExchangeRate(aDai), RAY);
-        assertEq(marketsManager.borrowP2PExchangeRate(aDai), RAY);
+        assertEq(marketsManager.supplyP2PBPY(cDai), expectedBPY);
+        assertEq(marketsManager.borrowP2PBPY(cDai), expectedBPY);
+        assertEq(marketsManager.supplyP2PExchangeRate(cDai), RAY);
+        assertEq(marketsManager.borrowP2PExchangeRate(cDai), RAY);
     }
 
     // ========================
@@ -32,116 +26,109 @@ contract TestGovernance is TestSetup {
 
     // Should revert when the function is called with an improper market
     function test_revert_on_not_real_market() public {
-        hevm.expectRevert(abi.encodeWithSignature("MarketIsNotListedOnAave()"));
+        hevm.expectRevert(MarketsManagerForCompound.MarketCreationFailedOnCompound.selector);
         marketsManager.createMarket(address(supplier1));
     }
 
     // Only Owner should be able to create markets in peer-to-peer
     function test_only_owner_can_create_markets_1() public {
         for (uint256 i = 0; i < pools.length; i++) {
-            address underlying = IAToken(pools[i]).UNDERLYING_ASSET_ADDRESS();
             hevm.expectRevert("Ownable: caller is not the owner");
-            supplier1.createMarket(underlying);
+            supplier1.createMarket(pools[i]);
 
             hevm.expectRevert("Ownable: caller is not the owner");
-            borrower1.createMarket(underlying);
+            borrower1.createMarket(pools[i]);
         }
 
-        marketsManager.createMarket(weth);
+        marketsManager.createMarket(cEth);
     }
 
     // Only Owner should be able to set reserve factor
     function test_only_owner_can_set_reserveFactor() public {
         for (uint256 i = 0; i < pools.length; i++) {
             hevm.expectRevert("Ownable: caller is not the owner");
-            supplier1.setReserveFactor(aDai, 1111);
+            supplier1.setReserveFactor(cDai, 1111);
 
             hevm.expectRevert("Ownable: caller is not the owner");
-            borrower1.setReserveFactor(aDai, 1111);
+            borrower1.setReserveFactor(cDai, 1111);
         }
 
-        marketsManager.setReserveFactor(aDai, 1111);
+        marketsManager.setReserveFactor(cDai, 1111);
     }
 
     // Reserve factor should be updated
     function test_reserveFactor_should_be_updated() public {
-        marketsManager.setReserveFactor(aDai, 1111);
-        assertEq(marketsManager.reserveFactor(aDai), 1111);
+        marketsManager.setReserveFactor(cDai, 1111);
+        assertEq(marketsManager.reserveFactor(cDai), 1111);
     }
 
     // Anyone can update the rates
     function test_rates_should_be_updated() public {
-        borrower1.updateRates(aDai);
+        borrower1.updateRates(cDai);
         uint256 firstBlockTimestamp = block.timestamp;
 
-        DataTypes.ReserveData memory data = lendingPool.getReserveData(dai);
-        uint256 expectedSPY = (data.currentLiquidityRate + data.currentVariableBorrowRate) /
-            2 /
-            SECOND_PER_YEAR;
+        ICToken cToken = ICToken(cDai);
+        uint256 expectedBPY = (cToken.supplyRatePerBlock() + cToken.borrowRatePerBlock()) / 2;
 
-        uint256 borrowP2PExchangeRate = marketsManager.borrowP2PExchangeRate(aDai);
-        uint256 supplyP2PExchangeRate = marketsManager.supplyP2PExchangeRate(aDai);
+        uint256 borrowP2PExchangeRate = marketsManager.borrowP2PExchangeRate(cDai);
+        uint256 supplyP2PExchangeRate = marketsManager.supplyP2PExchangeRate(cDai);
 
-        assertEq(marketsManager.supplyP2PSPY(aDai), expectedSPY);
-        assertEq(marketsManager.borrowP2PSPY(aDai), expectedSPY);
+        assertEq(marketsManager.supplyP2PBPY(cDai), expectedBPY);
+        assertEq(marketsManager.borrowP2PBPY(cDai), expectedBPY);
         assertEq(supplyP2PExchangeRate, RAY);
         assertEq(borrowP2PExchangeRate, RAY);
 
         hevm.warp(block.timestamp + 100000);
-        borrower1.updateRates(aDai);
+        borrower1.updateRates(cDai);
         uint256 secondBlockTimestamp = block.timestamp;
 
-        data = lendingPool.getReserveData(dai);
-        expectedSPY =
-            (data.currentLiquidityRate + data.currentVariableBorrowRate) /
-            2 /
-            SECOND_PER_YEAR;
+        expectedBPY = (cToken.supplyRatePerBlock() + cToken.borrowRatePerBlock()) / 2;
 
-        uint256 supplySPY = (expectedSPY *
-            (MAX_BASIS_POINTS - marketsManager.reserveFactor(aDai))) / MAX_BASIS_POINTS;
-        uint256 borrowSPY = (expectedSPY *
-            (MAX_BASIS_POINTS + marketsManager.reserveFactor(aDai))) / MAX_BASIS_POINTS;
-        assertEq(marketsManager.supplyP2PSPY(aDai), supplySPY);
-        assertEq(marketsManager.borrowP2PSPY(aDai), borrowSPY);
+        uint256 supplyBPY = (expectedBPY *
+            (MAX_BASIS_POINTS - marketsManager.reserveFactor(cDai))) / MAX_BASIS_POINTS;
+        uint256 borrowBPY = (expectedBPY *
+            (MAX_BASIS_POINTS + marketsManager.reserveFactor(cDai))) / MAX_BASIS_POINTS;
+        assertEq(marketsManager.supplyP2PBPY(cDai), supplyBPY);
+        assertEq(marketsManager.borrowP2PBPY(cDai), borrowBPY);
 
-        uint256 newBorrowP2PExchangeRate = borrowP2PExchangeRate.rayMul(
-            computeCompoundedInterest(borrowSPY, secondBlockTimestamp - firstBlockTimestamp)
-        );
-        uint256 newSupplyP2PExchangeRate = supplyP2PExchangeRate.rayMul(
-            computeCompoundedInterest(supplySPY, secondBlockTimestamp - firstBlockTimestamp)
-        );
+        // TODO
+        // uint256 newBorrowP2PExchangeRate = borrowP2PExchangeRate.rayMul(
+        //     computeCompoundedInterest(borrowBPY, secondBlockTimestamp - firstBlockTimestamp)
+        // );
+        // uint256 newSupplyP2PExchangeRate = supplyP2PExchangeRate.rayMul(
+        //     computeCompoundedInterest(supplyBPY, secondBlockTimestamp - firstBlockTimestamp)
+        // );
 
-        borrowP2PExchangeRate = marketsManager.borrowP2PExchangeRate(aDai);
-        supplyP2PExchangeRate = marketsManager.supplyP2PExchangeRate(aDai);
-        assertEq(supplyP2PExchangeRate, newSupplyP2PExchangeRate);
-        assertEq(borrowP2PExchangeRate, newBorrowP2PExchangeRate);
+        // borrowP2PExchangeRate = marketsManager.borrowP2PExchangeRate(cDai);
+        // supplyP2PExchangeRate = marketsManager.supplyP2PExchangeRate(cDai);
+        // assertEq(supplyP2PExchangeRate, newSupplyP2PExchangeRate);
+        // assertEq(borrowP2PExchangeRate, newBorrowP2PExchangeRate);
     }
 
     // marketsManagerForAave should not be changed after already set by Owner
     function test_positionsManager_should_not_be_changed() public {
-        hevm.expectRevert(abi.encodeWithSignature("PositionsManagerAlreadySet()"));
+        hevm.expectRevert(MarketsManagerForCompound.PositionsManagerAlreadySet.selector);
         marketsManager.setPositionsManager(address(fakePositionsManagerImpl));
     }
 
     // Should create a market the with right values
     function test_create_market_with_right_values() public {
-        DataTypes.ReserveData memory data = lendingPool.getReserveData(aave);
-        uint256 expectedSPY = (data.currentLiquidityRate + data.currentVariableBorrowRate) /
-            2 /
-            SECOND_PER_YEAR;
-        marketsManager.createMarket(aave);
+        ICToken cToken = ICToken(cAave);
+        uint256 expectedBPY = (cToken.supplyRatePerBlock() + cToken.borrowRatePerBlock()) / 2;
+        marketsManager.createMarket(cAave);
 
-        assertTrue(marketsManager.isCreated(aAave));
-        assertEq(marketsManager.supplyP2PSPY(aAave), expectedSPY);
-        assertEq(marketsManager.borrowP2PSPY(aAave), expectedSPY);
-        assertEq(marketsManager.supplyP2PExchangeRate(aAave), RAY);
-        assertEq(marketsManager.borrowP2PExchangeRate(aAave), RAY);
+        assertTrue(marketsManager.isCreated(cAave));
+        assertEq(marketsManager.supplyP2PBPY(cAave), expectedBPY);
+        assertEq(marketsManager.borrowP2PBPY(cAave), expectedBPY);
+        assertEq(marketsManager.supplyP2PExchangeRate(cAave), RAY);
+        assertEq(marketsManager.borrowP2PExchangeRate(cAave), RAY);
     }
 
     // Only MarketsaManager's Owner should set NMAX
     function test_should_set_maxGas() public {
-        PositionsManagerForAaveStorage.MaxGas memory newMaxGas = PositionsManagerForAaveStorage
-        .MaxGas({supply: 1, borrow: 1, withdraw: 1, repay: 1});
+
+            PositionsManagerForCompoundStorage.MaxGas memory newMaxGas
+         = PositionsManagerForCompoundStorage.MaxGas({supply: 1, borrow: 1, withdraw: 1, repay: 1});
 
         positionsManager.setMaxGas(newMaxGas);
         (uint64 supply, uint64 borrow, uint64 withdraw, uint64 repay) = positionsManager.maxGas();
@@ -173,20 +160,22 @@ contract TestGovernance is TestSetup {
 
     function test_only_owner_should_flip_market_strategy() public {
         hevm.expectRevert("Ownable: caller is not the owner");
-        supplier1.setNoP2P(aDai, true);
+        supplier1.setNoP2P(cDai, true);
 
         hevm.expectRevert("Ownable: caller is not the owner");
-        supplier2.setNoP2P(aDai, true);
+        supplier2.setNoP2P(cDai, true);
 
-        marketsManager.setNoP2P(aDai, true);
-        assertTrue(marketsManager.noP2P(aDai));
+        marketsManager.setNoP2P(cDai, true);
+        assertTrue(marketsManager.noP2P(cDai));
     }
 
-    function test_only_owner_can_set_aave_incentives_controller_on_rewards_manager() public {
-        hevm.expectRevert("Ownable: caller is not the owner");
-        supplier1.setAaveIncentivesControllerOnRewardsManager(address(0));
+    // TODO no incentives ?
 
-        rewardsManager.setAaveIncentivesController(address(1));
-        assertEq(address(rewardsManager.aaveIncentivesController()), address(1));
-    }
+    // function test_only_owner_can_set_aave_incentives_controller_on_rewards_manager() public {
+    //     hevm.expectRevert("Ownable: caller is not the owner");
+    //     supplier1.setCompoundIncentivesControllerOnRewardsManager(address(0));
+
+    //     rewardsManager.setAaveIncentivesController(address(1));
+    //     assertEq(address(rewardsManager.aaveIncentivesController()), address(1));
+    // }
 }
