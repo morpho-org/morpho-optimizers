0beece1efe490e4e43265723a8dcfdf78e423722
MerlinEgalite <egalite.merlin@gmail.com>
2022-03-17 10:47:48 +0100

    ✨ (compound) compound contracts compiling

--- contracts/compound/interfaces/IPositionsManagerForCompound.sol
+++ contracts/compound/interfaces/IPositionsManagerForCompound.sol
@@ -2,9 +2,34 @@
 pragma solidity 0.8.13;
 
 interface IPositionsManagerForCompound {
-    function createMarket(address _marketAddress) external returns (uint256[] memory);
+    struct Balance {
+        uint256 inP2P;
+        uint256 onPool;
+    }
+
+    struct Delta {
+        uint256 supplyP2PDelta;
+        uint256 borrowP2PDelta;
+        uint256 supplyP2PAmount;
+        uint256 borrowP2PAmount;
+    }
+
+    function createMarket(address) external returns (uint256[] memory);
+
+    function getUserMaxCapacitiesForAsset(address _user, address _poolTokenAddress)
+        external
+        view
+        returns (uint256 withdrawable, uint256 borrowable);
 
     function setNMAX(uint16) external;
 
-    function setThreshold(address, uint256) external;
+    function setTreasuryVault(address) external;
+
+    function setRewardsManager(address _rewardsManagerAddress) external;
+
+    function borrowBalanceInOf(address, address) external returns (Balance memory);
+
+    function supplyBalanceInOf(address, address) external returns (Balance memory);
+
+    function deltas(address) external view returns (Delta memory);
 }
