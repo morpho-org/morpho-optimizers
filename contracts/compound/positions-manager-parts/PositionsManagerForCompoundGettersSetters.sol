6ad239949b35adebf382424617d406efacf085df
MerlinEgalite <egalite.merlin@gmail.com>
2022-03-17 10:47:48 +0100

    ✨ (compound) compound contracts compiling

---
+++ contracts/compound/positions-manager-parts/PositionsManagerForCompoundGettersSetters.sol
@@ -0,0 +1,287 @@
+// SPDX-License-Identifier: GNU AGPLv3
+pragma solidity 0.8.7;
+
+import "@openzeppelin/contracts/utils/math/Math.sol";
+import "./PositionsManagerForCompoundEventsErrors.sol";
+
+/// @title PositionsManagerForCompoundGettersSetters.
+/// @notice Getters and setters for PositionsManagerForCompound, including externals, internals, user-accessible and admin-only functions.
+abstract contract PositionsManagerForCompoundGettersSetters is
+    PositionsManagerForCompoundEventsErrors
+{
+    using DoubleLinkedList for DoubleLinkedList.List;
+    using CompoundMath for uint256;
+
+    /// MODIFIERS ///
+
+    /// @notice Prevents a user to trigger a function when market is not created or paused.
+    /// @param _poolTokenAddress The address of the market to check.
+    modifier isMarketCreatedAndNotPaused(address _poolTokenAddress) {
+        if (!marketsManager.isCreated(_poolTokenAddress)) revert MarketNotCreated();
+        if (paused[_poolTokenAddress]) revert MarketPaused();
+        _;
+    }
+
+    /// UPGRADE ///
+
+    /// @notice Initializes the PositionsManagerForCompound contract.
+    /// @param _marketsManager The `marketsManager`.
+    /// @param _comptroller The `comptroller`.
+    /// @param _swapManager The `swapManager`.
+    /// @param _maxGas The `maxGas`.
+    /// @param _NDS The `NDS`.
+    function initialize(
+        IMarketsManagerForCompound _marketsManager,
+        IMatchingEngineForCompound _matchingEngine,
+        IComptroller _comptroller,
+        ISwapManager _swapManager,
+        MaxGas memory _maxGas,
+        uint8 _NDS
+    ) external initializer {
+        __ReentrancyGuard_init();
+        __Ownable_init();
+
+        marketsManager = _marketsManager;
+        matchingEngine = _matchingEngine;
+        comptroller = _comptroller;
+        swapManager = _swapManager;
+
+        maxGas = _maxGas;
+        NDS = _NDS;
+    }
+
+    /// SETTERS ///
+
+    /// @dev Sets `NDS`.
+    /// @param _newNDS The new `NDS` value.
+    function setNDS(uint8 _newNDS) external onlyOwner {
+        NDS = _newNDS;
+        emit NDSSet(_newNDS);
+    }
+
+    /// @dev Sets `maxGas`.
+    /// @param _maxGas The new `maxGas`.
+    function setMaxGas(MaxGas memory _maxGas) external onlyOwner {
+        maxGas = _maxGas;
+        emit MaxGasSet(_maxGas);
+    }
+
+    /// @notice Sets the `_newTreasuryVaultAddress`.
+    /// @param _newTreasuryVaultAddress The address of the new `treasuryVault`.
+    function setTreasuryVault(address _newTreasuryVaultAddress) external onlyOwner {
+        treasuryVault = _newTreasuryVaultAddress;
+        emit TreasuryVaultSet(_newTreasuryVaultAddress);
+    }
+
+    /// @dev Sets the `rewardsManager`.
+    /// @param _rewardsManagerAddress The address of the `rewardsManager`.
+    function setRewardsManager(address _rewardsManagerAddress) external onlyOwner {
+        rewardsManager = IRewardsManagerForCompound(_rewardsManagerAddress);
+        emit RewardsManagerSet(_rewardsManagerAddress);
+    }
+
+    /// @dev Sets the pause status on a specific market in case of emergency.
+    /// @param _poolTokenAddress The address of the market to pause/unpause.
+    function setPauseStatus(address _poolTokenAddress) external onlyOwner {
+        bool newPauseStatus = !paused[_poolTokenAddress];
+        paused[_poolTokenAddress] = newPauseStatus;
+        emit PauseStatusSet(_poolTokenAddress, newPauseStatus);
+    }
+
+    /// GETTERS ///
+
+    /// @notice Gets the head of the data structure on a specific market (for UI).
+    /// @param _poolTokenAddress The address of the market from which to get the head.
+    /// @param _positionType The type of user from which to get the head.
+    /// @return head The head in the data structure.
+    function getHead(address _poolTokenAddress, PositionType _positionType)
+        external
+        view
+        returns (address head)
+    {
+        if (_positionType == PositionType.SUPPLIERS_IN_P2P)
+            head = suppliersInP2P[_poolTokenAddress].getHead();
+        else if (_positionType == PositionType.SUPPLIERS_ON_POOL)
+            head = suppliersOnPool[_poolTokenAddress].getHead();
+        else if (_positionType == PositionType.BORROWERS_IN_P2P)
+            head = borrowersInP2P[_poolTokenAddress].getHead();
+        else if (_positionType == PositionType.BORROWERS_ON_POOL)
+            head = borrowersOnPool[_poolTokenAddress].getHead();
+    }
+
+    /// @notice Gets the next user after `_user` in the data structure on a specific market (for UI).
+    /// @param _poolTokenAddress The address of the market from which to get the user.
+    /// @param _positionType The type of user from which to get the next user.
+    /// @param _user The address of the user from which to get the next user.
+    /// @return next The next user in the data structure.
+    function getNext(
+        address _poolTokenAddress,
+        PositionType _positionType,
+        address _user
+    ) external view returns (address next) {
+        if (_positionType == PositionType.SUPPLIERS_IN_P2P)
+            next = suppliersInP2P[_poolTokenAddress].getNext(_user);
+        else if (_positionType == PositionType.SUPPLIERS_ON_POOL)
+            next = suppliersOnPool[_poolTokenAddress].getNext(_user);
+        else if (_positionType == PositionType.BORROWERS_IN_P2P)
+            next = borrowersInP2P[_poolTokenAddress].getNext(_user);
+        else if (_positionType == PositionType.BORROWERS_ON_POOL)
+            next = borrowersOnPool[_poolTokenAddress].getNext(_user);
+    }
+
+    /// @notice Returns the collateral value, debt value and max debt value of a given user (in ETH).
+    /// @param _user The user to determine liquidity for.
+    /// @return collateralValue The collateral value of the user (in ETH).
+    /// @return debtValue The current debt value of the user (in ETH).
+    /// @return maxDebtValue The maximum possible debt value of the user (in ETH).
+    function getUserBalanceStates(address _user)
+        external
+        view
+        returns (
+            uint256 collateralValue,
+            uint256 debtValue,
+            uint256 maxDebtValue
+        )
+    {
+        ICompoundOracle oracle = ICompoundOracle(comptroller.oracle());
+        uint256 numberOfEnteredMarkets = enteredMarkets[_user].length;
+        uint256 i;
+
+        while (i < numberOfEnteredMarkets) {
+            address poolTokenEntered = enteredMarkets[_user][i];
+            AssetLiquidityData memory assetData = getUserLiquidityDataForAsset(
+                _user,
+                poolTokenEntered,
+                oracle
+            );
+
+            unchecked {
+                collateralValue += assetData.collateralValue;
+                maxDebtValue += assetData.maxDebtValue;
+                debtValue += assetData.debtValue;
+                ++i;
+            }
+        }
+    }
+
+    /// @notice Returns the maximum amount available to withdraw and borrow for `_user` related to `_poolTokenAddress` (in underlyings).
+    /// @param _user The user to determine the capacities for.
+    /// @param _poolTokenAddress The address of the market.
+    /// @return withdrawable The maximum withdrawable amount of underlying token allowed (in underlying).
+    /// @return borrowable The maximum borrowable amount of underlying token allowed (in underlying).
+    function getUserMaxCapacitiesForAsset(address _user, address _poolTokenAddress)
+        external
+        view
+        returns (uint256 withdrawable, uint256 borrowable)
+    {
+        LiquidityData memory data;
+        AssetLiquidityData memory assetData;
+        ICompoundOracle oracle = ICompoundOracle(comptroller.oracle());
+        uint256 numberOfEnteredMarkets = enteredMarkets[_user].length;
+        uint256 i;
+
+        while (i < numberOfEnteredMarkets) {
+            address poolTokenEntered = enteredMarkets[_user][i];
+
+            if (_poolTokenAddress != poolTokenEntered) {
+                assetData = getUserLiquidityDataForAsset(_user, poolTokenEntered, oracle);
+
+                unchecked {
+                    data.maxDebtValue += assetData.maxDebtValue;
+                    data.debtValue += assetData.debtValue;
+                }
+            }
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        assetData = getUserLiquidityDataForAsset(_user, _poolTokenAddress, oracle);
+
+        unchecked {
+            data.maxDebtValue += assetData.maxDebtValue;
+            data.debtValue += assetData.debtValue;
+        }
+
+        // Not possible to withdraw nor borrow
+        if (data.maxDebtValue < data.debtValue) return (0, 0);
+
+        uint256 differenceInUnderlying = (data.maxDebtValue - data.debtValue).mul(
+            assetData.underlyingPrice
+        );
+
+        withdrawable = assetData.collateralValue.div(assetData.underlyingPrice);
+        if (assetData.collateralFactor != 0) {
+            withdrawable = Math.min(
+                withdrawable,
+                differenceInUnderlying.div(assetData.collateralFactor)
+            );
+        }
+
+        borrowable = differenceInUnderlying;
+    }
+
+    /// @notice Returns the data related to `_poolTokenAddress` for the `_user`.
+    /// @param _user The user to determine data for.
+    /// @param _poolTokenAddress The address of the market.
+    /// @param _poolTokenAddress The address of the market.
+    /// @param _oracle The oracle used.
+    /// @return assetData The data related to this asset.
+    function getUserLiquidityDataForAsset(
+        address _user,
+        address _poolTokenAddress,
+        ICompoundOracle _oracle
+    ) public view returns (AssetLiquidityData memory assetData) {
+        // Compute the current debt amount (in underlying)
+        address underlyingAddress = ICToken(_poolTokenAddress).underlying();
+        assetData.debtValue = _getUserBorrowBalanceInOf(_poolTokenAddress, _user);
+
+        // Compute the current collateral amount (in underlying)
+        assetData.collateralValue = _getUserSupplyBalanceInOf(_poolTokenAddress, _user);
+
+        assetData.underlyingPrice = _oracle.getUnderlyingPrice(underlyingAddress);
+        (, assetData.collateralFactor, ) = comptroller.markets(_poolTokenAddress);
+
+        // Then, convert values to ETH
+        assetData.collateralValue = assetData.collateralValue.mul(assetData.underlyingPrice);
+        assetData.debtValue = assetData.debtValue.mul(assetData.underlyingPrice);
+        assetData.maxDebtValue = assetData.collateralValue.mul(assetData.collateralFactor);
+    }
+
+    /// @dev Returns the supply balance of `_user` in the `_poolTokenAddress` market.
+    /// @param _user The address of the user.
+    /// @param _poolTokenAddress The market where to get the supply amount.
+    /// @return The supply balance of the user (in underlying).
+    function _getUserSupplyBalanceInOf(address _poolTokenAddress, address _user)
+        internal
+        view
+        returns (uint256)
+    {
+        return
+            supplyBalanceInOf[_poolTokenAddress][_user].inP2P.mul(
+                marketsManager.supplyP2PExchangeRate(_poolTokenAddress)
+            ) +
+            supplyBalanceInOf[_poolTokenAddress][_user].onPool.mul(
+                ICToken(_poolTokenAddress).exchangeRateStored()
+            ); // TODO: check if needed to use Current one.
+    }
+
+    /// @dev Returns the borrow balance of `_user` in the `_poolTokenAddress` market.
+    /// @param _user The address of the user.
+    /// @param _poolTokenAddress The market where to get the borrow amount.
+    /// @return The borrow balance of the user (in underlying).
+    function _getUserBorrowBalanceInOf(address _poolTokenAddress, address _user)
+        internal
+        view
+        returns (uint256)
+    {
+        return
+            borrowBalanceInOf[_poolTokenAddress][_user].inP2P.mul(
+                marketsManager.borrowP2PExchangeRate(_poolTokenAddress)
+            ) +
+            borrowBalanceInOf[_poolTokenAddress][_user].onPool.mul(
+                ICToken(_poolTokenAddress).borrowIndex()
+            );
+    }
+}
