ef0a9b87176e679cb7878fdd58e41dc41fd0adb2
gabriel <gabriel.astieres@gmail.com>
2022-03-17 19:23:10 +0100

    ♻: (compound-test): reworking Pausable & Positions Getters

--- test-foundry/compound/TestPausableMarket.t.sol
+++ test-foundry/compound/TestPausableMarket.t.sol
@@ -4,20 +4,22 @@ pragma solidity 0.8.7;
 import "./setup/TestSetup.sol";
 
 contract TestPausableMarket is TestSetup {
+    using CompoundMath for uint256;
+
     function test_only_markets_owner_can_trigger_pause_function() public {
         hevm.expectRevert("Ownable: caller is not the owner");
-        supplier1.setPauseStatus(aDai);
+        supplier1.setPauseStatus(dai);
 
-        positionsManager.setPauseStatus(aDai);
-        assertTrue(positionsManager.paused(aDai), "paused is false");
+        positionsManager.setPauseStatus(dai);
+        assertTrue(positionsManager.paused(dai), "paused is false");
     }
 
     function test_pause_unpause() public {
-        positionsManager.setPauseStatus(aDai);
-        assertTrue(positionsManager.paused(aDai), "paused is false");
+        positionsManager.setPauseStatus(dai);
+        assertTrue(positionsManager.paused(dai), "paused is false");
 
-        positionsManager.setPauseStatus(aDai);
-        assertFalse(positionsManager.paused(aDai), "paused is true");
+        positionsManager.setPauseStatus(dai);
+        assertFalse(positionsManager.paused(dai), "paused is true");
     }
 
     function test_ability_to_trigger_functions_when_not_paused() public {
@@ -25,69 +27,69 @@ contract TestPausableMarket is TestSetup {
         uint256 toBorrow = to6Decimals(amount / 2);
 
         supplier1.approve(dai, amount);
-        supplier1.supply(aDai, amount);
+        supplier1.supply(cDai, amount);
 
-        supplier1.borrow(aUsdc, toBorrow);
+        supplier1.borrow(cUsdc, toBorrow);
 
         supplier1.approve(usdc, toBorrow);
-        supplier1.repay(aUsdc, toBorrow);
+        supplier1.repay(cUsdc, toBorrow);
 
-        (, toBorrow) = positionsManager.getUserMaxCapacitiesForAsset(address(supplier1), aUsdc);
-        supplier1.borrow(aUsdc, toBorrow);
+        (, toBorrow) = positionsManager.getUserMaxCapacitiesForAsset(address(supplier1), cUsdc);
+        supplier1.borrow(cUsdc, toBorrow);
 
         // Change Oracle
         SimplePriceOracle customOracle = createAndSetCustomPriceOracle();
-        customOracle.setDirectPrice(dai, (oracle.getAssetPrice(dai) * 93) / 100);
+        customOracle.setDirectPrice(dai, (oracle.getUnderlyingPrice(cDai) * 93) / 100);
 
         uint256 toLiquidate = toBorrow / 2;
         User liquidator = borrower3;
         liquidator.approve(usdc, toLiquidate);
-        liquidator.liquidate(aUsdc, aDai, address(supplier1), toLiquidate);
+        liquidator.liquidate(cUsdc, cDai, address(supplier1), toLiquidate);
 
-        supplier1.withdraw(aDai, 1);
+        supplier1.withdraw(cDai, 1);
 
-        positionsManager.claimToTreasury(aDai);
+        positionsManager.claimToTreasury(cDai);
     }
 
     function test_not_possible_to_trigger_functions_when_paused() public {
         uint256 amount = 10000 ether;
 
         supplier1.approve(dai, 2 * amount);
-        supplier1.supply(aDai, amount);
+        supplier1.supply(cDai, amount);
 
         (, uint256 toBorrow) = positionsManager.getUserMaxCapacitiesForAsset(
             address(supplier1),
-            aUsdc
+            cUsdc
         );
-        supplier1.borrow(aUsdc, toBorrow);
+        supplier1.borrow(cUsdc, toBorrow);
 
-        positionsManager.setPauseStatus(aDai);
-        positionsManager.setPauseStatus(aUsdc);
-
-        hevm.expectRevert(abi.encodeWithSignature("MarketPaused()"));
-        supplier1.supply(aDai, amount);
+        positionsManager.setPauseStatus(cDai);
+        positionsManager.setPauseStatus(cUsdc);
 
         hevm.expectRevert(abi.encodeWithSignature("MarketPaused()"));
-        supplier1.borrow(aUsdc, 0);
+        supplier1.supply(cDai, amount);
+
+        hevm.expectRevert(abi.encodeWithSignature("MarketPaused()"));
+        supplier1.borrow(cUsdc, 0);
 
         supplier1.approve(usdc, toBorrow);
         hevm.expectRevert(abi.encodeWithSignature("MarketPaused()"));
-        supplier1.repay(aUsdc, toBorrow);
+        supplier1.repay(cUsdc, toBorrow);
         hevm.expectRevert(abi.encodeWithSignature("MarketPaused()"));
-        supplier1.withdraw(aDai, 1);
+        supplier1.withdraw(cDai, 1);
 
         // Change Oracle
         SimplePriceOracle customOracle = createAndSetCustomPriceOracle();
-        customOracle.setDirectPrice(dai, (oracle.getAssetPrice(dai) * 93) / 100);
+        customOracle.setDirectPrice(dai, (oracle.getUnderlyingPrice(dai) * 93) / 100);
 
         uint256 toLiquidate = toBorrow / 2;
         User liquidator = borrower3;
         liquidator.approve(usdc, toLiquidate);
 
         hevm.expectRevert(abi.encodeWithSignature("MarketPaused()"));
-        liquidator.liquidate(aUsdc, aDai, address(supplier1), toLiquidate);
+        liquidator.liquidate(cUsdc, cDai, address(supplier1), toLiquidate);
 
         hevm.expectRevert(abi.encodeWithSignature("MarketPaused()"));
-        positionsManager.claimToTreasury(aDai);
+        positionsManager.claimToTreasury(cDai);
     }
 }
--- test-foundry/compound/setup/TestSetup.sol
+++ test-foundry/compound/setup/TestSetup.sol
@@ -1,20 +1,15 @@
 // SPDX-License-Identifier: GNU AGPLv3
 pragma solidity 0.8.7;
-
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import {ICToken, ICompoundOracle} from "@contracts/compound/interfaces/compound/ICompound.sol";
 import "@contracts/compound/interfaces/IRewardsManagerForCompound.sol";
 import "@contracts/common/interfaces/ISwapManager.sol";
-
 import "hardhat/console.sol";
 import "../../common/helpers/Chains.sol";
-
 import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
-
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
-
 import {SwapManagerUniV3OnEth} from "@contracts/common/SwapManagerUniV3OnEth.sol";
 import {SwapManagerUniV3} from "@contracts/common/SwapManagerUniV3.sol";
 import {SwapManagerUniV2} from "@contracts/common/SwapManagerUniV2.sol";
@@ -23,7 +18,6 @@ import {UniswapV2PoolCreator} from "../../common/uniswap/UniswapV2PoolCreator.so
 import "@contracts/compound/PositionsManagerForCompound.sol";
 import "@contracts/compound/MarketsManagerForCompound.sol";
 import "@contracts/compound/MatchingEngineForCompound.sol";
-
 import "../../common/helpers/MorphoToken.sol";
 import "../../common/helpers/SimplePriceOracle.sol";
 import {User} from "../helpers/User.sol";
@@ -31,16 +25,19 @@ import "../../common/setup/HevmAdapter.sol";
 import {Utils} from "./Utils.sol";
 import "@config/Config.sol";
 
+interface IAdminComptroller {
+    function _setPriceOracle(SimplePriceOracle newOracle) external returns (uint256);
+    
+    function admin() external view returns (address);
+}
+
 contract TestSetup is Config, Utils, HevmAdapter {
     using SafeERC20 for IERC20;
-
     uint256 public constant MAX_BASIS_POINTS = 10_000;
     uint256 public constant INITIAL_BALANCE = 1_000_000;
-
     ProxyAdmin public proxyAdmin;
     TransparentUpgradeableProxy public positionsManagerProxy;
     TransparentUpgradeableProxy public marketsManagerProxy;
-
     MatchingEngineForCompound internal matchingEngine;
     PositionsManagerForCompound internal positionsManagerImplV1;
     PositionsManagerForCompound internal positionsManager;
@@ -52,21 +49,17 @@ contract TestSetup is Config, Utils, HevmAdapter {
     UniswapPoolCreator public uniswapPoolCreator;
     UniswapV2PoolCreator public uniswapV2PoolCreator;
     MorphoToken public morphoToken;
-
     IComptroller public comptroller;
     ICompoundOracle public oracle;
-
     User public supplier1;
     User public supplier2;
     User public supplier3;
     User[] public suppliers;
-
     User public borrower1;
     User public borrower2;
     User public borrower3;
     User[] public borrowers;
     User public treasuryVault;
-
     address[] public pools;
 
     function setUp() public {
@@ -78,29 +71,28 @@ contract TestSetup is Config, Utils, HevmAdapter {
     function initContracts() internal {
         PositionsManagerForCompound.MaxGas memory maxGas = PositionsManagerForCompoundStorage
         .MaxGas({supply: 3e6, borrow: 3e6, withdraw: 3e6, repay: 3e6});
-
+        comptroller = IComptroller(comptrollerAddress);
         matchingEngine = new MatchingEngineForCompound();
 
         // Deploy proxy
-
         proxyAdmin = new ProxyAdmin();
-
         marketsManagerImplV1 = new MarketsManagerForCompound();
         marketsManagerProxy = new TransparentUpgradeableProxy(
             address(marketsManagerImplV1),
             address(this),
             ""
         );
+
         marketsManagerProxy.changeAdmin(address(proxyAdmin));
         marketsManager = MarketsManagerForCompound(address(marketsManagerProxy));
         marketsManager.initialize(comptroller);
-
         positionsManagerImplV1 = new PositionsManagerForCompound();
         positionsManagerProxy = new TransparentUpgradeableProxy(
             address(positionsManagerImplV1),
             address(this),
             ""
         );
+
         positionsManagerProxy.changeAdmin(address(proxyAdmin));
         positionsManager = PositionsManagerForCompound(address(positionsManagerProxy));
         positionsManager.initialize(
@@ -113,28 +105,22 @@ contract TestSetup is Config, Utils, HevmAdapter {
         );
 
         treasuryVault = new User(positionsManager);
-
         fakePositionsManagerImpl = new PositionsManagerForCompound();
-
         oracle = ICompoundOracle(comptroller.oracle());
-
         marketsManager.setPositionsManager(address(positionsManager));
         positionsManager.setTreasuryVault(address(treasuryVault));
-
-        createMarket(aDai);
-        createMarket(aUsdc);
-        createMarket(aWbtc);
-        createMarket(aUsdt);
+        createMarket(cDai);
+        createMarket(cUsdc);
+        createMarket(cWbtc);
+        createMarket(cUsdt);
     }
 
-    function createMarket(address _aToken) internal {
-        address underlying = ICToken(_aToken).underlying();
-        marketsManager.createMarket(underlying);
-
+    function createMarket(address _cToken) internal {
+        marketsManager.createMarket(_cToken);
         // All tokens must also be added to the pools array, for the correct behavior of TestLiquidate::createAndSetCustomPriceOracle.
-        pools.push(_aToken);
-
-        hevm.label(_aToken, ERC20(_aToken).symbol());
+        pools.push(_cToken);
+        hevm.label(_cToken, ERC20(_cToken).symbol());
+        address underlying = ICToken(_cToken).underlying();
         hevm.label(underlying, ERC20(underlying).symbol());
     }
 
@@ -159,6 +145,7 @@ contract TestSetup is Config, Utils, HevmAdapter {
             );
             fillUserBalances(borrowers[i]);
         }
+
         borrower1 = borrowers[0];
         borrower2 = borrowers[1];
         borrower3 = borrowers[2];
@@ -185,7 +172,6 @@ contract TestSetup is Config, Utils, HevmAdapter {
         while (borrowers.length < _nbOfSigners) {
             borrowers.push(new User(positionsManager));
             fillUserBalances(borrowers[borrowers.length - 1]);
-
             suppliers.push(new User(positionsManager));
             fillUserBalances(suppliers[suppliers.length - 1]);
         }
@@ -193,19 +179,14 @@ contract TestSetup is Config, Utils, HevmAdapter {
 
     function createAndSetCustomPriceOracle() public returns (SimplePriceOracle) {
         SimplePriceOracle customOracle = new SimplePriceOracle();
-
-        hevm.store(
-            address(comptroller),
-            keccak256(abi.encode(bytes32("oracle"), 2)),
-            bytes32(uint256(uint160(address(customOracle))))
-        );
+        IAdminComptroller adminComptroller = IAdminComptroller(address(comptroller));
+        hevm.prank(adminComptroller.admin());
+        uint256 result = adminComptroller._setPriceOracle(customOracle);
+        require(result == 0); // No error
 
         for (uint256 i = 0; i < pools.length; i++) {
-            address underlying = ICToken(pools[i]).underlying();
-
-            customOracle.setDirectPrice(underlying, oracle.getUnderlyingPrice(underlying));
+            customOracle.setDirectPrice(pools[i], oracle.getUnderlyingPrice(pools[i]));
         }
-
         return customOracle;
     }
 
@@ -223,7 +204,6 @@ contract TestSetup is Config, Utils, HevmAdapter {
             withdraw: _withdraw,
             repay: _repay
         });
-
         positionsManager.setMaxGas(newMaxGas);
     }
 }
