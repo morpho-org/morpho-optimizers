e78408fdd303a135a801e7b4e887571586a2063a
MerlinEgalite <egalite.merlin@gmail.com>
2022-03-19 12:38:40 +0100

    🧪 (compound-test) update test files

--- test-foundry/compound/TestFees.t.sol
+++ test-foundry/compound/TestFees.t.sol
@@ -1,17 +1,15 @@
 // SPDX-License-Identifier: GNU AGPLv3
 pragma solidity 0.8.7;
 
-import "@contracts/aave/libraries/aave/WadRayMath.sol";
-
 import "./setup/TestSetup.sol";
 
 contract TestFees is TestSetup {
-    using WadRayMath for uint256;
+    using CompoundMath for uint256;
 
     // Should not be possible to set the fee factor higher than 50%
     function test_higher_than_max_fees() public {
-        marketsManager.setReserveFactor(aUsdc, 5_001);
-        testEquality(marketsManager.reserveFactor(aUsdc), 5000);
+        marketsManager.setReserveFactor(cUsdc, 5_001);
+        testEquality(marketsManager.reserveFactor(cUsdc), 5000);
     }
 
     // Only MarketsManager owner can set the treasury vault
@@ -22,59 +20,55 @@ contract TestFees is TestSetup {
 
     // DAO should be able to claim fees
     function test_claim_fees() public {
-        marketsManager.setReserveFactor(aDai, 1000); // 10%
+        marketsManager.setReserveFactor(cDai, 1000); // 10%
 
         // Increase time so that rates update
         hevm.warp(block.timestamp + 1);
 
         uint256 balanceBefore = IERC20(dai).balanceOf(positionsManager.treasuryVault());
         supplier1.approve(dai, type(uint256).max);
-        supplier1.supply(aDai, 100 * WAD);
-        supplier1.borrow(aDai, 50 * WAD);
+        supplier1.supply(cDai, 100 * WAD);
+        supplier1.borrow(cDai, 50 * WAD);
 
         hevm.warp(block.timestamp + (365 days));
 
-        supplier1.repay(aDai, type(uint256).max);
-        positionsManager.claimToTreasury(aDai);
+        supplier1.repay(cDai, type(uint256).max);
+        positionsManager.claimToTreasury(cDai);
         uint256 balanceAfter = IERC20(dai).balanceOf(positionsManager.treasuryVault());
 
         assertLt(balanceBefore, balanceAfter);
     }
 
-    // Collected fees should be of the correct amount
+    // Collected fees should be of the correct amount.
     function test_fee_amount() public {
-        marketsManager.setReserveFactor(aDai, 1000); // 10%
+        marketsManager.setReserveFactor(cDai, 1000); // 10%
 
         // Increase time so that rates update
         hevm.warp(block.timestamp + 1);
 
         uint256 balanceBefore = IERC20(dai).balanceOf(positionsManager.treasuryVault());
         supplier1.approve(dai, type(uint256).max);
-        supplier1.supply(aDai, 100 * WAD);
-        supplier1.borrow(aDai, 50 * WAD);
+        supplier1.supply(cDai, 100 * WAD);
+        supplier1.borrow(cDai, 50 * WAD);
 
-        DataTypes.ReserveData memory reserveData = lendingPool.getReserveData(
-            IAToken(aDai).UNDERLYING_ASSET_ADDRESS()
-        );
+        ICToken cToken = ICToken(cDai);
+        uint256 supplyBPY = cToken.supplyRatePerBlock();
+        uint256 borrowBPY = cToken.borrowRatePerBlock();
 
-        uint256 meanSPY = Math.average(
-            reserveData.currentLiquidityRate,
-            reserveData.currentVariableBorrowRate
-        ) / (365 days); // In ray
+        uint256 meanBPY = (supplyBPY + borrowBPY) / 2;
 
-        uint256 supplyP2PSPY = (meanSPY * 9000) / MAX_BASIS_POINTS;
-        uint256 borrowP2PSPY = (meanSPY * 11000) / MAX_BASIS_POINTS;
+        uint256 supplyP2PBPY = (meanBPY * 9000) / MAX_BASIS_POINTS;
+        uint256 borrowP2PBPY = (meanBPY * 11000) / MAX_BASIS_POINTS;
 
-        uint256 newSupplyExRate = RAY.rayMul(computeCompoundedInterest(supplyP2PSPY, 365 days));
-        uint256 newBorrowExRate = RAY.rayMul(computeCompoundedInterest(borrowP2PSPY, 365 days));
+        uint256 newSupplyExRate = WAD.mul(_computeCompoundedInterest(supplyP2PBPY, 100));
+        uint256 newBorrowExRate = WAD.mul(_computeCompoundedInterest(borrowP2PBPY, 100));
 
-        uint256 expectedFees = (50 * WAD).rayMul(newBorrowExRate) -
-            (50 * WAD).rayMul(newSupplyExRate);
+        uint256 expectedFees = (50 * WAD).mul(newBorrowExRate) - (50 * WAD).mul(newSupplyExRate);
 
-        hevm.warp(block.timestamp + (365 days));
+        hevm.roll(block.number + 100);
 
-        supplier1.repay(aDai, type(uint256).max);
-        positionsManager.claimToTreasury(aDai);
+        supplier1.repay(cDai, type(uint256).max);
+        positionsManager.claimToTreasury(cDai);
         uint256 balanceAfter = IERC20(dai).balanceOf(positionsManager.treasuryVault());
         uint256 gainedByDAO = balanceAfter - balanceBefore;
 
@@ -83,20 +77,20 @@ contract TestFees is TestSetup {
 
     // DAO should not collect fees when factor is null
     function test_claim_nothing() public {
-        marketsManager.setReserveFactor(aDai, 0);
+        marketsManager.setReserveFactor(cDai, 0);
 
         // Increase time so that rates update
         hevm.warp(block.timestamp + 1);
 
         uint256 balanceBefore = IERC20(dai).balanceOf(positionsManager.treasuryVault());
         supplier1.approve(dai, type(uint256).max);
-        supplier1.supply(aDai, 100 * WAD);
-        supplier1.borrow(aDai, 50 * WAD);
+        supplier1.supply(cDai, 100 * WAD);
+        supplier1.borrow(cDai, 50 * WAD);
 
         hevm.warp(block.timestamp + (365 days));
 
-        supplier1.repay(aDai, type(uint256).max);
-        positionsManager.claimToTreasury(aDai);
+        supplier1.repay(cDai, type(uint256).max);
+        positionsManager.claimToTreasury(cDai);
         uint256 balanceAfter = IERC20(dai).balanceOf(positionsManager.treasuryVault());
 
         testEquality(balanceBefore, balanceAfter);
