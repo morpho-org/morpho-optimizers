fe026b346d7624785871e2d82dc2b7a5dc124815
MerlinEgalite <44097430+MerlinEgalite@users.noreply.github.com>
2022-04-01 11:14:37 +0200

    🎨 (rates computation) small improvements

--- contracts/aave/InterestRatesV1.sol
+++ contracts/aave/InterestRatesV1.sol
@@ -16,17 +16,17 @@ contract InterestRatesV1 is IInterestRates {
     /// @param _poolSupplyRate The market's supply rate on the pool (in ray).
     /// @param _poolBorrowRate The market's borrow rate on the pool (in ray).
     /// @param _reserveFactor The markets's reserve factor (in basis points).
-    /// @return p2pSupplyRate The market's supply rate in P2P (in ray).
-    /// @return p2pBorrowRate The market's borrow rate in P2P (in ray).
+    /// @return p2pSupplyRate_ The market's supply rate in P2P (in ray).
+    /// @return p2pBorrowRate_ The market's borrow rate in P2P (in ray).
     function computeRates(
         uint256 _poolSupplyRate,
         uint256 _poolBorrowRate,
         uint256 _reserveFactor
-    ) external pure override returns (uint256 p2pSupplyRate, uint256 p2pBorrowRate) {
-        // rate = 2/3 * poolSupplyRate + 1/3 * poolBorrowRate, as there are more suppliers than borrowers.
+    ) external pure override returns (uint256 p2pSupplyRate_, uint256 p2pBorrowRate_) {
+        // rate = 2/3 * poolSupplyRate + 1/3 * poolBorrowRate.
         uint256 rate = (2 * _poolSupplyRate + _poolBorrowRate) / 3;
 
-        p2pSupplyRate = rate - (_reserveFactor * (rate - _poolSupplyRate)) / MAX_BASIS_POINTS;
-        p2pBorrowRate = rate + (_reserveFactor * (_poolBorrowRate - rate)) / MAX_BASIS_POINTS;
+        p2pSupplyRate_ = rate - (_reserveFactor * (rate - _poolSupplyRate)) / MAX_BASIS_POINTS;
+        p2pBorrowRate_ = rate + (_reserveFactor * (_poolBorrowRate - rate)) / MAX_BASIS_POINTS;
     }
 }
