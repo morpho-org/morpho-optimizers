e309cf31ea6c8d9ab61ed0aedff1e6ff94b6bf39
MerlinEgalite <44097430+MerlinEgalite@users.noreply.github.com>
2022-04-05 23:36:21 +0200

    🐛 (swap manager V2) fix consult function

--- test-foundry/common/uniswap/UniswapV2PoolCreator.sol
+++ test-foundry/common/uniswap/UniswapV2PoolCreator.sol
@@ -13,7 +13,8 @@ contract UniswapV2PoolCreator {
         IUniswapV2Router02(0x60aE616a2155Ee3d9A68541Ba4544862310933d4); // JoeRouter.
     address public constant WETH9 = 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7; // Intermediate token address.
 
-    uint256 public amount0ToMint = 10 ether;
+    // Destabilize the pool to make sire the swap manager is working fine.
+    uint256 public amount0ToMint = 0.001 ether;
     uint256 public amount1ToMint = 10 ether;
 
     function createPoolAndAddLiquidity(address _token0)
