4a49fc5f9cb80e19181af97928e5c5ecee4233f5
Philippe Gonday <philippe@gonday.com>
2022-04-07 20:04:26 +0200

    ✨ Add a view ti get unclaimed rewards

--- contracts/aave/rewards-managers/RewardsManagerForAaveOnMainnetAndAvalanche.sol
+++ contracts/aave/rewards-managers/RewardsManagerForAaveOnMainnetAndAvalanche.sol
@@ -37,4 +37,30 @@ contract RewardsManagerForAaveOnMainnetAndAvalanche is RewardsManagerForAave {
             localData.lastIndex = newIndex;
         }
     }
+
+    /// @inheritdoc RewardsManagerForAave
+    function _getNewIndex(address _asset, uint256 _totalBalance)
+        internal view
+        override
+        returns (uint256 newIndex)
+    {
+        LocalAssetData storage localData = localAssetData[_asset];
+        uint256 blockTimestamp = block.timestamp;
+        uint256 lastTimestamp = localData.lastUpdateTimestamp;
+        if (blockTimestamp == lastTimestamp) return localData.lastIndex;
+        else {
+            (
+                uint256 oldIndex,
+                uint256 emissionPerSecond,
+                uint256 lastTimestampOnAave
+            ) = aaveIncentivesController.getAssetData(_asset);
+
+            newIndex = _getAssetIndex(
+                oldIndex,
+                emissionPerSecond,
+                lastTimestampOnAave,
+                _totalBalance
+            );
+        }
+    }
 }
