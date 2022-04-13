58e9ec2af178aa5e5bf9c2fc66b30670ddc418d8
MerlinEgalite <44097430+MerlinEgalite@users.noreply.github.com>
2022-04-13 17:58:14 +0200

    ✨ (comp rewards) Add COMP rewards manager

--- contracts/compound/interfaces/IRewardsManagerForCompound.sol
+++ contracts/compound/interfaces/IRewardsManagerForCompound.sol
@@ -1,19 +1,45 @@
 // SPDX-License-Identifier: GNU AGPLv3
 pragma solidity 0.8.13;
 
+import "./compound/ICompound.sol";
+
 interface IRewardsManagerForCompound {
-    function accrueUserUnclaimedRewards(address[] calldata, address) external returns (uint256);
+    function claimRewards(address[] calldata, address) external returns (uint256);
 
-    function claimRewards(
-        address[] calldata,
-        uint256,
-        address
-    ) external returns (uint256);
+    function accrueUserUnclaimedRewards(address[] calldata _cTokenAddresses, address)
+        external
+        view
+        returns (uint256);
 
-    function updateUserAssetAndAccruedRewards(
+    function userUnclaimedCompRewards(address) external view returns (uint256);
+
+    function getUserUnclaimedRewards(address[] calldata _cTokenAddresses, address _user)
+        external
+        returns (uint256 unclaimedRewards);
+
+    function compSupplierIndex(address, address) external view returns (uint256);
+
+    function compBorrowerIndex(address, address) external view returns (uint256);
+
+    function localCompSupplyState(address)
+        external
+        view
+        returns (IComptroller.CompMarketState memory);
+
+    function localCompBorrowState(address)
+        external
+        view
+        returns (IComptroller.CompMarketState memory);
+
+    function accrueUserSupplyUnclaimedRewards(
         address,
         address,
-        uint256,
+        uint256
+    ) external;
+
+    function accrueUserBorrowUnclaimedRewards(
+        address,
+        address,
         uint256
     ) external;
 }
