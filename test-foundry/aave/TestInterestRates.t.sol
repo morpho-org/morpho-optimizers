7016c7f9c9be5cd46c236e4ea81b6dbf650dc90c
MathisGD <mathis.gontier_delaunay@telecom-sudparis.eu>
2022-04-13 17:59:42 +0200

    🧪 (exchange rates) test: add basic exchange rate computation test

--- test-foundry/aave/TestInterestRates.t.sol
+++ test-foundry/aave/TestInterestRates.t.sol
@@ -3,4 +3,23 @@ pragma solidity 0.8.13;
 
 import "./setup/TestSetup.sol";
 
-contract TestInterestRates is TestSetup {}
+contract TestInterestRates is TestSetup {
+    function testExchangeRateComputation() public {
+        Types.Params memory params = Types.Params(
+            1 * RAY, // supplyP2pExchangeRate;
+            1 * RAY, // borrowP2pExchangeRate
+            2 * RAY, // poolSupplyExchangeRate;
+            3 * RAY, // poolBorrowExchangeRate;
+            1 * RAY, // lastPoolSupplyExchangeRate;
+            1 * RAY, // lastPoolBorrowExchangeRate;
+            0, // reserveFactor;
+            Types.Delta(0, 0, 0, 0) // delta;
+        );
+
+        (uint256 newSupplyP2PExchangeRate, uint256 newBorrowP2PExchangeRate) = interestRates
+        .computeP2PExchangeRates(params);
+
+        assertEq(newSupplyP2PExchangeRate, (7 * RAY) / 3);
+        assertEq(newBorrowP2PExchangeRate, (7 * RAY) / 3);
+    }
+}
