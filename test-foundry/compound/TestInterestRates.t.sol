97165a810682b6927733320eb19c6df117d5f285
MathisGD <mathis.gontier_delaunay@telecom-sudparis.eu>
2022-04-15 17:06:38 +0200

    🧪 (exchange rates) test: new delta tests

--- test-foundry/compound/TestInterestRates.t.sol
+++ test-foundry/compound/TestInterestRates.t.sol
@@ -22,4 +22,64 @@ contract TestInterestRates is TestSetup {
         assertEq(newSupplyP2PExchangeRate, (7 * WAD) / 3);
         assertEq(newBorrowP2PExchangeRate, (7 * WAD) / 3);
     }
+
+    function testExchangeRateComputationWithReserveFactor() public {
+        Types.Params memory params = Types.Params(
+            1 * WAD, // supplyP2PExchangeRate;
+            1 * WAD, // borrowP2PExchangeRate
+            2 * WAD, // poolSupplyExchangeRate;
+            3 * WAD, // poolBorrowExchangeRate;
+            1 * WAD, // lastPoolSupplyExchangeRate;
+            1 * WAD, // lastPoolBorrowExchangeRate;
+            5_000, // reserveFactor;
+            Types.Delta(0, 0, 0, 0) // delta;
+        );
+
+        (uint256 newSupplyP2PExchangeRate, uint256 newBorrowP2PExchangeRate) = interestRates
+        .computeP2PExchangeRates(params);
+
+        assertEq(newSupplyP2PExchangeRate, ((2 * 2 + 1 * 3) * WAD) / 3 / 2 + (2 * WAD) / 2);
+        assertEq(newBorrowP2PExchangeRate, ((2 * 2 + 1 * 3) * WAD) / 3 / 2 + (3 * WAD) / 2);
+    }
+
+    function testExchangeRateComputationWithDelta() public {
+        Types.Params memory params = Types.Params(
+            1 * WAD, // supplyP2PExchangeRate;
+            1 * WAD, // borrowP2PExchangeRate
+            2 * WAD, // poolSupplyExchangeRate;
+            3 * WAD, // poolBorrowExchangeRate;
+            1 * WAD, // lastPoolSupplyExchangeRate;
+            1 * WAD, // lastPoolBorrowExchangeRate;
+            0, // reserveFactor;
+            Types.Delta(1 * WAD, 1 * WAD, 4 * WAD, 6 * WAD) // delta;
+        );
+
+        (uint256 newSupplyP2PExchangeRate, uint256 newBorrowP2PExchangeRate) = interestRates
+        .computeP2PExchangeRates(params);
+
+        console.log(newSupplyP2PExchangeRate);
+        console.log(newBorrowP2PExchangeRate);
+
+        assertEq(newSupplyP2PExchangeRate, ((7 * WAD) / 3 + 2 * WAD) / 2);
+        assertEq(newBorrowP2PExchangeRate, ((7 * WAD) / 3 + 3 * WAD) / 2);
+    }
+
+    function testExchangeRateComputationWithDeltaAndReserveFactor() public {
+        Types.Params memory params = Types.Params(
+            1 * WAD, // supplyP2PExchangeRate;
+            1 * WAD, // borrowP2PExchangeRate
+            2 * WAD, // poolSupplyExchangeRate;
+            3 * WAD, // poolBorrowExchangeRate;
+            1 * WAD, // lastPoolSupplyExchangeRate;
+            1 * WAD, // lastPoolBorrowExchangeRate;
+            5_000, // reserveFactor;
+            Types.Delta(1 * WAD, 1 * WAD, 4 * WAD, 6 * WAD) // delta;
+        );
+
+        (uint256 newSupplyP2PExchangeRate, uint256 newBorrowP2PExchangeRate) = interestRates
+        .computeP2PExchangeRates(params);
+
+        assertEq(newSupplyP2PExchangeRate, (((7 * WAD) / 3 + 2 * WAD) / 2 + 2 * WAD) / 2);
+        assertEq(newBorrowP2PExchangeRate, (((7 * WAD) / 3 + 3 * WAD) / 2 + 3 * WAD) / 2);
+    }
 }
