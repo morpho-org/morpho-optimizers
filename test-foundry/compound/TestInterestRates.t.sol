20a0e83c1fba384871ab5d4ff161fe5206ba1977
MathisGD <mathis.gontier_delaunay@telecom-sudparis.eu>
2022-04-20 13:26:57 +0200

    🔨 (fix ER tests) add fuzzing test

--- test-foundry/compound/TestInterestRates.t.sol
+++ test-foundry/compound/TestInterestRates.t.sol
@@ -118,4 +118,52 @@ contract TestInterestRates is TestSetup {
         assertApproxEq(newSupplyP2PExchangeRate, expectedNewSupplyP2PExchangeRate, 1);
         assertApproxEq(newBorrowP2PExchangeRate, expectedNewBorrowP2PExchangeRate, 1);
     }
+
+    function testFuzzInterestRates(
+        uint64 _1,
+        uint64 _2,
+        uint64 _3,
+        uint64 _4,
+        uint64 _5,
+        uint64 _6,
+        uint16 _7,
+        uint64 _8,
+        uint64 _9,
+        uint64 _10,
+        uint64 _11
+    ) public {
+        uint256 _supplyP2PExchangeRate = WAD + _1;
+        uint256 _borrowP2PExchangeRate = WAD + _2;
+        uint256 _poolSupplyExchangeRate = WAD + _3;
+        uint256 _poolBorrowExchangeRate = WAD + _4;
+        uint256 _lastPoolSupplyExchangeRate = WAD + _5;
+        uint256 _lastPoolBorrowExchangeRate = WAD + _6;
+        uint256 _reserveFactor = _7 % 10_000;
+        uint256 _supplyP2PDelta = WAD + _8;
+        uint256 _borrowP2PDelta = WAD + _9;
+        uint256 _supplyP2PAmount = WAD + _10;
+        uint256 _borrowP2PAmount = WAD + _11;
+
+        hevm.assume(_lastPoolSupplyExchangeRate <= _poolSupplyExchangeRate); // prettier-ignore
+        hevm.assume(_lastPoolBorrowExchangeRate <= _poolBorrowExchangeRate); // prettier-ignore
+        hevm.assume(_poolBorrowExchangeRate * WAD / _lastPoolBorrowExchangeRate > _poolSupplyExchangeRate * WAD / _lastPoolSupplyExchangeRate); // prettier-ignore
+        hevm.assume(_supplyP2PAmount * _supplyP2PExchangeRate / WAD > _supplyP2PDelta * _poolSupplyExchangeRate / WAD); // prettier-ignore
+        hevm.assume(_borrowP2PAmount * _borrowP2PExchangeRate / WAD > _borrowP2PDelta * _poolBorrowExchangeRate / WAD); // prettier-ignore
+
+        Types.Params memory params = Types.Params(
+            _supplyP2PExchangeRate,
+            _borrowP2PExchangeRate,
+            _poolSupplyExchangeRate,
+            _poolBorrowExchangeRate,
+            _lastPoolSupplyExchangeRate,
+            _lastPoolBorrowExchangeRate,
+            _reserveFactor,
+            Types.Delta(_supplyP2PDelta, _borrowP2PDelta, _supplyP2PAmount, _borrowP2PAmount)
+        );
+
+        (uint256 newSupplyP2PExchangeRate, uint256 newBorrowP2PExchangeRate) = interestRates.computeP2PExchangeRates(params); // prettier-ignore
+        (uint256 expectedNewSupplyP2PExchangeRate, uint256 expectedNewBorrowP2PExchangeRate) = computeP2PExchangeRates(params); // prettier-ignore
+        assertApproxEq(newSupplyP2PExchangeRate, expectedNewSupplyP2PExchangeRate, 300); // prettier-ignore
+        assertApproxEq(newBorrowP2PExchangeRate, expectedNewBorrowP2PExchangeRate, 300); // prettier-ignore
+    }
 }
