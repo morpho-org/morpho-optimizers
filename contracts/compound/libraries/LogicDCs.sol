e46553dd6eeb8ef908adb15d966f35a40881ff87
MathisGD <mathis.gontier_delaunay@telecom-sudparis.eu>
2022-04-21 15:40:07 +0200

    🏋️ (size refactor) move logic and setup delegate calls

---
+++ contracts/compound/libraries/LogicDCs.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: GNU AGPLv3
+pragma solidity 0.8.13;
+
+import "../interfaces/ILogicForCompound.sol";
+
+import "@openzeppelin/contracts/utils/Address.sol";
+
+library LogicDCs {
+    using Address for address;
+
+    function _supplyDC(
+        ILogicForCompound _logic,
+        address _poolTokenAddress,
+        uint256 _amount,
+        uint256 _maxGasToConsume
+    ) internal {
+        address(_logic).functionDelegateCall(
+            abi.encodeWithSelector(
+                _logic.supply.selector,
+                _poolTokenAddress,
+                _amount,
+                _maxGasToConsume
+            )
+        );
+    }
+
+    function _borrowDC(
+        ILogicForCompound _logic,
+        address _poolTokenAddress,
+        uint256 _amount,
+        uint256 _maxGasToConsume
+    ) internal {
+        address(_logic).functionDelegateCall(
+            abi.encodeWithSelector(
+                _logic.borrow.selector,
+                _poolTokenAddress,
+                _amount,
+                _maxGasToConsume
+            )
+        );
+    }
+
+    function _withdrawDC(
+        ILogicForCompound _logic,
+        address _poolTokenAddress,
+        uint256 _amount,
+        address _supplier,
+        address _receiver,
+        uint256 _maxGasToConsume
+    ) internal {
+        address(_logic).functionDelegateCall(
+            abi.encodeWithSelector(
+                _logic.withdraw.selector,
+                _poolTokenAddress,
+                _amount,
+                _supplier,
+                _receiver,
+                _maxGasToConsume
+            )
+        );
+    }
+
+    function _repayDC(
+        ILogicForCompound _logic,
+        address _poolTokenAddress,
+        address _user,
+        uint256 _amount,
+        uint256 _maxGasToConsume
+    ) internal {
+        address(_logic).functionDelegateCall(
+            abi.encodeWithSelector(
+                _logic.repay.selector,
+                _poolTokenAddress,
+                _user,
+                _amount,
+                _maxGasToConsume
+            )
+        );
+    }
+
+    function _checkUserLiquidityDC(
+        ILogicForCompound _logic,
+        address _user,
+        address _poolTokenAddress,
+        uint256 _withdrawnAmount,
+        uint256 _borrowedAmount
+    ) internal {
+        address(_logic).functionDelegateCall(
+            abi.encodeWithSelector(
+                _logic.checkUserLiquidity.selector,
+                _user,
+                _poolTokenAddress,
+                _withdrawnAmount,
+                _borrowedAmount
+            )
+        );
+    }
+}
