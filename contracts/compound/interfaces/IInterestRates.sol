21802fe92a82eff31b9693f3cf7935ce17bf9c3f
MerlinEgalite <44097430+MerlinEgalite@users.noreply.github.com>
2022-04-22 19:52:18 +0200

    ⚡️ (enhancement) Improve gas consumption in rates computation

--- contracts/compound/interfaces/IInterestRates.sol
+++ contracts/compound/interfaces/IInterestRates.sol
@@ -8,4 +8,14 @@ interface IInterestRates {
         external
         pure
         returns (uint256 newSupplyP2PExchangeRate, uint256 newBorrowP2PExchangeRate);
+
+    function computeSupplyP2PExchangeRate(Types.Params memory _params)
+        external
+        view
+        returns (uint256);
+
+    function computeBorrowP2PExchangeRate(Types.Params memory _params)
+        external
+        view
+        returns (uint256);
 }
