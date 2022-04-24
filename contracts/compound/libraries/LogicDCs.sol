446b5100c669aa0a9bcbeec39de5a9e047def18a
MathisGD <mathis.gontier_delaunay@telecom-sudparis.eu>
2022-04-24 13:04:52 +0200

    📃 (size refactor) delegate calls library natspec comments

--- contracts/compound/libraries/LogicDCs.sol
+++ contracts/compound/libraries/LogicDCs.sol
@@ -8,6 +8,11 @@ import "@openzeppelin/contracts/utils/Address.sol";
 library LogicDCs {
     using Address for address;
 
+    /// @notice Delegate calls the supply function of the logic contract.
+    /// @param _logic The logic contract.
+    /// @param _poolTokenAddress The address of the pool token the user wants to interact with.
+    /// @param _amount The amount of token (in underlying).
+    /// @param _maxGasToConsume The maximum amount of gas to consume within a matching engine loop.
     function _supplyDC(
         ILogic _logic,
         address _poolTokenAddress,
@@ -24,6 +29,11 @@ library LogicDCs {
         );
     }
 
+    /// @notice Delegate calls the borrow function of the logic contract.
+    /// @param _logic The logic contract.
+    /// @param _poolTokenAddress The address of the market the user wants to interact with.
+    /// @param _amount The amount of token (in underlying).
+    /// @param _maxGasToConsume The maximum amount of gas to consume within a matching engine loop.
     function _borrowDC(
         ILogic _logic,
         address _poolTokenAddress,
@@ -40,6 +50,13 @@ library LogicDCs {
         );
     }
 
+    /// @notice Delegate calls the withdraw function of the logic contract.
+    /// @param _logic The logic contract.
+    /// @param _poolTokenAddress The address of the market the user wants to interact with.
+    /// @param _amount The amount of token (in underlying).
+    /// @param _supplier The address of the supplier.
+    /// @param _receiver The address of the user who will receive the tokens.
+    /// @param _maxGasToConsume The maximum amount of gas to consume within a matching engine loop.
     function _withdrawDC(
         ILogic _logic,
         address _poolTokenAddress,
@@ -60,6 +77,12 @@ library LogicDCs {
         );
     }
 
+    /// @notice Delegate calls the repay function of the logic contract.
+    /// @param _logic The logic contract.
+    /// @param _poolTokenAddress The address of the market the user wants to interact with.
+    /// @param _user The address of the user.
+    /// @param _amount The amount of token (in underlying).
+    /// @param _maxGasToConsume The maximum amount of gas to consume within a matching engine loop.
     function _repayDC(
         ILogic _logic,
         address _poolTokenAddress,
@@ -78,6 +101,12 @@ library LogicDCs {
         );
     }
 
+    /// @notice Delegate calls the liquidate function of the logic contract.
+    /// @param _logic The logic contract.
+    /// @param _poolTokenBorrowedAddress The address of the pool token the liquidator wants to repay.
+    /// @param _poolTokenCollateralAddress The address of the collateral pool token the liquidator wants to seize.
+    /// @param _borrower The address of the borrower to liquidate.
+    /// @param _amount The amount of token (in underlying) to repay.
     function _liquidateDC(
         ILogic _logic,
         address _poolTokenBorrowedAddress,
