eebeff44af78dc8ae18cad3ad8a76f60444924c1
MathisGD <mathis.gontier_delaunay@telecom-sudparis.eu>
2022-04-24 15:15:11 +0200

    🧼 (size refactor) switch matching engine functions to internal

--- contracts/compound/positions-manager-parts/PositionsManagerStorage.sol
+++ contracts/compound/positions-manager-parts/PositionsManagerStorage.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.13;
 
 import "../interfaces/IMarketsManager.sol";
-import "../interfaces/IMatchingEngine.sol";
 import "../interfaces/IRewardsManager.sol";
 import "../interfaces/ILogic.sol";
 import "../interfaces/compound/ICompound.sol";
