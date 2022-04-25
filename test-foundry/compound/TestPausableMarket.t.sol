cbc7c3326cf0338d1a035d0a8179f15a5bb45b1f
MerlinEgalite <44097430+MerlinEgalite@users.noreply.github.com>
2022-04-25 22:44:13 +0200

    ✅ (partial pause) Update and add test for partial pause

--- test-foundry/compound/TestPausableMarket.t.sol
+++ test-foundry/compound/TestPausableMarket.t.sol
@@ -11,18 +11,40 @@ contract TestPausableMarket is TestSetup {
         supplier1.setPauseStatus(dai);
 
         positionsManager.setPauseStatus(dai);
-        assertTrue(positionsManager.paused(dai), "paused is false");
+        (bool isPaused, ) = positionsManager.pauseStatus(dai);
+        assertTrue(isPaused, "paused is false");
+    }
+
+    function testOnlyOwnerShouldTriggerPartialPauseFunction() public {
+        hevm.expectRevert("Ownable: caller is not the owner");
+        supplier1.setPartialPauseStatus(dai);
+
+        positionsManager.setPartialPauseStatus(dai);
+        (, bool isPartialPaused) = positionsManager.pauseStatus(dai);
+        assertTrue(isPartialPaused, "partial paused is false");
     }
 
     function testPauseUnpause() public {
         positionsManager.setPauseStatus(dai);
-        assertTrue(positionsManager.paused(dai), "paused is false");
+        (bool isPaused, ) = positionsManager.pauseStatus(dai);
+        assertTrue(isPaused, "paused is false");
 
         positionsManager.setPauseStatus(dai);
-        assertFalse(positionsManager.paused(dai), "paused is true");
+        (isPaused, ) = positionsManager.pauseStatus(dai);
+        assertFalse(isPaused, "paused is true");
     }
 
-    function testShouldTriggerFunctionsWhenNotPaused() public {
+    function testPartialPausePartialUnpause() public {
+        positionsManager.setPartialPauseStatus(dai);
+        (, bool isPartialPaused) = positionsManager.pauseStatus(dai);
+        assertTrue(isPartialPaused, "partial paused is false");
+
+        positionsManager.setPartialPauseStatus(dai);
+        (, isPartialPaused) = positionsManager.pauseStatus(dai);
+        assertFalse(isPartialPaused, "partial paused is true");
+    }
+
+    function testShouldTriggerAllFunctionsWhenNotPausedAndNotPartialPaused() public {
         uint256 amount = 10_000 ether;
         uint256 toBorrow = to6Decimals(amount / 2);
 
@@ -54,8 +76,8 @@ contract TestPausableMarket is TestSetup {
         positionsManager.claimToTreasury(cDai);
     }
 
-    function testShouldNotTriggerFunctionsWhenPaused() public {
-        uint256 amount = 10000 ether;
+    function testShouldNotTriggerAnyFunctionWhenPaused() public {
+        uint256 amount = 10_000 ether;
 
         supplier1.approve(dai, 2 * amount);
         supplier1.supply(cDai, amount);
@@ -85,7 +107,7 @@ contract TestPausableMarket is TestSetup {
         SimplePriceOracle customOracle = createAndSetCustomPriceOracle();
         customOracle.setUnderlyingPrice(cDai, (oracle.getUnderlyingPrice(cDai) * 95) / 100);
 
-        uint256 toLiquidate = (toBorrow - 2) / 2; // Minus 2 only due to roundings.
+        uint256 toLiquidate = toBorrow / 3; // Minus 2 only due to roundings.
         User liquidator = borrower3;
         liquidator.approve(usdc, toLiquidate);
 
@@ -93,6 +115,46 @@ contract TestPausableMarket is TestSetup {
         liquidator.liquidate(cUsdc, cDai, address(supplier1), toLiquidate);
 
         hevm.expectRevert(abi.encodeWithSignature("MarketPaused()"));
+        positionsManager.claimToTreasury(cDai);
+    }
+
+    function testShouldOnlyTriggerSpecificFunctionsWhenPartialPaused() public {
+        // Specific functions are repay, withdraw and liquidate.
+        uint256 amount = 10_000 ether;
+
+        supplier1.approve(dai, 2 * amount);
+        supplier1.supply(cDai, amount);
+
+        (, uint256 toBorrow) = positionsManager.getUserMaxCapacitiesForAsset(
+            address(supplier1),
+            cUsdc
+        );
+        supplier1.borrow(cUsdc, toBorrow);
+
+        positionsManager.setPartialPauseStatus(cDai);
+        positionsManager.setPartialPauseStatus(cUsdc);
+
+        hevm.expectRevert(abi.encodeWithSignature("MarketPaused()"));
+        supplier1.supply(cDai, amount);
+
+        hevm.expectRevert(abi.encodeWithSignature("MarketPaused()"));
+        supplier1.borrow(cUsdc, 1);
+
+        supplier1.approve(usdc, toBorrow);
+        supplier1.repay(cUsdc, 1e6);
+        supplier1.withdraw(cDai, 1 ether);
+
+        // Change Oracle
+        SimplePriceOracle customOracle = createAndSetCustomPriceOracle();
+        customOracle.setUnderlyingPrice(cDai, (oracle.getUnderlyingPrice(cDai) * 97) / 100);
+
+        uint256 toLiquidate = toBorrow / 3;
+        User liquidator = borrower3;
+        liquidator.approve(usdc, toLiquidate);
+        liquidator.liquidate(cUsdc, cDai, address(supplier1), toLiquidate);
+
+        // Does not revert because the market is paused.
+        hevm.expectRevert(abi.encodeWithSignature("AmountIsZero()"));
         positionsManager.claimToTreasury(cDai);
     }
 }
