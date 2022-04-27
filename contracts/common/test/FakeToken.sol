b375a2ed666d8c105d6600e930379325d76b1272
MerlinEgalite <44097430+MerlinEgalite@users.noreply.github.com>
2022-04-27 19:57:37 +0200

    ✅  (rewards-distribution) Add ts tests to create merkle tree

---
+++ contracts/common/test/FakeToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GNU AGPLv3
+pragma solidity 0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract FakeToken is ERC20 {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _receiver
+    ) ERC20(_name, _symbol) {
+        _mint(_receiver, 10_000 ether);
+    }
+}
