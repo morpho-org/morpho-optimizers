bc3a7b528997bcd1842fbdf081da52b4bd26e5c5
MerlinEgalite <44097430+MerlinEgalite@users.noreply.github.com>
2022-04-28 17:33:42 +0200

    🎨 (incentives-vault) Add docs, events and comments

--- contracts/compound/comp-rewards/IncentivesVault.sol
+++ contracts/compound/comp-rewards/IncentivesVault.sol
@@ -19,23 +19,46 @@ contract IncentivesVault is Ownable {
     address public immutable morphoToken; // The address of the MORPHO token.
     address public morphoDao; // The address of the Morpho DAO treasury.
     address public oracle; // Thre oracle used to get the price of the pair MORPHO/COMP 🦋.
-    uint256 public bonus; // The bonus of MORPHO tokens to give to the user (in basis point).
+    uint256 public bonus; // The bonus of MORPHO tokens to give to the user as a percentage to add on top of the consulted amount on the oracle (in basis point).
     bool public isActive; // Whether the swith of COMP rewards to MORPHO rewards is active or not.
 
     /// EVENTS ///
 
+    /// @notice Emitted when the oracle is set.
     event OracleSet(address _newOracle);
+
+    /// @notice Emitted when the Morpho DAO is set.
     event MorphoDaoSet(address _newMorphoDao);
+
+    /// @notice Emitted when the reward bonus is set.
     event BonusSet(uint256 _newBonus);
+
+    /// @notice Emitted when the activation status is changed.
     event ActivationStatusChanged(bool _newStatus);
+
+    /// @notice Emitted when MORPHO tokens are transferred to the DAO.
+    event MorphoTokensTransferred(uint256 _amount);
+
+    /// @notice Emitted when COMP tokens are switched to MORPHO tokens.
+    /// @param _receiver The address of the receiver.
+    /// @param _compAmount The amount of COMP switched.
+    /// @param _morphoAmount The amount of MORPHO sent.
+    event CompTokensSwitched(address indexed _receiver, uint256 _compAmount, uint256 _morphoAmount);
 
     /// ERRROS ///
 
+    /// @notice Thrown when only the posiyions manager can trigger the function.
     error OnlyPositionsManager();
+
+    /// @notice Thrown when the vault is not active.
     error VaultNotActive();
 
     /// CONSTRUCTOR ///
 
+    /// @notice Constructs the IncentivesVault contract.
+    /// @param _positionsManager The address of the Positions Manager.
+    /// @param _morphoToken The address of the MORPHO token.
+    /// @param _oracle The adress of the oracle.
     constructor(
         address _positionsManager,
         address _morphoToken,
@@ -46,38 +69,56 @@ contract IncentivesVault is Ownable {
         oracle = _oracle;
     }
 
+    /// @notice Sets the oracle.
+    /// @param _newOracle The address of the new oracle.
     function setOracle(address _newOracle) external onlyOwner {
         oracle = _newOracle;
         emit OracleSet(_newOracle);
     }
 
+    /// @notice Sets the morho DAO.
+    /// @param _newMorphoDao The address of the Morpho DAO.
     function setMorphoDao(address _newMorphoDao) external onlyOwner {
         morphoDao = _newMorphoDao;
         emit MorphoDaoSet(_newMorphoDao);
     }
 
+    /// @notice Sets the reward bonus.
+    /// @param _newBonus The new reward bonus.
     function setBonus(uint256 _newBonus) external onlyOwner {
         bonus = _newBonus;
         emit BonusSet(_newBonus);
     }
 
+    /// @notice Toggles the activation status.
     function toggleActivation() external onlyOwner {
         bool newStatus = !isActive;
         isActive = newStatus;
         emit ActivationStatusChanged(newStatus);
     }
 
+    /// @notice Transfers the MORPHO tokens to the DAO.
+    /// @param _amount The amount of MORPHO tokens to transfer to the DAO.
     function transferMorphoTokensToDao(uint256 _amount) external onlyOwner {
         ERC20(morphoToken).transfer(morphoDao, _amount);
+        emit MorphoTokensTransferred(_amount);
     }
 
-    function convertCompToMorphoTokens(address _to, uint256 _amount) external {
+    /// @notice Converts COMP tokens to MORPHO tokens and sends them to the receiver.
+    /// @param _receiver The address of the receiver.
+    /// @param _amount The amount to transfer to the receiver.
+    function convertCompToMorphoTokens(address _receiver, uint256 _amount) external {
         if (msg.sender != positionsManager) revert OnlyPositionsManager();
         if (!isActive) revert VaultNotActive();
 
+        // Transfer COMP to the DAO.
         ERC20(COMP).safeTransferFrom(msg.sender, morphoDao, _amount);
+
+        // Add a bonus on MORPHO rewards.
         uint256 amountOut = (IOracle(oracle).consult(_amount) * (MAX_BASIS_POINTS + bonus)) /
             MAX_BASIS_POINTS;
-        ERC20(morphoToken).transfer(_to, amountOut);
+        ERC20(morphoToken).transfer(_receiver, amountOut);
+
+        emit CompTokensSwitched(_receiver, _amount, amountOut);
     }
 }
