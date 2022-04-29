19b9cb93ac68802ef87d3be94363d0b510d4eba9
gabriel <gabriel.astieres@gmail.com>
2022-04-29 19:18:53 +0200

    ✅(fuzzing) work on supply fuzzing

--- test-foundry/fuzzing/compound/TestSupplyFuzzing.t.sol
+++ test-foundry/fuzzing/compound/TestSupplyFuzzing.t.sol
@@ -6,7 +6,14 @@ import "./TestSetupFuzzing.sol";
 contract TestSupplyFuzzing is TestSetupFuzzing {
     using CompoundMath for uint256;
 
-    function testSupplyFuzzed(uint128 _amount, uint8 _asset) public {
+    struct AssetVars {
+        address suppliedAsset;
+        address suppliedUnderlying;
+        address borrowedAsset;
+        address borrowedUnderlying;
+    }
+
+    function testSupply1Fuzzed(uint128 _amount, uint8 _asset) public {
         (address asset, address underlying) = getAsset(_asset);
 
         uint256 amount = _amount;
@@ -34,5 +41,44 @@ contract TestSupplyFuzzing is TestSetupFuzzing {
         assertApproxEq(onPool, ICToken(asset).balanceOf(address(positionsManager)), 5, "on pool");
         assertApproxEq(onPool, expectedOnPool, 5, "on pool");
         assertEq(inP2P, 0, "in P2P");
+    }
+
+    function testSupply2Fuzzed(
+        uint128 _suppliedAmount,
+        uint8 _randomModulo,
+        uint8 _suppliedAsset,
+        uint8 _borrowedAsset
+    ) public {
+        AssetVars memory vars;
+
+        (vars.suppliedAsset, vars.suppliedUnderlying) = getAsset(_suppliedAsset);
+        (vars.borrowedAsset, vars.borrowedUnderlying) = getAsset(_borrowedAsset);
+
+        uint256 suppliedAmount = _suppliedAmount;
+
+        hevm.assume(
+            suppliedAmount > 0 &&
+                suppliedAmount <=
+                2 * ERC20(vars.suppliedUnderlying).balanceOf(address(borrower1)) &&
+                _randomModulo > 0
+        );
+
+        borrower1.approve(vars.suppliedUnderlying, suppliedAmount);
+        borrower1.supply(vars.suppliedAsset, suppliedAmount);
+
+        (, uint256 borrowable) = positionsManager.getUserMaxCapacitiesForAsset(
+            address(borrower1),
+            vars.borrowedAsset
+        );
+        uint256 borrowedAmount = (borrowable * _randomModulo) / 255;
+
+        hevm.assume(
+            borrowedAmount > 0 &&
+                borrowedAmount <= ERC20(vars.borrowedUnderlying).balanceOf(address(borrower1))
+        );
+        borrower1.borrow(vars.borrowedAsset, borrowedAmount);
+
+        borrower1.approve(vars.suppliedUnderlying, suppliedAmount);
+        borrower1.supply(vars.suppliedAsset, suppliedAmount);
     }
 }
