9e7bf8c3bbac1300ae8f1fad5270b1d88ab3d7bf
gabriel <gabriel.astieres@gmail.com>
2022-04-29 19:18:53 +0200

    ✅(fuzzing) work on supply fuzzing

--- test-foundry/fuzzing/compound/TestBorrowFuzzing.t.sol
+++ test-foundry/fuzzing/compound/TestBorrowFuzzing.t.sol
@@ -50,10 +50,10 @@ contract TestBorrow is TestSetupFuzzing {
         uint8 _borrowedAsset
     ) public {
         (address suppliedAsset, address suppliedUnderlying) = getAsset(_suppliedAsset);
-        (address borrowedAsset, address borrowedUnderlying) = getAsset(_borrowedAsset);
+        (address borrowedAsset, ) = getAsset(_borrowedAsset);
 
-        uint256 balanceSuppliedBefore = ERC20(suppliedUnderlying).balanceOf(address(borrower1));
-        uint256 balanceBorrowedBefore = ERC20(borrowedUnderlying).balanceOf(address(borrower1));
+        // uint256 balanceSuppliedBefore = ERC20(suppliedUnderlying).balanceOf(address(borrower1));
+        // uint256 balanceBorrowedBefore = ERC20(borrowedUnderlying).balanceOf(address(borrower1));
 
         hevm.assume(
             amountSupplied != 0 &&
@@ -71,8 +71,8 @@ contract TestBorrow is TestSetupFuzzing {
             borrowedAsset
         );
 
-        uint256 balanceSuppliedAfter = ERC20(suppliedUnderlying).balanceOf(address(borrower1));
-        uint256 balanceBorrowedAfter = ERC20(borrowedUnderlying).balanceOf(address(borrower1));
+        // uint256 balanceSuppliedAfter = ERC20(suppliedUnderlying).balanceOf(address(borrower1));
+        // uint256 balanceBorrowedAfter = ERC20(borrowedUnderlying).balanceOf(address(borrower1));
 
         hevm.assume(amountBorrowed <= borrowable);
         borrower1.borrow(borrowedAsset, amountBorrowed);
