7ba9defe5dcf917d4e2562cc632cd668f15a9303
MerlinEgalite <44097430+MerlinEgalite@users.noreply.github.com>
2022-05-05 10:13:37 +0200

    🔥 (fix-ci) Remove dead files and dependencies

--- .solcover.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  skipFiles: [
-    'interfaces/compound/ICompound.sol',
-    'interfaces/IMarketsManagerForCompLike.sol',
-    'interfaces/IPositionsManagerForCompLike.sol',
-    'libraries/RedBlackBinaryTree.sol',
-    'test/SimplePriceOracle.sol',
-  ],
-};
