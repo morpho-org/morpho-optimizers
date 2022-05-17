d17f8f9e13a1c79fa845d1e17f71f36d3e9c6969
Quentin Garchery <garchery.quentin@gmail.com>
2022-05-17 09:36:59 +0200

    🐛 (heap-queue) test debug and other fixes

--- contracts/common/libraries/Heap.sol
+++ contracts/common/libraries/Heap.sol
@@ -12,19 +12,23 @@ library BasicHeap {
         mapping(address => uint256) indexes;
     }
 
-    function length(Heap storage heap) public view returns (uint256) {
+    function length(Heap storage heap) internal view returns (uint256) {
         return heap.accounts.length;
     }
 
-    function getValueOf(Heap storage heap, address _id) public view returns (uint256) {
-        return heap.accounts[heap.indexes[_id] - 1].value;
+    function getValueOf(Heap storage heap, address _id) internal view returns (uint256) {
+        uint256 index = heap.indexes[_id];
+        if (index == 0) return 0;
+        else return heap.accounts[index - 1].value;
     }
 
     function getHead(Heap storage heap) internal view returns (address) {
-        return heap.accounts[1].id;
+        if (heap.accounts.length == 0) return address(0);
+        else return heap.accounts[0].id;
     }
 
     function getTail(Heap storage heap) internal view returns (address) {
+        require(heap.accounts.length > 0, "empty HEAP");
         return heap.accounts[heap.accounts.length - 1].id;
     }
 
@@ -32,11 +36,8 @@ library BasicHeap {
         return heap.accounts[heap.indexes[_id]].id;
     }
 
-    function getPrev(Heap storage heap, address _id) internal view returns (address) {
-        return heap.accounts[heap.indexes[_id] - 2].id;
-    }
-
-    function load(Account[] storage accounts, uint256 index) public view returns (Account storage) {
+    function load(Account[] storage accounts, uint256 index) private view returns (Account memory) {
+        require(1 <= index && index <= accounts.length, "LOAD index out of bounds");
         return accounts[index - 1];
     }
 
@@ -44,7 +45,8 @@ library BasicHeap {
         Account[] storage accounts,
         uint256 index,
         Account memory e
-    ) public {
+    ) private {
+        require(1 <= index && index <= accounts.length, "STORE index out of bounds");
         accounts[index - 1] = e;
     }
 
@@ -52,32 +54,34 @@ library BasicHeap {
         Heap storage heap,
         uint256 index1,
         uint256 index2
-    ) internal {
+    ) private {
+        require(1 <= index1 && index1 <= heap.accounts.length, "SWAP index1 out of bounds");
+        require(1 <= index2 && index2 <= heap.accounts.length, "SWAP index2 out of bounds");
         Account[] storage accounts = heap.accounts;
         mapping(address => uint256) storage indexes = heap.indexes;
-        Account storage account_old_index1 = load(accounts, index1); // TODO : is storage needed here ?
-        Account storage account_old_index2 = load(accounts, index2);
+        Account memory account_old_index1 = load(accounts, index1);
+        Account memory account_old_index2 = load(accounts, index2);
         store(accounts, index1, account_old_index2);
         store(accounts, index2, account_old_index1);
         indexes[account_old_index2.id] = index1;
         indexes[account_old_index1.id] = index2;
     }
 
-    function left(uint256 index) public pure returns (uint256) {
+    function left(uint256 index) private pure returns (uint256) {
         return 2 * index;
     }
 
-    function right(uint256 index) public pure returns (uint256) {
+    function right(uint256 index) private pure returns (uint256) {
         return 2 * index + 1;
     }
 
-    function parent(uint256 index) public pure returns (uint256) {
+    function parent(uint256 index) private pure returns (uint256) {
         return index / 2;
     }
 
-    function siftUp(Heap storage heap, uint256 index) internal {
+    function siftUp(Heap storage heap, uint256 index) private {
         Account[] storage accounts = heap.accounts;
-        require(index > 0 && index <= accounts.length, "index out of bounds (siftUp)");
+        require(index > 0 && index <= accounts.length, "SIFTUP index out of bounds");
         uint256 mother = parent(index);
         while (mother > 0 && load(accounts, index).value > load(accounts, mother).value) {
             swap(heap, index, mother);
@@ -85,9 +89,9 @@ library BasicHeap {
         }
     }
 
-    function siftDown(Heap storage heap, uint256 index) internal {
+    function siftDown(Heap storage heap, uint256 index) private {
         Account[] storage accounts = heap.accounts;
-        require(index > 0 && index <= accounts.length, "index out of bounds (siftUp)");
+        require(index > 0 && index <= accounts.length, "SIFTDOWN index out of bounds");
         uint256 leftIndex;
         uint256 rightIndex;
         uint256 maxIndex;
@@ -110,8 +114,7 @@ library BasicHeap {
         }
     }
 
-    function insertOne(
-        // rename into "insert"
+    function insert(
         Heap storage heap,
         address id,
         uint256 value
@@ -131,7 +134,7 @@ library BasicHeap {
     ) private {
         Account[] storage accounts = heap.accounts;
         uint256 index = heap.indexes[id];
-        Account storage account = load(accounts, index);
+        Account memory account = load(accounts, index);
         account.value = newValue;
         siftDown(heap, index);
     }
@@ -144,20 +147,25 @@ library BasicHeap {
     ) private {
         Account[] storage accounts = heap.accounts;
         uint256 index = heap.indexes[id];
-        Account storage account = load(accounts, index);
+        Account memory account = load(accounts, index);
         account.value = newValue;
         siftUp(heap, index);
     }
 
-    function removeOne(Heap storage heap, address id) private {
-        // TODO : rename into "remove"
+    function remove(Heap storage heap, address id) private {
         // only call when id is in the heap
         Account[] storage accounts = heap.accounts;
         uint256 index = heap.indexes[id];
-        delete heap.indexes[id];
-        swap(heap, index, accounts.length);
-        accounts.pop();
-        siftDown(heap, index);
+        require(index != 0, "remove on non-present element");
+        if (index == accounts.length) {
+            accounts.pop();
+            delete heap.indexes[id];
+        } else {
+            swap(heap, index, accounts.length);
+            accounts.pop();
+            delete heap.indexes[id];
+            siftDown(heap, index);
+        }
     }
 
     function updateHeap(
@@ -167,8 +175,8 @@ library BasicHeap {
         uint256 newValue
     ) internal {
         if (formerValue != newValue) {
-            if (newValue == 0) removeOne(heap, id);
-            else if (formerValue == 0) insertOne(heap, id, newValue);
+            if (newValue == 0) remove(heap, id);
+            else if (formerValue == 0) insert(heap, id, newValue);
             else if (formerValue < newValue) increase(heap, id, newValue);
             else decrease(heap, id, newValue);
         }
