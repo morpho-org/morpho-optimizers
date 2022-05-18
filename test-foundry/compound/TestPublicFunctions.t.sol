7bbcc33df703891b353869f4ab66545dde927b67
MerlinEgalite <44097430+MerlinEgalite@users.noreply.github.com>
2022-05-18 14:19:55 +0200

    ✅ (missing-modifier) Add tests for missing modifier

---
+++ test-foundry/compound/TestPublicFunctions.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GNU AGPLv3
+pragma solidity 0.8.13;
+
+import "./setup/TestSetup.sol";
+
+contract TestPublicFunctions is TestSetup {
+    function testUserShouldNotUpdateP2PIndexesOfMarketNotCreatedYet() public {
+        hevm.prank(address(2));
+        hevm.expectRevert(abi.encodeWithSignature("MarketNotCreated()"));
+        morpho.updateP2PIndexes(cAave);
+    }
+}
