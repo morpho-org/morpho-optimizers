ba15b8bfe0797466bac48337a8bbc7474a90094d
Quentin Garchery <garchery.quentin@gmail.com>
2022-05-21 10:58:01 +0200

    💡 (heap-queue) add NATSPEC comments

--- contracts/common/libraries/Heap.sol
+++ contracts/common/libraries/Heap.sol
@@ -19,32 +19,53 @@ library BasicHeap {
 
     /// VIEW ///
 
+    /// @notice Returns the length of the `_heap`.
+    /// @param _heap The heap parameter.
+    /// @return The length of the heap.
     function length(Heap storage _heap) internal view returns (uint256) {
         return _heap.accounts.length;
     }
 
+    /// @notice Returns the value of the account linked to `_id`.
+    /// @param _heap The heap to search in.
+    /// @param _id The address of the account.
+    /// @return The value of the account.
     function getValueOf(Heap storage _heap, address _id) internal view returns (uint256) {
         uint256 index = _heap.indexes[_id];
         if (index == 0) return 0;
         else return _heap.accounts[index - 1].value;
     }
 
+    /// @notice Returns the address at the head of the `_heap`.
+    /// @param _heap The heap to get the head.
+    /// @return The address of the head.
     function getHead(Heap storage _heap) internal view returns (address) {
         if (_heap.accounts.length > 0) return _heap.accounts[0].id;
         else return address(0);
     }
 
+    /// @notice Returns the address at the tail of the `_heap`.
+    /// @param _heap The heap to get the tail.
+    /// @return The address of the tail.
     function getTail(Heap storage _heap) internal view returns (address) {
         if (_heap.accounts.length > 0) return _heap.accounts[_heap.accounts.length - 1].id;
         else return address(0);
     }
 
+    /// @notice Returns the previous address from the current `_id`.
+    /// @param _heap The heap to search in.
+    /// @param _id The address of the account.
+    /// @return The address of the previous account.
     function getPrev(Heap storage _heap, address _id) internal view returns (address) {
         uint256 index = _heap.indexes[_id];
         if (index > 1) return _heap.accounts[index - 2].id;
         else return address(0);
     }
 
+    /// @notice Returns the next address from the current `_id`.
+    /// @param _heap The heap to search in.
+    /// @param _id The address of the account.
+    /// @return The address of the next account.
     function getNext(Heap storage _heap, address _id) internal view returns (address) {
         uint256 index = _heap.indexes[_id];
         if (index < _heap.accounts.length) return _heap.accounts[index].id;
@@ -53,6 +74,12 @@ library BasicHeap {
 
     /// PRIVATE ///
 
+    /// @notice Swaps two accounts in the `_heap`.
+    /// @dev The heap may lose its invariant about the order of the values stored.
+    /// @dev Only call this function with indexes in the bounds of the array.
+    /// @param _heap The heap to modify.
+    /// @param _index1 The index of the first account in the heap.
+    /// @param _index2 The index of the second account in the heap.
     function swap(
         Heap storage _heap,
         uint256 _index1,
@@ -66,6 +93,10 @@ library BasicHeap {
         _heap.indexes[accountOldIndex1.id] = _index2;
     }
 
+    /// @notice Moves an account up the heap until its value is smaller than its parent's.
+    /// @dev This functions restores the invariant about the order of the values stored when the account at `_index` is the only one with value greater than what it should be.
+    /// @param _heap The heap to modify.
+    /// @param _index The index of the account to move.
     function shiftUp(Heap storage _heap, uint256 _index) private {
         uint256 mother = _index / 2;
         while (mother > 0 && _heap.accounts[_index - 1].value > _heap.accounts[mother - 1].value) {
@@ -74,6 +105,10 @@ library BasicHeap {
         }
     }
 
+    /// @notice Moves an account down the heap until its value is greater than the ones of its children.
+    /// @dev This functions restores the invariant about the order of the values stored when the account at `_index` is the only one with value smaller than what it should be.
+    /// @param _heap The heap to modify.
+    /// @param _index The index of the account to move.
     function shiftDown(Heap storage _heap, uint256 _index) private {
         uint256 leftIndex;
         uint256 rightIndex;
@@ -102,7 +137,12 @@ library BasicHeap {
         }
     }
 
-    // only call when id is not in the _heap and with value != 0
+    /// @notice Inserts an account in the `_heap`.
+    /// @dev Only call this function when `_id`.
+    /// @dev Reverts with AddressIsZero if `_value` is 0.
+    /// @param _heap The heap to modify.
+    /// @param _id The address of the account to insert.
+    /// @param _value The value of the account to insert.
     function insert(
         Heap storage _heap,
         address _id,
@@ -115,7 +155,11 @@ library BasicHeap {
         shiftUp(_heap, _heap.accounts.length);
     }
 
-    // only when id is in the _heap with a value greater than newValue
+    /// @notice Increase the amount of an account in the `_heap`.
+    /// @dev Only call this function when `_id` is in the `_heap` with a value greater than `_newValue`.
+    /// @param _heap The heap to modify.
+    /// @param _id The address of the account to decrease the amount.
+    /// @param _newValue The new value of the account.
     function decrease(
         Heap storage _heap,
         address _id,
@@ -126,7 +170,11 @@ library BasicHeap {
         shiftDown(_heap, index);
     }
 
-    // only when id is in the _heap with a value smaller than newValue
+    /// @notice Increase the amount of an account in the `_heap`.
+    /// @dev Only call this function when `_id` is in the `_heap` with a smaller value than `_newValue`.
+    /// @param _heap The heap to modify.
+    /// @param _id The address of the account to increase the amount.
+    /// @param _newValue The new value of the account.
     function increase(
         Heap storage _heap,
         address _id,
@@ -137,7 +185,10 @@ library BasicHeap {
         shiftUp(_heap, index);
     }
 
-    // only call when id is in the _heap
+    /// @notice Removes an account in the `_heap`.
+    /// @dev Only call when `_id` is in the `_heap`.
+    /// @param _heap The heap to modify.
+    /// @param _id The address of the account to remove.
     function remove(Heap storage _heap, address _id) private {
         Account[] storage accounts = _heap.accounts;
         uint256 index = _heap.indexes[_id];
@@ -154,7 +205,10 @@ library BasicHeap {
 
     /// INTERNAL ///
 
-    // only call with id in the _heap,with value formerValue
+    /// @notice Removes an account in the `_heap`.
+    /// @dev Only call with `_id` is in the `_heap` with value `_formerValue`.
+    /// @param _heap The heap to modify.
+    /// @param _id The address of the account to remove.
     function update(
         Heap storage _heap,
         address _id,
