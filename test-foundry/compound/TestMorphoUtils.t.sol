f7dbd1e2d67be132133a0d360edff8242c8b05ef
MerlinEgalite <44097430+MerlinEgalite@users.noreply.github.com>
2022-05-23 12:45:22 +0200

    ✅ (missing-modifier) Update test naming

--- test-foundry/compound/TestMorphoUtils.t.sol
+++ test-foundry/compound/TestMorphoUtils.t.sol
@@ -3,8 +3,8 @@ pragma solidity 0.8.13;
 
 import "./setup/TestSetup.sol";
 
-contract TestPublicFunctions is TestSetup {
-    function testUserShouldNotUpdateP2PIndexesOfMarketNotCreatedYet() public {
+contract TestMorphoUtils is TestSetup {
+    function testUserShouldNotUpdateP2PIndexesOfMarketNotCreated() public {
         hevm.prank(address(2));
         hevm.expectRevert(abi.encodeWithSignature("MarketNotCreated()"));
         morpho.updateP2PIndexes(cAave);
