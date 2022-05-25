4581a2c2fe3f966da5f985b1b04b28a9a0e0f939
Patrick <pakim249@ischool.berkeley.edu>
2022-05-25 17:52:58 +0200

    Update IMorpho interface

--- contracts/compound/interfaces/IMorpho.sol
+++ contracts/compound/interfaces/IMorpho.sol
@@ -65,12 +65,12 @@ interface IMorpho {
 
     /// USERS ///
 
-    function supply(address _poolTokenAddress, uint256 _amount) external;
-    function supply(address _poolTokenAddress, uint256 _amount, uint256 _maxGasForMatching) external;
+    function supply(address _poolTokenAddress, address _onBehalf, uint256 _amount) external;
+    function supply(address _poolTokenAddress, address _onBehalf, uint256 _amount, uint256 _maxGasForMatching) external;
     function borrow(address _poolTokenAddress, uint256 _amount) external;
     function borrow(address _poolTokenAddress, uint256 _amount, uint256 _maxGasForMatching) external;
     function withdraw(address _poolTokenAddress, uint256 _amount) external;
-    function repay(address _poolTokenAddress, uint256 _amount) external;
+    function repay(address _poolTokenAddress, address _onBehalf, uint256 _amount) external;
     function liquidate(address _poolTokenBorrowedAddress, address _poolTokenCollateralAddress, address _borrower, uint256 _amount) external;
     function claimRewards(address[] calldata _cTokenAddresses, bool _tradeForMorphoToken) external;
 }
