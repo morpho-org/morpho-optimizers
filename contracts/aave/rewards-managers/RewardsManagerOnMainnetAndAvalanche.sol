83f868f91dcf2b4748e1b7e74d4629b35836ddcb
MerlinEgalite <44097430+MerlinEgalite@users.noreply.github.com>
2022-05-29 15:57:44 +0200

    🚚 (aave-v2) Rename Rewards Managers

--- contracts/aave/rewards-managers/RewardsManagerOnMainnetAndAvalanche.sol
+++ contracts/aave/rewards-managers/RewardsManagerOnMainnetAndAvalanche.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.13;
 
 import "../RewardsManager.sol";
 
-contract RewardsManagerForAaveOnMainnetAndAvalanche is RewardsManager {
+contract RewardsManagerOnMainnetAndAvalanche is RewardsManager {
     constructor(
         ILendingPool _lendingPool,
         IMorpho _morpho,
