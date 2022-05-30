d6010ca6b43e07b7c05f420ff2579d3fa63d88ac
MerlinEgalite <44097430+MerlinEgalite@users.noreply.github.com>
2022-05-30 10:28:25 +0200

    ✅ (aave-v2) Add test governance

---
+++ test-foundry/aave/TestGovernance.t.sol
@@ -0,0 +1,206 @@
+// SPDX-License-Identifier: GNU AGPLv3
+pragma solidity 0.8.13;
+
+import "./setup/TestSetup.sol";
+
+contract TestGovernance is TestSetup {
+    using WadRayMath for uint256;
+
+    function testShouldDeployContractWithTheRightValues() public {
+        assertEq(address(morpho.positionsManager()), address(positionsManager));
+        assertEq(address(morpho.interestRatesManager()), address(interestRatesManager));
+        assertEq(address(morpho.addressesProvider()), address(lendingPoolAddressesProviderAddress));
+        assertEq(
+            address(morpho.lendingPool()),
+            ILendingPoolAddressesProvider(lendingPoolAddressesProviderAddress).getLendingPool()
+        );
+        assertEq(morpho.maxSortedUsers(), 20);
+
+        (uint256 supply, uint256 borrow, uint256 withdraw, uint256 repay) = morpho
+        .defaultMaxGasForMatching();
+        assertEq(supply, 3e6);
+        assertEq(borrow, 3e6);
+        assertEq(withdraw, 3e6);
+        assertEq(repay, 3e6);
+    }
+
+    function testShouldRevertWhenCreatingMarketWithAnImproperMarket() public {
+        Types.MarketParameters memory marketParams = Types.MarketParameters(3_333, 0);
+
+        hevm.expectRevert(abi.encodeWithSignature("MarketIsNotListedOnAave()"));
+        morpho.createMarket(address(supplier1), marketParams);
+    }
+
+    function testOnlyOwnerCanCreateMarkets() public {
+        Types.MarketParameters memory marketParams = Types.MarketParameters(3_333, 0);
+
+        for (uint256 i = 0; i < pools.length; i++) {
+            hevm.expectRevert("Ownable: caller is not the owner");
+            supplier1.createMarket(pools[i], marketParams);
+
+            hevm.expectRevert("Ownable: caller is not the owner");
+            borrower1.createMarket(pools[i], marketParams);
+        }
+
+        morpho.createMarket(wEth, marketParams);
+    }
+
+    function testShouldCreateMarketWithRightParams() public {
+        Types.MarketParameters memory rightParams = Types.MarketParameters(1_000, 3_333);
+        Types.MarketParameters memory wrongParams1 = Types.MarketParameters(10_001, 0);
+        Types.MarketParameters memory wrongParams2 = Types.MarketParameters(0, 10_001);
+
+        hevm.expectRevert(abi.encodeWithSignature("ExceedsMaxBasisPoints()"));
+        morpho.createMarket(wEth, wrongParams1);
+        hevm.expectRevert(abi.encodeWithSignature("ExceedsMaxBasisPoints()"));
+        morpho.createMarket(wEth, wrongParams2);
+
+        morpho.createMarket(wEth, rightParams);
+        (uint16 reserveFactor, uint256 p2pIndexCursor) = morpho.marketParameters(aWeth);
+        assertEq(reserveFactor, 1_000);
+        assertEq(p2pIndexCursor, 3_333);
+    }
+
+    function testOnlyOwnerCanSetReserveFactor() public {
+        for (uint256 i = 0; i < pools.length; i++) {
+            hevm.expectRevert("Ownable: caller is not the owner");
+            supplier1.setReserveFactor(aDai, 1111);
+
+            hevm.expectRevert("Ownable: caller is not the owner");
+            borrower1.setReserveFactor(aDai, 1111);
+        }
+
+        morpho.setReserveFactor(aDai, 1111);
+    }
+
+    function testReserveFactorShouldBeUpdatedWithRightValue() public {
+        morpho.setReserveFactor(aDai, 1111);
+        (uint16 reserveFactor, ) = morpho.marketParameters(aDai);
+        assertEq(reserveFactor, 1111);
+    }
+
+    function testShouldCreateMarketWithTheRightValues() public {
+        IAToken aToken = IAToken(aAave);
+        Types.MarketParameters memory marketParams = Types.MarketParameters(3_333, 0);
+        morpho.createMarket(wEth, marketParams);
+
+        (bool isCreated, , ) = morpho.marketStatus(aAave);
+
+        assertTrue(isCreated);
+        assertEq(morpho.p2pSupplyIndex(aAave), WadRayMath.ray());
+        assertEq(morpho.p2pBorrowIndex(aAave), WadRayMath.ray());
+    }
+
+    function testShouldSetMaxGasWithRightValues() public {
+        Types.MaxGasForMatching memory newMaxGas = Types.MaxGasForMatching({
+            supply: 1,
+            borrow: 1,
+            withdraw: 1,
+            repay: 1
+        });
+
+        morpho.setDefaultMaxGasForMatching(newMaxGas);
+        (uint64 supply, uint64 borrow, uint64 withdraw, uint64 repay) = morpho
+        .defaultMaxGasForMatching();
+        assertEq(supply, newMaxGas.supply);
+        assertEq(borrow, newMaxGas.borrow);
+        assertEq(withdraw, newMaxGas.withdraw);
+        assertEq(repay, newMaxGas.repay);
+
+        hevm.expectRevert("Ownable: caller is not the owner");
+        supplier1.setDefaultMaxGasForMatching(newMaxGas);
+
+        hevm.expectRevert("Ownable: caller is not the owner");
+        borrower1.setDefaultMaxGasForMatching(newMaxGas);
+    }
+
+    function testOnlyOwnerCanSetMaxSortedUsers() public {
+        uint256 newMaxSortedUsers = 30;
+
+        morpho.setMaxSortedUsers(newMaxSortedUsers);
+        assertEq(morpho.maxSortedUsers(), newMaxSortedUsers);
+
+        hevm.expectRevert("Ownable: caller is not the owner");
+        supplier1.setMaxSortedUsers(newMaxSortedUsers);
+
+        hevm.expectRevert("Ownable: caller is not the owner");
+        borrower1.setMaxSortedUsers(newMaxSortedUsers);
+    }
+
+    function testOnlyOwnerShouldFlipMarketStrategy() public {
+        hevm.expectRevert("Ownable: caller is not the owner");
+        hevm.prank(address(supplier1));
+        morpho.setP2PDisable(aDai, true);
+
+        hevm.expectRevert("Ownable: caller is not the owner");
+        hevm.prank(address(supplier2));
+        morpho.setP2PDisable(aDai, true);
+
+        morpho.setP2PDisable(aDai, true);
+        assertTrue(morpho.p2pDisabled(aDai));
+    }
+
+    function testOnlyOwnerShouldSetPositionsManager() public {
+        IPositionsManager positionsManagerV2 = new PositionsManager();
+
+        hevm.prank(address(0));
+        hevm.expectRevert("Ownable: caller is not the owner");
+        morpho.setPositionsManager(positionsManagerV2);
+
+        morpho.setPositionsManager(positionsManagerV2);
+        assertEq(address(morpho.positionsManager()), address(positionsManagerV2));
+    }
+
+    function testOnlyOwnerShouldSetRewardsManager() public {
+        IRewardsManager rewardsManagerV2 = new RewardsManagerOnMainnetAndAvalanche(
+            lendingPool,
+            IMorpho(address(morpho))
+        );
+
+        hevm.prank(address(0));
+        hevm.expectRevert("Ownable: caller is not the owner");
+        morpho.setRewardsManager(rewardsManagerV2);
+
+        morpho.setRewardsManager(rewardsManagerV2);
+        assertEq(address(morpho.rewardsManager()), address(rewardsManagerV2));
+    }
+
+    function testOnlyOwnerShouldSetInterestRates() public {
+        IInterestRatesManager interestRatesV2 = new InterestRatesManager();
+
+        hevm.prank(address(0));
+        hevm.expectRevert("Ownable: caller is not the owner");
+        morpho.setInterestRates(interestRatesV2);
+
+        morpho.setInterestRates(interestRatesV2);
+        assertEq(address(morpho.interestRatesManager()), address(interestRatesV2));
+    }
+
+    function testOnlyOwnerShouldSetIncentivesVault() public {
+        IIncentivesVault incentivesVaultV2 = new IncentivesVault(
+            IMorpho(address(morpho)),
+            morphoToken,
+            ERC20(address(1)),
+            address(2),
+            dumbOracle
+        );
+
+        hevm.prank(address(0));
+        hevm.expectRevert("Ownable: caller is not the owner");
+        morpho.setIncentivesVault(incentivesVaultV2);
+
+        morpho.setIncentivesVault(incentivesVaultV2);
+        assertEq(address(morpho.incentivesVault()), address(incentivesVaultV2));
+    }
+
+    function testOnlyOwnerShouldSetTreasuryVault() public {
+        address treasuryVaultV2 = address(2);
+
+        hevm.prank(address(0));
+        hevm.expectRevert("Ownable: caller is not the owner");
+        morpho.setTreasuryVault(treasuryVaultV2);
+
+        morpho.setTreasuryVault(treasuryVaultV2);
+        assertEq(address(morpho.treasuryVault()), treasuryVaultV2);
+    }
+}
