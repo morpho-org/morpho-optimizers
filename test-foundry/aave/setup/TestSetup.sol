85655666d7928bcdd90bda8767e8ef90f47ff15d
MerlinEgalite <44097430+MerlinEgalite@users.noreply.github.com>
2022-05-30 11:35:36 +0200

    🧪 (aave-v2) Skip fee test

--- test-foundry/aave/setup/TestSetup.sol
+++ test-foundry/aave/setup/TestSetup.sol
@@ -278,7 +278,7 @@ contract TestSetup is Config, Utils, stdCheats {
         }
     }
 
-    /// @notice Computes and returns peer-to-peer rates for a specific market (without taking into account deltas !).
+    /// @notice Computes and returns peer-to-peer rates for a specific market (without taking into account deltas!).
     /// @param _poolTokenAddress The market address.
     /// @return p2pSupplyRate_ The market's supply rate in peer-to-peer (in ray).
     /// @return p2pBorrowRate_ The market's borrow rate in peer-to-peer (in ray).
