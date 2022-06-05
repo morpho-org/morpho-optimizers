091c21a8430c219b50ec060b78821fdfdba1d553
MerlinEgalite <44097430+MerlinEgalite@users.noreply.github.com>
2022-06-05 21:59:53 +0200

    🔧 (rewards-v3) Add config Aave V3 avalanche

--- config/avalanche-mainnet/aave-v3/Config.sol
+++ config/avalanche-mainnet/aave-v3/Config.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.0;
 
 contract Config {
-    address aave = 0xD6DF932A45C0f255f85145f286eA0b292B21C90B;
-    address dai = 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063;
-    address usdc = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
-    address usdt = 0xc2132D05D31c914a87C6611C10748AEb04B58e8F;
-    address wbtc = 0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6;
-    address wEth = 0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619;
-    address wmatic = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
+    address aave = 0x63a72806098Bd3D9520cC43356dD78afe5D386D9;
+    address dai = 0xd586E7F844cEa2F87f50152665BCbc2C279D8d70;
+    address usdc = 0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E;
+    address usdt = 0x9702230A8Ea53601f5cD2dc00fDBc13d4dF4A8c7;
+    address wbtc = 0x50b7545627a5162F82A992c33b87aDc75187B218;
+    address wEth = 0x49D5c2BdFfac6CE2BFdB6640F4F80f226bc10bAB;
+    address wavax = 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7;
 
     address aAave = 0xf329e36C7bF6E5E86ce2150875a84Ce77f477375;
     address aDai = 0x82E64f49Ed5EC1bC6e43DAD4FC8Af9bb3A2312EE;
@@ -16,7 +16,7 @@ contract Config {
     address aUsdt = 0x6ab707Aca953eDAeFBc4fD23bA73294241490620;
     address aWbtc = 0x078f358208685046a11C85e8ad32895DED33A249;
     address aWeth = 0xe50fA9b3c56FfB159cB0FCA61F5c9D750e8128c8;
-    address aWmatic = 0x6d80113e533a2C0fe82EaBD35f1875DcEA89Ea97;
+    address avWavax = 0x6d80113e533a2C0fe82EaBD35f1875DcEA89Ea97;
 
     address stableDebtDai = 0xd94112B5B62d53C9402e7A60289c6810dEF1dC9B;
     address variableDebtDai = 0x8619d80FB0141ba7F184CbF22fd724116D9f7ffC;
@@ -25,8 +25,6 @@ contract Config {
     address poolDataProviderAddress = 0x69FA688f1Dc47d4B5d8029D5a35FB7a548310654;
     address poolAddressesProviderAddress = 0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb;
     address rewardsControllerAddress = 0x929EC64c34a17401F460460D4B9390518E5B473e;
-
-    // Uniswap V3
 
     uint24 MORPHO_UNIV3_FEE = 3000;
     uint24 REWARD_UNIV3_FEE = 3000;
