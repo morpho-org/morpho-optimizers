db7f9c8cdbd2a8b03d3a6854548408234416b052
MerlinEgalite <44097430+MerlinEgalite@users.noreply.github.com>
2022-06-05 17:50:17 +0200

    ✨ (rewards-v3) Rewards manager compiling

--- contracts/aave-v3/interfaces/IRewardsManager.sol
+++ contracts/aave-v3/interfaces/IRewardsManager.sol
@@ -1,20 +1,41 @@
 // SPDX-License-Identifier: GNU AGPLv3
 pragma solidity ^0.8.0;
 
-import "@aave/core-v3/contracts/interfaces/IAaveIncentivesController.sol";
+import "@aave/periphery-v3/contracts/rewards/interfaces/IRewardsController.sol";
 
 interface IRewardsManager {
-    function aaveIncentivesController() external view returns (IAaveIncentivesController);
+    function rewardsController() external view returns (IRewardsController);
 
-    function setAaveIncentivesController(address) external;
+    function setRewardsController(address) external;
 
-    function getUserIndex(address, address) external returns (uint256);
+    function getUserAccruedRewards(
+        address[] calldata _assets,
+        address _user,
+        address _reward
+    ) external view returns (uint256 totalAccrued);
 
-    function accrueUserUnclaimedRewards(address[] calldata, address) external returns (uint256);
+    function getAllUserRewards(address[] calldata _assets, address _user)
+        external
+        view
+        returns (address[] memory rewardsList, uint256[] memory unclaimedAmounts);
 
-    function getUserUnclaimedRewards(address[] calldata, address) external view returns (uint256);
+    function getUserAssetIndex(
+        address _user,
+        address _asset,
+        address _reward
+    ) external view returns (uint256);
 
-    function claimRewards(address[] calldata, address) external returns (uint256);
+    function getUserRewards(
+        address[] calldata _assets,
+        address _user,
+        address _reward
+    ) external view returns (uint256);
+
+    function claimRewards(
+        address[] calldata,
+        address,
+        address[] calldata
+    ) external returns (address[] memory rewardsList, uint256[] memory claimedAmount);
 
     function updateUserAssetAndAccruedRewards(
         address,
