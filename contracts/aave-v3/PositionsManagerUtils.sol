48917772e95859403540b3ed40733d9edcaa8015
MathisGD <mathis.gontier_delaunay@telecom-sudparis.eu>
2022-06-05 17:59:06 +0200

    📚 (aave-v3) fix import and doc PositionsManagerUtils

--- contracts/aave-v3/PositionsManagerUtils.sol
+++ contracts/aave-v3/PositionsManagerUtils.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GNU AGPLv3
 pragma solidity ^0.8.0;
 
-import {IVariableDebtToken} from "@aave/core-v3/contracts/interfaces/IVariableDebtToken.sol";
+import "@aave/core-v3/contracts/interfaces/IVariableDebtToken.sol";
 
 import "@rari-capital/solmate/src/utils/SafeTransferLib.sol";
 
