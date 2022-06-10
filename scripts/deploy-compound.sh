a7f2df6e29756176a016c713d2e365a6c323eea9
rubilmax <rmilon@gmail.com>
2022-06-10 10:12:02 +0200

    🔨 (deploy-compound) Update deployment script

--- scripts/deploy-compound.sh
+++ scripts/deploy-compound.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail # exit on error
 
 
 read -p "❓ Deploy Morpho-Compound's InterestRatesManager on ${NETWORK}? " -n 1 -r
@@ -9,7 +10,9 @@ then
 	echo "Deploying Morpho-Compound's InterestRatesManager on ${NETWORK}..."
 
 	forge create --private-key "${DEPLOYER_PRIVATE_KEY}" \
-        --optimize contracts/compound/InterestRatesManager.sol:InterestRatesManager
+        --contracts contracts/compound \
+        --optimize contracts/compound/InterestRatesManager.sol:InterestRatesManager \
+        --verify
 
     echo "🎉 InterestRatesManager deployed!"
 fi
@@ -24,6 +27,7 @@ then
 	echo "Deploying Morpho-Compound's PositionsManager on ${NETWORK}..."
 
 	forge create --private-key "${DEPLOYER_PRIVATE_KEY}" \
+        --contracts contracts/compound \
         --optimize contracts/compound/PositionsManager.sol:PositionsManager
 
     echo "🎉 PositionsManager deployed!"
@@ -39,6 +43,7 @@ then
 	echo "Deploying Morpho-Compound's Implementation on ${NETWORK}..."
 
 	forge create --private-key "${DEPLOYER_PRIVATE_KEY}" \
+        --contracts contracts/compound \
         --optimize contracts/compound/Morpho.sol:Morpho
 
     echo "🎉 Morpho Implementation deployed!"
@@ -75,14 +80,19 @@ then
     read -p "           Morpho-Compound's PositionsManager address on ${NETWORK}? " -r MORPHO_POSITIONS_MANAGER_ADDRESS
     read -p "           Morpho-Compound's InterestRatesManager address on ${NETWORK}? " -r MORPHO_INTEREST_RATES_MANAGER_ADDRESS
     read -p "           Compound's Comptroller address on ${NETWORK}? " -r COMPTROLLER_ADDRESS
+    read -p "           Morpho-Compound's defaultMaxGasForMatching on ${NETWORK}? " -r DEFAULT_MAX_GAS_FOR_MATCHING
     read -p "           ${NETWORK}'s cETH address? " -r CETH_ADDRESS
     read -p "           ${NETWORK}'s wETH address? " -r WETH_ADDRESS
 
 	echo "Initializing Morpho-Compound's Proxy on ${NETWORK} at ${MORPHO_PROXY_ADDRESS}, with 1 dustThreshold & 16 maxSortedUsers..."
 
-    MAX_GAS_FOR_MATCHING=$(cast abi-encode "tuple(uint64,uint64,uint64,uint64)" 100000 100000 100000 100000)
-	cast send --private-key "${DEPLOYER_PRIVATE_KEY}" "${MORPHO_PROXY_ADDRESS}" \
-        0x34544040000000000000000000000000"${MORPHO_POSITIONS_MANAGER_ADDRESS:2}"000000000000000000000000"${MORPHO_INTEREST_RATES_MANAGER_ADDRESS:2}"000000000000000000000000"${COMPTROLLER_ADDRESS:2}""${MAX_GAS_FOR_MATCHING:2}"00000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000010000000000000000000000000"${CETH_ADDRESS:2}"000000000000000000000000"${WETH_ADDRESS:2}"
+    POSITIONS_MANAGERO_INTEREST_RATES_MANAGER_COMPTROLLER_ADDRESS=$(cast abi-encode "tuple(address,address,address)" "${MORPHO_POSITIONS_MANAGER_ADDRESS}" "${MORPHO_INTEREST_RATES_MANAGER_ADDRESS}" "${COMPTROLLER_ADDRESS}")
+    DUST_THRESHOLD_MAX_SORTED_USERS=$(cast abi-encode "tuple(uint256,uint256)" 1 16)
+    DEFAULT_MAX_GAS_FOR_MATCHING=$(cast abi-encode "tuple(uint64,uint64,uint64,uint64)" "${DEFAULT_MAX_GAS_FOR_MATCHING}" "${DEFAULT_MAX_GAS_FOR_MATCHING}" "${DEFAULT_MAX_GAS_FOR_MATCHING}" "${DEFAULT_MAX_GAS_FOR_MATCHING}")
+    CETH_WETH_ADDRESS=$(cast abi-encode "tuple(address,address)" "${CETH_ADDRESS}" "${WETH_ADDRESS}")
+
+    cast send --private-key "${DEPLOYER_PRIVATE_KEY}" "${MORPHO_PROXY_ADDRESS}" \
+        0x34544040"${POSITIONS_MANAGERO_INTEREST_RATES_MANAGER_COMPTROLLER_ADDRESS:2}""${DEFAULT_MAX_GAS_FOR_MATCHING:2}""${DUST_THRESHOLD_MAX_SORTED_USERS:2}""${CETH_WETH_ADDRESS:2}"
 
     echo "🎉 Morpho Proxy initialized!"
 fi
@@ -99,6 +109,7 @@ then
 	echo "Deploying Morpho-Compound's RewardsManager for Proxy at ${MORPHO_PROXY_ADDRESS} on $NETWORK..."
 
 	forge create --private-key "${DEPLOYER_PRIVATE_KEY}" \
+        --contracts contracts/compound \
         --optimize contracts/compound/RewardsManager.sol:RewardsManager \
         --constructor-args "${MORPHO_PROXY_ADDRESS}"
 
@@ -117,6 +128,7 @@ then
 	echo "Deploying Morpho-Compound's Lens for Proxy at ${MORPHO_PROXY_ADDRESS} on $NETWORK..."
 
 	forge create --private-key "${DEPLOYER_PRIVATE_KEY}" \
+        --contracts contracts/compound \
         --optimize contracts/compound/Lens.sol:Lens \
         --constructor-args "${MORPHO_PROXY_ADDRESS}"
 
