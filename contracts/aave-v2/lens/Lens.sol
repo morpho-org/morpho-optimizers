3b8efa83c74655ad24f64e640d3729268382379c
Rubilmax <rmilon@gmail.com>
2022-06-20 14:23:38 +0200

    🔥 (lens-aave) Remove rewards lens

--- contracts/aave-v2/lens/Lens.sol
+++ contracts/aave-v2/lens/Lens.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: GNU AGPLv3
 pragma solidity ^0.8.0;
 
-import "./RewardsLens.sol";
+import "./RatesLens.sol";
 
 /// @title Lens.
 /// @author Morpho Labs.
 /// @custom:contact security@morpho.xyz
 /// @notice This contract exposes an API to query on-chain data related to the Morpho Protocol, its markets and its users.
-contract Lens is RewardsLens {
+contract Lens is RatesLens {
     function initialize(address _morphoAddress, address _addressesProviderAddress)
         external
         initializer
@@ -15,6 +15,5 @@ contract Lens is RewardsLens {
         morpho = IMorpho(_morphoAddress);
         addressesProvider = ILendingPoolAddressesProvider(_addressesProviderAddress);
         pool = ILendingPool(addressesProvider.getLendingPool());
-        rewardsManager = IRewardsManager(morpho.rewardsManager());
     }
 }
