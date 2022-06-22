3a4a2fb39002ea35fa7855144100a34719fad051
MerlinEgalite <egalite.merlin@gmail.com>
2022-06-22 18:23:34 +0200

    🎨 (pause-claim-rewards) Renaming
    Signed-off-by: MerlinEgalite <egalite.merlin@gmail.com>

--- test-foundry/compound/TestRewards.t.sol
+++ test-foundry/compound/TestRewards.t.sol
@@ -41,7 +41,7 @@ contract TestRewards is TestSetup {
         address[] memory cDaiInArray = new address[](1);
         cDaiInArray[0] = cDai;
 
-        morpho.setPauseStatusClaimRewards(true);
+        morpho.setClaimRewardsPauseStatus(true);
 
         hevm.expectRevert(abi.encodeWithSignature("ClaimRewardsPaused()"));
         morpho.claimRewards(cDaiInArray, false);
