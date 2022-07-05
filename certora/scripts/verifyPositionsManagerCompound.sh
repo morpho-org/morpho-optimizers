f8995c617ee9eab3e70f3407ad3add1c1fb916f3
Nick Armstrong <nick@certora.com>
2022-07-05 11:53:06 -0700

    updated comptroller and interestRatesManager linking

--- certora/scripts/verifyPositionsManagerCompound.sh
+++ certora/scripts/verifyPositionsManagerCompound.sh
@@ -5,17 +5,22 @@ make -C certora munged
 certoraRun \
     certora/munged/compound/PositionsManager.sol \
     certora/munged/compound/RewardsManager.sol \
+    certora/munged/compound/InterestRatesManager.sol \
     certora/helpers/compound/DummyPoolTokenImpl.sol \
     certora/helpers/compound/DummyPoolTokenA.sol \
     certora/helpers/compound/DummyPoolTokenB.sol \
     certora/helpers/compound/SymbolicOracle.sol \
     certora/helpers/compound/SymbolicComptroller.sol \
+    --link PositionsManager:comptroller=SymbolicComptroller \
+    --link PositionsManager:interestRatesManager=InterestRatesManager \
     --verify PositionsManager:certora/spec/PositionsManagerCompound.spec \
+    --method 'liquidateLogic(address,address,address,uint256)' \
     --solc solc8.13 \
     --loop_iter 2 \
     --optimistic_loop \
     --cache morpho \
-    --msg "PMFC $1"
+    --msg "PMFC $1" \
+    --staging \
 
 
     # notes:
