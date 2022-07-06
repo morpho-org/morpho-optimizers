2d787d448a5b7d55ebe8a3d498e52a085beb154d
MerlinEgalite <egalite.merlin@gmail.com>
2022-07-06 09:46:36 +0200

    🔥 (aave-1) Remove unused files
    Signed-off-by: MerlinEgalite <egalite.merlin@gmail.com>

--- contracts/common/interfaces/ISwapManager.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GNU AGPLv3
-pragma solidity ^0.8.0;
-
-interface ISwapManager {
-    function swapToMorphoToken(uint256 _amountIn, address _receiver)
-        external
-        returns (uint256 amountOut);
-}
