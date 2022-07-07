085c4779d1e02798724c1723cc77a21407608cf9
Nick Armstrong <nick@certora.com>
2022-07-07 10:55:07 -0700

    solc fix, aave removed

--- certora/harness/compound/PositionsManagerHarness.sol
+++ certora/harness/compound/PositionsManagerHarness.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: None
-pragma solidity 0.8.7;
+pragma solidity 0.8.13;
 
 // This is the contract that is actually verified; it may contain some helper
 // methods for the spec to access internal state, or may override some of the
