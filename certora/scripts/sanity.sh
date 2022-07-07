83d2020eca0c8b56e8be286852595dc933387edc
Nick Armstrong <nick@certora.com>
2022-07-07 10:49:02 -0700

    sanity script fixed

--- certora/scripts/sanity.sh
+++ certora/scripts/sanity.sh
@@ -2,18 +2,21 @@
 make -C certora munged
 
 certoraRun \
-    certora/harness/SwapManagerHarness.sol \
-    certora/helpers/DummyERC20A.sol \
-    certora/helpers/DummyERC20B.sol \
-    certora/munged/RewardsManager.sol \
-    --packages @uniswap=node_modules/@uniswap \
-               @openzeppelin=node_modules/@openzeppelin \
-    --verify SwapManagerHarness:certora/spec/sanity.spec \
-    --rule sanity \
-    --solc solc8.7 \
+    certora/munged/compound/PositionsManager.sol \
+    certora/munged/compound/RewardsManager.sol \
+    certora/munged/compound/InterestRatesManager.sol \
+    certora/helpers/compound/DummyPoolTokenImpl.sol \
+    certora/helpers/compound/DummyPoolTokenA.sol \
+    certora/helpers/compound/DummyPoolTokenB.sol \
+    certora/helpers/compound/SymbolicOracle.sol \
+    certora/helpers/compound/SymbolicComptroller.sol \
+    --link PositionsManager:comptroller=SymbolicComptroller \
+    --link PositionsManager:interestRatesManager=InterestRatesManager \
+    --verify PositionsManager:certora/spec/PositionsManagerCompound.spec \
+    --solc solc8.13 \
     --loop_iter 2 \
-    --solc_args '["--optimize"]' \
-    --settings -t=60 \
-    --msg "sanity" \
-    --send_only \
-    $*
+    --optimistic_loop \
+    --cache morpho \
+    --rule "sanity" \
+    --msg "PMFC Sanity" \
+    --staging \
