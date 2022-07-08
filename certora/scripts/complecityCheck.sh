7a7e84a406d8fa393e7eea9f4f0aab37d87c25ec
Nick Armstrong <nick@certora.com>
2022-07-08 14:03:17 -0700

    complexity check

---
+++ certora/scripts/complecityCheck.sh
@@ -0,0 +1,14 @@
+certoraRun \
+    certora/munged/compound/PositionsManager.sol \
+    certora/helpers/DummyERC20A.sol \
+    certora/helpers/DummyERC20B.sol \
+    --verify PositionsManager:certora/helpers/complexity.spec \
+    --solc solc8.13 \
+    --staging \
+    --optimistic_loop \
+    --send_only \
+    --msg "PositionsManager complexity check"
+    
+# to run for other scripts just copy the above and change lines #2, #5, and optionally #11 to match the given contract
+# sometimes the below line is needed, doesn't seem to be in this case but left it for reference
+# --packages @openzeppelin=node_modules/@openzeppelin \
