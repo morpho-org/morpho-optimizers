2a220cf2f735cfb26f16fff1ceaabcfa61292576
Quentin Garchery <garchery.quentin@gmail.com>
2022-07-11 12:19:26 +0200

    scripts update: send_only, solc version, remove aave

--- certora/verifyPositionsManagerForAave.sh
+++
@@ -1,9 +0,0 @@
-make -C certora munged
-
-certoraRun contracts/aave/PositionsManagerForAave.sol \
-           contracts/aave/positions-manager-parts/PositionsManagerForAaveStorage.sol \
---verify PositionsManagerForAave:certora/PositionsManagerForAave.spec \
---optimistic_loop --loop_iter 1 \
---solc_map PositionsManagerForAave=solc8.13,PositionsManagerForAaveStorage=solc8.7 \
---settings -t=600,-postProcessCounterExamples=true \
---msg "sanity"
