bf66a14d2749ef898dc128ad91c2a9aa19de0dea
MerlinEgalite <egalite.merlin@gmail.com>
2022-07-11 11:24:28 +0200

    ♻️ (rewards) Remove tokens
    Signed-off-by: MerlinEgalite <egalite.merlin@gmail.com>

--- contracts/common/test/FakeToken.sol
+++ contracts/common/test/FakeToken.sol
@@ -4,11 +4,9 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FakeToken is ERC20 {
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        address _receiver
-    ) ERC20(_name, _symbol) {
-        _mint(_receiver, 10_000 ether);
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {}
+
+    function mint(address _receiver, uint256 _amount) external {
+        _mint(_receiver, _amount);
     }
 }
