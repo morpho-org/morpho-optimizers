45448eedb8ea81762bcf199e95e47da18c7f3616
Nick Armstrong <nick@certora.com>
2022-07-13 11:18:53 -0700

    cleaned up helpers and scripts

--- certora/scripts/sanity.sh
+++ certora/scripts/sanity.sh
@@ -5,18 +5,21 @@ certoraRun \
     certora/munged/compound/PositionsManager.sol \
     certora/munged/compound/RewardsManager.sol \
     certora/munged/compound/InterestRatesManager.sol \
-    certora/helpers/compound/DummyPoolTokenImpl.sol \
-    certora/helpers/compound/DummyPoolTokenA.sol \
-    certora/helpers/compound/DummyPoolTokenB.sol \
-    certora/helpers/compound/SymbolicOracle.sol \
-    certora/helpers/compound/SymbolicComptroller.sol \
+    certora/helpers/compound/contracts/DummyPoolTokenImpl.sol \
+    certora/helpers/compound/contracts/DummyPoolTokenA.sol \
+    certora/helpers/compound/contracts/DummyPoolTokenB.sol \
+    certora/helpers/compound/contracts/SymbolicOracle.sol \
+    certora/helpers/compound/contracts/SymbolicComptroller.sol \
     --link PositionsManager:comptroller=SymbolicComptroller \
     --link PositionsManager:interestRatesManager=InterestRatesManager \
     --verify PositionsManager:certora/spec/PositionsManagerCompound.spec \
-    --solc solc8.13 \
     --loop_iter 2 \
+    --solc solc8.13 \
     --optimistic_loop \
     --cache morpho \
     --rule "sanity" \
-    --msg "PMFC Sanity" \
+    --msg "PMFC Sanity $1" \
+    --method 'liquidateLogic(address,address,address,uint256)' \
     --staging \
+    --send_only \
+    # --debug \
--- certora/spec/sanity.spec
+++
@@ -1,9 +0,0 @@
-import "../helpers/erc20.spec"
-
-rule sanity(method f)
-{
-	env e;
-	calldataarg args;
-	f(e,args);
-	assert false;
-}
