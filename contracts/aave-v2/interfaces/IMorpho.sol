b35eaee18d6d59230e192cdf0b581b0855c8a121
MerlinEgalite <egalite.merlin@gmail.com>
2022-07-14 11:57:22 +0200

    🔥 (gas-opti) Remove getUserHypotheticalBalanceStates from Morpho
    Signed-off-by: MerlinEgalite <egalite.merlin@gmail.com>

--- contracts/aave-v2/interfaces/IMorpho.sol
+++ contracts/aave-v2/interfaces/IMorpho.sol
@@ -54,11 +54,6 @@ interface IMorpho {
     /// GETTERS ///
 
     function getMarketsCreated() external view returns (address[] memory marketsCreated_);
-    function getUserHypotheticalBalanceStates(
-        address _user,
-        address _poolTokenAddress,
-        uint256 _withdrawnAmount,
-        uint256 _borrowedAmount) external view returns (Types.LiquidityData memory);
     function getHead(address _poolTokenAddress, Types.PositionType _positionType) external view returns (address head);
     function getNext(address _poolTokenAddress, Types.PositionType _positionType, address _user) external view returns (address next);
 
