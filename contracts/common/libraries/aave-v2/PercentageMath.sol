a77f500c789111ff32a0fab3fd9357d963d13d4e
MerlinEgalite <egalite.merlin@gmail.com>
2022-07-14 19:22:05 +0200

    ♻️ (#1100) Refacto math libs
    Signed-off-by: MerlinEgalite <egalite.merlin@gmail.com>

--- contracts/common/libraries/aave-v2/PercentageMath.sol
+++ contracts/common/libraries/aave-v2/PercentageMath.sol
@@ -3,21 +3,21 @@ pragma solidity ^0.8.0;
 
 import {Errors} from "./Errors.sol";
 
+/// @title PercentageMath.
+/// @author Morpho Labs.
+/// @custom:contact security@morpho.xyz
+/// @notice Library to conduct percentage multiplication inspired by https://github.com/aave/protocol-v2/blob/master/contracts/protocol/libraries/math/PercentageMath.sol.
 library PercentageMath {
-    uint256 constant PERCENTAGE_FACTOR = 1e4; //percentage plus two decimals
-    uint256 constant HALF_PERCENT = PERCENTAGE_FACTOR / 2;
+    uint256 internal constant PERCENTAGE_FACTOR = 1e4; // Percentage plus two decimals.
+    uint256 internal constant HALF_PERCENT = PERCENTAGE_FACTOR / 2;
 
-    /**
-     * @dev Executes a percentage multiplication
-     * @param value The value of which the percentage needs to be calculated
-     * @param percentage The percentage of the value to be calculated
-     * @return The percentage of value
-     **/
+    /// @dev Executes a percentage multiplication.
+    /// @param value The value of which the percentage needs to be calculated.
+    /// @param percentage The percentage of the value to be calculated.
+    /// @return The percentage of value.
     function percentMul(uint256 value, uint256 percentage) internal pure returns (uint256) {
         unchecked {
-            if (value == 0 || percentage == 0) {
-                return 0;
-            }
+            if (value == 0 || percentage == 0) return 0;
 
             require(
                 value <= (type(uint256).max - HALF_PERCENT) / percentage,
@@ -28,12 +28,10 @@ library PercentageMath {
         }
     }
 
-    /**
-     * @dev Executes a percentage division
-     * @param value The value of which the percentage needs to be calculated
-     * @param percentage The percentage of the value to be calculated
-     * @return The value divided the percentage
-     **/
+    /// @dev Executes a percentage division.
+    /// @param value The value of which the percentage needs to be calculated.
+    /// @param percentage The percentage of the value to be calculated.
+    /// @return The value divided the percentage.
     function percentDiv(uint256 value, uint256 percentage) internal pure returns (uint256) {
         unchecked {
             require(percentage != 0, Errors.MATH_DIVISION_BY_ZERO);
--- contracts/common/libraries/aave-v2/WadRayMath.sol
+++ contracts/common/libraries/aave-v2/WadRayMath.sol
@@ -3,64 +3,37 @@ pragma solidity ^0.8.0;
 
 import {Errors} from "./Errors.sol";
 
+/// @title WadRayMath.
+/// @author Morpho Labs.
+/// @custom:contact security@morpho.xyz
+/// @notice Library to conduct percentage multiplication inspired by https://github.com/aave/protocol-v2/blob/master/contracts/protocol/libraries/math/WadRayMath.sol.
 library WadRayMath {
     uint256 internal constant WAD = 1e18;
-    uint256 internal constant halfWAD = WAD / 2;
+    uint256 internal constant HALF_WAD = WAD / 2;
 
     uint256 internal constant RAY = 1e27;
-    uint256 internal constant halfRAY = RAY / 2;
+    uint256 internal constant HALF_RAY = RAY / 2;
 
     uint256 internal constant WAD_RAY_RATIO = 1e9;
 
-    function ray() internal pure returns (uint256) {
-        return RAY;
-    }
-
-    /**
-     * @return One wad, 1e18
-     **/
-    function wad() internal pure returns (uint256) {
-        return WAD;
-    }
-
-    /**
-     * @return Half ray, 1e27/2
-     **/
-    function halfRay() internal pure returns (uint256) {
-        return halfRAY;
-    }
-
-    /**
-     * @return Half ray, 1e18/2
-     **/
-    function halfWad() internal pure returns (uint256) {
-        return halfWAD;
-    }
-
-    /**
-     * @dev Multiplies two wad, rounding half up to the nearest wad
-     * @param a Wad
-     * @param b Wad
-     * @return The result of a*b, in wad
-     **/
+    /// @dev Multiplies two wad, rounding half up to the nearest wad.
+    /// @param a Wad.
+    /// @param b Wad.
+    /// @return The result of a*b, in wad.
     function wadMul(uint256 a, uint256 b) internal pure returns (uint256) {
         unchecked {
-            if (a == 0 || b == 0) {
-                return 0;
-            }
+            if (a == 0 || b == 0) return 0;
 
-            require(a <= (type(uint256).max - halfWAD) / b, Errors.MATH_MULTIPLICATION_OVERFLOW);
+            require(a <= (type(uint256).max - HALF_WAD) / b, Errors.MATH_MULTIPLICATION_OVERFLOW);
 
-            return (a * b + halfWAD) / WAD;
+            return (a * b + HALF_WAD) / WAD;
         }
     }
 
-    /**
-     * @dev Divides two wad, rounding half up to the nearest wad
-     * @param a Wad
-     * @param b Wad
-     * @return The result of a/b, in wad
-     **/
+    /// @dev Divides two wad, rounding half up to the nearest wad.
+    /// @param a Wad.
+    /// @param b Wad.
+    /// @return The result of a/b, in wad.
     function wadDiv(uint256 a, uint256 b) internal pure returns (uint256) {
         unchecked {
             require(b != 0, Errors.MATH_DIVISION_BY_ZERO);
@@ -72,30 +45,24 @@ library WadRayMath {
         }
     }
 
-    /**
-     * @dev Multiplies two ray, rounding half up to the nearest ray
-     * @param a Ray
-     * @param b Ray
-     * @return The result of a*b, in ray
-     **/
+    /// @dev Multiplies two ray, rounding half up to the nearest ray.
+    /// @param a Ray.
+    /// @param b Ray.
+    /// @return The result of a*b, in ray.
     function rayMul(uint256 a, uint256 b) internal pure returns (uint256) {
         unchecked {
-            if (a == 0 || b == 0) {
-                return 0;
-            }
+            if (a == 0 || b == 0) return 0;
 
-            require(a <= (type(uint256).max - halfRAY) / b, Errors.MATH_MULTIPLICATION_OVERFLOW);
+            require(a <= (type(uint256).max - HALF_RAY) / b, Errors.MATH_MULTIPLICATION_OVERFLOW);
 
-            return (a * b + halfRAY) / RAY;
+            return (a * b + HALF_RAY) / RAY;
         }
     }
 
-    /**
-     * @dev Divides two ray, rounding half up to the nearest ray
-     * @param a Ray
-     * @param b Ray
-     * @return The result of a/b, in ray
-     **/
+    /// @dev Divides two ray, rounding half up to the nearest ray.
+    /// @param a Ray.
+    /// @param b Ray.
+    /// @return The result of a/b, in ray.
     function rayDiv(uint256 a, uint256 b) internal pure returns (uint256) {
         unchecked {
             require(b != 0, Errors.MATH_DIVISION_BY_ZERO);
@@ -107,11 +74,9 @@ library WadRayMath {
         }
     }
 
-    /**
-     * @dev Casts ray down to wad
-     * @param a Ray
-     * @return a casted to wad, rounded half up to the nearest wad
-     **/
+    /// @dev Casts ray down to wad.
+    /// @param a Ray.
+    /// @return a casted to wad, rounded half up to the nearest wad.
     function rayToWad(uint256 a) internal pure returns (uint256) {
         unchecked {
             uint256 halfRatio = WAD_RAY_RATIO / 2;
@@ -122,11 +87,9 @@ library WadRayMath {
         }
     }
 
-    /**
-     * @dev Converts wad up to ray
-     * @param a Wad
-     * @return a converted in ray
-     **/
+    /// @dev Converts wad up to ray.
+    /// @param a Wad.
+    /// @return a converted in ray.
     function wadToRay(uint256 a) internal pure returns (uint256) {
         unchecked {
             uint256 result = a * WAD_RAY_RATIO;
