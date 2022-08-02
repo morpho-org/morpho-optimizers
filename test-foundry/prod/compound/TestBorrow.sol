ab882b88ae4991bfdffcdfef29cb63b3ff4562d8
Rubilmax <rmilon@gmail.com>
2022-08-02 10:48:35 +0200

    Added borrow test

--- test-foundry/prod/compound/TestBorrow.sol
+++ test-foundry/prod/compound/TestBorrow.sol
@@ -3,20 +3,22 @@ pragma solidity 0.8.13;
 
 import "./setup/TestSetup.sol";
 
-contract TestSupply is TestSetup {
+contract TestBorrow is TestSetup {
     using CompoundMath for uint256;
 
-    struct SupplyTest {
+    struct BorrowTest {
         ERC20 underlying;
         ICToken poolToken;
         uint256 decimals;
+        uint256 wEthPrice;
+        uint256 underlyingPrice;
         uint256 morphoBalanceOnPoolBefore;
         uint256 morphoUnderlyingBalanceBefore;
-        uint256 p2pSupplyIndex;
-        uint256 poolSupplyIndex;
-        uint256 supplyRatePerBlock;
-        uint256 p2pSupplyRatePerBlock;
-        uint256 poolSupplyRatePerBlock;
+        uint256 p2pBorrowIndex;
+        uint256 poolBorrowIndex;
+        uint256 borrowRatePerBlock;
+        uint256 p2pBorrowRatePerBlock;
+        uint256 poolBorrowRatePerBlock;
         uint256 balanceInP2P;
         uint256 balanceOnPool;
         uint256 unclaimedRewardsBefore;
@@ -29,76 +31,90 @@ contract TestSupply is TestSetup {
         uint256 totalUnderlyingAfter;
     }
 
-    function testShouldSupplyAmountP2PAndOnPool(uint8 marketIndex, uint256 amount) public {
+    function testShouldBorrowAmountP2PAndFromPool(uint8 marketIndex, uint256 amount) public {
         address[] memory markets = lens.getAllMarkets();
 
         vm.assume(marketIndex < markets.length);
 
-        SupplyTest memory test;
+        BorrowTest memory test;
         test.poolToken = ICToken(markets[marketIndex]);
         test.underlying = ERC20(
             address(test.poolToken) == morpho.cEth() ? morpho.wEth() : test.poolToken.underlying()
         );
         test.decimals = test.underlying.decimals();
 
+        ICompoundOracle oracle = ICompoundOracle(morpho.comptroller().oracle());
+        test.wEthPrice = oracle.getUnderlyingPrice(cEth);
+        test.underlyingPrice = oracle.getUnderlyingPrice(address(test.poolToken));
+
+        uint256 wEthBalance = ERC20(wEth).balanceOf(address(borrower1));
+
         vm.assume(
             amount >= 10**(test.decimals - 3) &&
-                amount <= test.underlying.balanceOf(address(supplier1))
+                amount <=
+                Math.min(
+                    morpho.comptroller().borrowCaps(address(test.poolToken)),
+                    Math.min(
+                        wEthBalance.mul(test.wEthPrice).div(test.underlyingPrice),
+                        test.underlying.balanceOf(address(test.poolToken))
+                    )
+                )
         );
 
         test.morphoBalanceOnPoolBefore = test.poolToken.balanceOf(address(morpho));
         test.morphoUnderlyingBalanceBefore = test.underlying.balanceOf(address(morpho));
 
-        supplier1.approve(address(test.underlying), amount);
-        supplier1.supply(address(test.poolToken), amount);
+        borrower1.approve(wEth, type(uint256).max);
+        borrower1.supply(cEth, wEthBalance);
+        borrower1.borrow(address(test.poolToken), amount);
 
-        test.p2pSupplyIndex = morpho.p2pSupplyIndex(address(test.poolToken));
-        test.poolSupplyIndex = test.poolToken.exchangeRateCurrent();
-        test.supplyRatePerBlock = lens.getCurrentUserSupplyRatePerBlock(
+        test.p2pBorrowIndex = morpho.p2pBorrowIndex(address(test.poolToken));
+        test.poolBorrowIndex = test.poolToken.borrowIndex();
+        test.borrowRatePerBlock = lens.getCurrentUserBorrowRatePerBlock(
             address(test.poolToken),
-            address(supplier1)
+            address(borrower1)
         );
-        (test.p2pSupplyRatePerBlock, , test.poolSupplyRatePerBlock, ) = lens.getRatesPerBlock(
+        (, test.p2pBorrowRatePerBlock, , test.poolBorrowRatePerBlock) = lens.getRatesPerBlock(
             address(test.poolToken)
         );
 
-        (test.balanceInP2P, test.balanceOnPool) = morpho.supplyBalanceInOf(
+        (test.balanceInP2P, test.balanceOnPool) = morpho.borrowBalanceInOf(
             address(test.poolToken),
-            address(supplier1)
+            address(borrower1)
         );
 
         address[] memory poolTokens = new address[](1);
         poolTokens[0] = address(test.poolToken);
-        test.unclaimedRewardsBefore = lens.getUserUnclaimedRewards(poolTokens, address(supplier1));
+        test.unclaimedRewardsBefore = lens.getUserUnclaimedRewards(poolTokens, address(borrower1));
 
-        test.underlyingInP2PBefore = test.balanceInP2P.mul(test.p2pSupplyIndex);
-        test.underlyingOnPoolBefore = test.balanceOnPool.mul(test.poolSupplyIndex);
+        test.underlyingInP2PBefore = test.balanceInP2P.mul(test.p2pBorrowIndex);
+        test.underlyingOnPoolBefore = test.balanceOnPool.mul(test.poolBorrowIndex);
         test.totalUnderlyingBefore = test.underlyingOnPoolBefore + test.underlyingInP2PBefore;
 
         assertLe(
             test.underlyingOnPoolBefore + test.underlyingInP2PBefore,
             amount,
-            "greater supplied amount than expected"
+            "greater borrowed amount than expected"
         );
         assertGe(
             test.underlyingOnPoolBefore + test.underlyingInP2PBefore + 10**(test.decimals / 2),
             amount,
-            "unexpected supplied amount"
+            "unexpected borrowed amount"
         );
         if (morpho.p2pDisabled(address(test.poolToken)))
             assertEq(test.underlyingInP2PBefore, 0, "unexpected underlying balance p2p");
         assertEq(test.unclaimedRewardsBefore, 0, "unclaimed rewards not zero");
 
-        assertEq( // TODO: check supply delta
-            test.underlying.balanceOf(address(morpho)),
-            test.morphoUnderlyingBalanceBefore,
-            "unexpected morpho underlying balance"
-        );
-        assertEq(
-            test.poolToken.balanceOf(address(morpho)) - test.morphoBalanceOnPoolBefore,
-            test.balanceOnPool,
-            "unexpected morpho underlying balance on pool"
-        );
+        // assertEq( // TODO: check borrow delta
+        //     test.underlying.balanceOf(address(morpho)),
+        //     test.morphoUnderlyingBalanceBefore,
+        //     "unexpected morpho underlying balance"
+        // );
+        // assertEq(
+        //     test.morphoBalanceOnPoolBefore - test.poolToken.balanceOf(address(morpho)),
+        //     test.balanceOnPool,
+        //     "unexpected morpho underlying balance on pool"
+        // );
 
         vm.roll(block.number + 500);
 
@@ -106,19 +122,19 @@ contract TestSupply is TestSetup {
 
         vm.roll(block.number + 500);
 
-        test.unclaimedRewardsAfter = lens.getUserUnclaimedRewards(poolTokens, address(supplier1));
+        test.unclaimedRewardsAfter = lens.getUserUnclaimedRewards(poolTokens, address(borrower1));
         (test.underlyingOnPoolAfter, test.underlyingInP2PAfter, test.totalUnderlyingAfter) = lens
-        .getCurrentSupplyBalanceInOf(address(test.poolToken), address(supplier1));
+        .getCurrentBorrowBalanceInOf(address(test.poolToken), address(borrower1));
 
         for (uint256 i; i < 1_000; ++i) {
             test.totalUnderlyingBefore = test.totalUnderlyingBefore.mul(
-                1e18 + test.supplyRatePerBlock
+                1e18 + test.borrowRatePerBlock
             );
             test.underlyingOnPoolBefore = test.underlyingOnPoolBefore.mul(
-                1e18 + test.poolSupplyRatePerBlock
+                1e18 + test.poolBorrowRatePerBlock
             );
             test.underlyingInP2PBefore = test.underlyingInP2PBefore.mul(
-                1e18 + test.p2pSupplyRatePerBlock
+                1e18 + test.p2pBorrowRatePerBlock
             );
         }
 
@@ -138,7 +154,7 @@ contract TestSupply is TestSetup {
             test.totalUnderlyingBefore,
             test.totalUnderlyingAfter,
             test.totalUnderlyingBefore / 1e3,
-            "unexpected total underlying amount from avg supply rate"
+            "unexpected total underlying amount from avg borrow rate"
         );
         assertApproxEqAbs(
             test.underlyingInP2PBefore + test.underlyingOnPoolBefore,
@@ -148,7 +164,7 @@ contract TestSupply is TestSetup {
         );
         if (
             test.underlyingOnPoolAfter > 0 &&
-            morpho.comptroller().compSupplySpeeds(address(test.poolToken)) > 0
+            morpho.comptroller().compBorrowSpeeds(address(test.poolToken)) > 0
         )
             assertGt(
                 test.unclaimedRewardsAfter,
