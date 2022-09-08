965bcaa88e038e93c06dd8e6d411d0a863a0a377
MerlinEgalite <egalite.merlin@gmail.com>
2022-09-08 08:33:20 +0200

    🧑‍💻 (packing) ease readability

--- contracts/aave-v3/ExitPositionsManager.sol
+++ contracts/aave-v3/ExitPositionsManager.sol
@@ -14,6 +14,7 @@ contract ExitPositionsManager is IExitPositionsManager, PositionsManagerUtils {
     using HeapOrdering for HeapOrdering.HeapArray;
     using PercentageMath for uint256;
     using SafeTransferLib for ERC20;
+    using Types for Types.Market;
     using WadRayMath for uint256;
     using Math for uint256;
 
@@ -161,7 +162,7 @@ contract ExitPositionsManager is IExitPositionsManager, PositionsManagerUtils {
         if (_amount == 0) revert AmountIsZero();
         if (_receiver == address(0)) revert AddressIsZero();
         Types.Market memory market = market[_poolToken];
-        if (market.underlyingToken == address(0)) revert MarketNotCreated();
+        if (!market.isCreated()) revert MarketNotCreated();
         if (market.isWithdrawPaused) revert WithdrawPaused();
 
         _updateIndexes(_poolToken);
@@ -188,7 +189,7 @@ contract ExitPositionsManager is IExitPositionsManager, PositionsManagerUtils {
     ) external {
         if (_amount == 0) revert AmountIsZero();
         Types.Market memory market = market[_poolToken];
-        if (market.underlyingToken == address(0)) revert MarketNotCreated();
+        if (!market.isCreated()) revert MarketNotCreated();
         if (market.isRepayPaused) revert RepayPaused();
 
         _updateIndexes(_poolToken);
@@ -210,10 +211,10 @@ contract ExitPositionsManager is IExitPositionsManager, PositionsManagerUtils {
         uint256 _amount
     ) external {
         Types.Market memory collateralMarket = market[_poolTokenCollateral];
-        if (collateralMarket.underlyingToken == address(0)) revert MarketNotCreated();
+        if (!collateralMarket.isCreated()) revert MarketNotCreated();
         if (collateralMarket.isLiquidateCollateralPaused) revert LiquidateCollateralPaused();
         Types.Market memory borrowedMarket = market[_poolTokenBorrowed];
-        if (borrowedMarket.underlyingToken == address(0)) revert MarketNotCreated();
+        if (!borrowedMarket.isCreated()) revert MarketNotCreated();
         if (borrowedMarket.isLiquidateBorrowPaused) revert LiquidateBorrowPaused();
 
         if (
