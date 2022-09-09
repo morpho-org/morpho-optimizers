ab34510ac938412a4b363bc732f0804bb394d266
MerlinEgalite <egalite.merlin@gmail.com>
2022-09-09 18:34:39 +0200

    ♻️ (granular-pausing): apply it to aave v2

--- test-foundry/aave-v2/TestWithdraw.t.sol
+++ test-foundry/aave-v2/TestWithdraw.t.sol
@@ -653,7 +653,7 @@ contract TestWithdraw is TestSetup {
         borrower1.supply(aWeth, 10 * amount);
         borrower1.borrow(aDai, 10 * amount);
 
-        morpho.setPauseStatus(aDai, true, true, true, true, true, true);
+        morpho.setWithdrawPauseStatus(aDai, true);
 
         supplier1.withdraw(aDai, amount);
         supplier1.withdraw(aDai, amount);
