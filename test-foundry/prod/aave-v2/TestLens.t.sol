9279c6b7eb60f0661c39a927a0688c62a9d82a0a
MerlinEgalite <egalite.merlin@gmail.com>
2022-10-03 15:14:05 +0200

    fix: test

--- test-foundry/prod/aave-v2/TestLens.t.sol
+++ test-foundry/prod/aave-v2/TestLens.t.sol
@@ -3,41 +3,59 @@ pragma solidity 0.8.13;
 
 import "./setup/TestSetup.sol";
 
-contract TestBorrow is TestSetup {
+contract TestLens is TestSetup {
     using ReserveConfiguration for DataTypes.ReserveConfigurationMap;
     using WadRayMath for uint256;
     using Math for uint256;
+
+    struct Vars {
+        uint256 p2pSupplyIndex;
+        uint256 p2pBorrowIndex;
+        uint256 poolSupplyIndex;
+        uint256 poolBorrowIndex;
+    }
 
     function testUpgradeLens() public {
         Lens lensImplV2 = new Lens(address(morpho));
 
         address[] memory marketsCreated = lens.getAllMarkets();
+        Vars[] memory expectedValues = new Vars[](marketsCreated.length);
+
+        vm.prank(proxyAdmin.owner());
+        proxyAdmin.upgrade(lensProxy, address(lensImplV2));
 
         for (uint256 i; i < marketsCreated.length; ++i) {
             address market = marketsCreated[i];
             if (market == aStEth) continue;
 
             (
-                uint256 p2pSupplyIndexBefore,
-                uint256 p2pBorrowIndexBefore,
-                uint256 poolSupplyIndexBefore,
-                uint256 poolBorrowIndexBefore
+                uint256 p2pSupplyIndex,
+                uint256 p2pBorrowIndex,
+                uint256 poolSupplyIndex,
+                uint256 poolBorrowIndex
             ) = lens.getIndexes(market);
 
-            vm.prank(proxyAdmin.owner());
-            proxyAdmin.upgrade(lensProxy, address(lensImplV2));
+            expectedValues[i].p2pSupplyIndex = p2pSupplyIndex;
+            expectedValues[i].p2pBorrowIndex = p2pBorrowIndex;
+            expectedValues[i].poolSupplyIndex = poolSupplyIndex;
+            expectedValues[i].poolBorrowIndex = poolBorrowIndex;
+        }
+
+        for (uint256 i; i < marketsCreated.length; ++i) {
+            address market = marketsCreated[i];
+            if (market == aStEth) continue;
 
             (
-                uint256 p2pSupplyIndexAfter,
-                uint256 p2pBorrowIndexAfter,
-                uint256 poolSupplyIndexAfter,
-                uint256 poolBorrowIndexAfter
+                uint256 p2pSupplyIndex,
+                uint256 p2pBorrowIndex,
+                uint256 poolSupplyIndex,
+                uint256 poolBorrowIndex
             ) = lens.getIndexes(market);
 
-            assertEq(p2pSupplyIndexAfter, p2pSupplyIndexBefore);
-            assertEq(p2pBorrowIndexAfter, p2pBorrowIndexBefore);
-            assertEq(poolSupplyIndexAfter, poolSupplyIndexBefore);
-            assertEq(poolBorrowIndexAfter, poolBorrowIndexBefore);
+            assertEq(expectedValues[i].p2pSupplyIndex, p2pSupplyIndex);
+            assertEq(expectedValues[i].p2pBorrowIndex, p2pBorrowIndex);
+            assertEq(expectedValues[i].poolSupplyIndex, poolSupplyIndex);
+            assertEq(expectedValues[i].poolBorrowIndex, poolBorrowIndex);
         }
     }
 }
