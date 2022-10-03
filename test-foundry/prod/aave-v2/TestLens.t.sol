c93d91fd4c2b1965afecaa0f6d417402b15fbf18
MerlinEgalite <egalite.merlin@gmail.com>
2022-10-03 14:59:30 +0200

    test: improve test

--- test-foundry/prod/aave-v2/TestLens.t.sol
+++ test-foundry/prod/aave-v2/TestLens.t.sol
@@ -11,26 +11,33 @@ contract TestBorrow is TestSetup {
     function testUpgradeLens() public {
         Lens lensImplV2 = new Lens(address(morpho));
 
-        (
-            uint256 p2pSupplyIndexBefore,
-            uint256 p2pBorrowIndexBefore,
-            uint256 poolSupplyIndexBefore,
-            uint256 poolBorrowIndexBefore
-        ) = lens.getIndexes(aDai);
+        address[] memory marketsCreated = lens.getAllMarkets();
 
-        vm.prank(proxyAdmin.owner());
-        proxyAdmin.upgrade(lensProxy, address(lensImplV2));
+        for (uint256 i; i < marketsCreated.length; ++i) {
+            address market = marketsCreated[i];
+            if (market == aStEth) continue;
 
-        (
-            uint256 p2pSupplyIndexAfter,
-            uint256 p2pBorrowIndexAfter,
-            uint256 poolSupplyIndexAfter,
-            uint256 poolBorrowIndexAfter
-        ) = lens.getIndexes(aDai);
+            (
+                uint256 p2pSupplyIndexBefore,
+                uint256 p2pBorrowIndexBefore,
+                uint256 poolSupplyIndexBefore,
+                uint256 poolBorrowIndexBefore
+            ) = lens.getIndexes(market);
 
-        assertEq(p2pSupplyIndexAfter, p2pSupplyIndexBefore);
-        assertEq(p2pBorrowIndexAfter, p2pBorrowIndexBefore);
-        assertEq(poolSupplyIndexAfter, poolSupplyIndexBefore);
-        assertEq(poolBorrowIndexAfter, poolBorrowIndexBefore);
+            vm.prank(proxyAdmin.owner());
+            proxyAdmin.upgrade(lensProxy, address(lensImplV2));
+
+            (
+                uint256 p2pSupplyIndexAfter,
+                uint256 p2pBorrowIndexAfter,
+                uint256 poolSupplyIndexAfter,
+                uint256 poolBorrowIndexAfter
+            ) = lens.getIndexes(market);
+
+            assertEq(p2pSupplyIndexAfter, p2pSupplyIndexBefore);
+            assertEq(p2pBorrowIndexAfter, p2pBorrowIndexBefore);
+            assertEq(poolSupplyIndexAfter, poolSupplyIndexBefore);
+            assertEq(poolBorrowIndexAfter, poolBorrowIndexBefore);
+        }
     }
 }
