959274a608dc9d18cb0519a9c51ee3d5278b4419
MerlinEgalite <egalite.merlin@gmail.com>
2022-10-04 12:03:19 +0200

    🔥 (#1336) Remove lib

--- contracts/aave-v2/libraries/Types.sol
+++ contracts/aave-v2/libraries/Types.sol
@@ -1,16 +1,6 @@
 // SPDX-License-Identifier: GNU AGPLv3
 pragma solidity ^0.8.0;
 
-library MarketLib {
-    function isCreated(Types.Market storage _market) internal view returns (bool) {
-        return _market.underlyingToken != address(0);
-    }
-
-    function isCreatedMemory(Types.Market memory _market) internal pure returns (bool) {
-        return _market.underlyingToken != address(0);
-    }
-}
-
 /// @title Types.
 /// @author Morpho Labs.
 /// @custom:contact security@morpho.xyz
