fbcd000f0d3cc9e71da4f728d72450c9c6ae7e73
Quentin Garchery <garchery.quentin@gmail.com>
2022-11-09 11:09:39 +0100

    refactor: remove useless storage assigns

--- contracts/compound/PositionsManager.sol
+++ contracts/compound/PositionsManager.sol
@@ -564,9 +564,7 @@ contract PositionsManager is IPositionsManager, MatchingEngine {
         );
 
         deltas.p2pSupplyDelta += _amount.div(poolSupplyIndex);
-        deltas.p2pSupplyDelta = deltas.p2pSupplyDelta;
         deltas.p2pBorrowDelta += _amount.div(lastPoolIndexes.lastBorrowPoolIndex);
-        deltas.p2pBorrowDelta = deltas.p2pBorrowDelta;
         emit P2PSupplyDeltaUpdated(_poolToken, deltas.p2pSupplyDelta);
         emit P2PBorrowDeltaUpdated(_poolToken, deltas.p2pBorrowDelta);
 
