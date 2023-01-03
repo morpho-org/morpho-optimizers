d4fe1055af38f68c6d2921a17b97b21864b864aa
patrick <patrick@morpho.xyz>
2023-01-03 11:57:30 -0500

    fix: remove defunct test

--- test/aave-v2/TestBorrow.t.sol
+++ test/aave-v2/TestBorrow.t.sol
@@ -210,16 +210,6 @@ contract TestBorrow is TestSetup {
         morpho.borrow(aDai, 0, type(uint256).max);
     }
 
-    function testShouldNotBorrowAssetNotBorrowable() public {
-        uint256 amount = 100 ether;
-
-        borrower1.approve(dai, type(uint256).max);
-        borrower1.supply(aDai, amount);
-
-        hevm.expectRevert(EntryPositionsManager.BorrowingNotEnabled.selector);
-        borrower1.borrow(aAave, amount / 2);
-    }
-
     function testShouldNotAllowSmallBorrow() public {
         (uint256 ltv, , , , ) = pool.getConfiguration(dai).getParamsMemory();
 
