c5e426241bf5bfb0d9575b662aa19c79dd60464b
Quentin Garchery <garchery.quentin@gmail.com>
2023-01-03 14:24:12 +0100

    fix: add .env.local and FOUNDRY_ETH_RPC_URL to env

--- export_env.sh
+++ export_env.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+. .env.local
+
 [ -z "${PROTOCOL}" ] && export PROTOCOL="compound"
 [ -z "${NETWORK}" ] && export NETWORK="eth-mainnet"
 
@@ -8,6 +10,7 @@
 [ -z "${FOUNDRY_PROFILE}" ] && export FOUNDRY_PROFILE="${PROTOCOL}"
 [ -z "${FOUNDRY_REMAPPINGS}" ] && export FOUNDRY_REMAPPINGS="@config/=config/${NETWORK}/${PROTOCOL}/"
 [ -z "${FOUNDRY_PRIVATE_KEY}" ] && export FOUNDRY_PRIVATE_KEY="${DEPLOYER_PRIVATE_KEY}"
+[ -z "${FOUNDRY_ETH_RPC_URL}" ] && export FOUNDRY_ETH_RPC_URL="https://${NETWORK}.g.alchemy.com/v2/${ALCHEMY_KEY}"
 
 if [ "${FOUNDRY_PROFILE}" = "production" ]; then
     export FOUNDRY_TEST="test/prod/${PROTOCOL}/"
