bda8ff1cb0db2b823945853145ea7f70014151f9
Rubilmax <rmilon@gmail.com>
2023-01-09 19:02:02 +0100

    Update tests

--- test/aave-v2/setup/TestSetup.sol
+++ test/aave-v2/setup/TestSetup.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.0;
 
+import "src/aave-v2/interfaces/aave/IReserveInterestRateStrategy.sol";
 import "src/aave-v2/interfaces/aave/IVariableDebtToken.sol";
 import "src/aave-v2/interfaces/aave/IAToken.sol";
 import "src/aave-v2/interfaces/IMorpho.sol";
